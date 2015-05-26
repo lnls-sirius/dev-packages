92c7a008712417d02d4252ab444320bf6d5e3819
ximenes <xresende@gmail.com>
2015-05-26 13:48:45 -0300

    Change in setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@
 
 from distutils.core import setup
 
+with open('VERSION','r') as _f:
+    __version__ = _f.read().strip()
 
 setup(
     name='MathPhys',
-    version='0.1.0',
+    version=__version__, 
     description='LNLS Math and Physics utilities',
     url='https://github.com/lnls-fac/mathphys',
     packages=['mathphys'],
