88568c7e47cee67e3c0e6fdab6bb2d3de8b31371
Afonso Haruo Carnielli Mukai <afonso.mukai@lnls.br>
2015-05-26 10:58:23 -0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+
+setup(
+    name='MathPhys',
+    version='0.1.0',
+    description='LNLS Math and Physics utilities',
+    url='https://github.com/lnls-fac/mathphys',
+    packages=['mathphys'],
+)
