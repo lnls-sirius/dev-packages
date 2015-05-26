bd5ddbe0fe474cdadc97bf6d220919659f50f77c
Afonso Haruo Carnielli Mukai <afonso.mukai@lnls.br>
2015-05-26 15:24:56 -0300

    Change installation to use setuptools

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup, find_packages
 
 
 setup(
-    name='MathPhys',
+    name='mathphys',
     version='0.1.0',
     description='LNLS Math and Physics utilities',
     url='https://github.com/lnls-fac/mathphys',
-    packages=['mathphys'],
+    package_dir={'mathphys': 'src'},
+    packages=find_packages(),
+
+    install_requires=['lnls==0.1'],
+    dependency_links=['https://github.com/lnls-fac/lnls/archive/v0.1.0.tar.gz#egg=lnls-0.1.0'],
 )
