4b55c5f027c5763f01318ecf8401d65e8737e0cb
Afonso Haruo Carnielli Mukai <afonso.mukai@lnls.br>
2015-06-02 18:18:55 -0300

    Remove Beam class

--- mathphys/beam_optics.py
+++ mathphys/beam_optics.py
@@ -1,31 +1,6 @@
 import math as _math
 import mathphys as _mp
 
-class Beam:
-
-    def __init__(self, energy, current = 0):
-
-        self.energy = energy
-        self.current = current
-        self.brho, self.velocity, self.beta, self.gamma = Beam.calc_brho(self.energy)
-
-    @staticmethod
-    def calc_brho(energy = None, gamma = None, beta = None, velocity = None):
-        electron_rest_energy_GeV = mathphys.units.joule_2_eV * (mathphys.constants.electron_rest_energy / 1e9)
-        gamma    = energy/electron_rest_energy_GeV
-        beta     = math.sqrt(((gamma-1.0)/gamma)*((gamma+1.0)/gamma))
-        velocity = mathphys.constants.light_speed * beta
-        brho     = beta * (energy * 1e9) / mathphys.constants.light_speed
-        return brho, velocity, beta, gamma
-
-    def __str__(self):
-        r = ''
-        r += '{0:<10s} {1:f} GeV'.format('energy:', self.energy)
-        r += '\n{0:<10s} {1:f}'.format('gamma:', self.gamma)
-        r += '\n{0:<10s} 1 - {1:e}'.format('beta:', 1.0-self.beta)
-        r += '\n{0:<10s} {1:.0f} - {2:f} m/s'.format('velocity:', consts.light_speed, consts.light_speed - self.velocity)
-        r += '\n{0:<10s} {1:f} T.m'.format('brho:', self.brho)
-        return r
 
 def calc_U0(beam_energy, I2):
     """Calculate U0 [keV] from beam energy [GeV] and I2 [1/m]"""
