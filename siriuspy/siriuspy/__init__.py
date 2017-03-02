590b4f427d121988af4b73c68973c43da6a1cae5
Fernando <fernandohds564@gmail.com>
2017-03-02 16:23:07 -0300

    naming system module.

--- siriuspy/siriuspy/__init__.py
+++ siriuspy/siriuspy/__init__.py
@@ -4,10 +4,11 @@ from . import ns
 from . import ccdb
 from . import web
 from . import magnet
+from . import naming_system
 
 import os as _os
 with open(_os.path.join(__path__[0], 'VERSION'), 'r') as _f:
      __version__ = _f.read().strip()
 del _os
 
-__all__ = ['config','util','ns','ccdb','web','magnet']
+__all__ = ['config','util','ns','ccdb','web','magnet','naming_system']
