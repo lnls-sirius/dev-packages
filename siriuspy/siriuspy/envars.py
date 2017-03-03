41e2c5614ea31a4623462d8a8f06dff6c9f0405f
ximenes <xresende@gmail.com>
2017-03-03 09:04:03 -0300

    Edit siriuspy/envars.py

--- siriuspy/siriuspy/envars.py
+++ siriuspy/siriuspy/envars.py
@@ -3,28 +3,74 @@ import os as _os
 
 # --- folders ---
 
-folder_root = _os.environ.get('ROOT_GROUP', '/home/fac_files')
-
-folder_epics_base = _os.environ.get('EPICS_DIR', default='/usr/local/epics')
-
-folder_lnls_sirius = _os.environ.get('LNLS_SIRIUS', _os.path.join(folder_root, 'lnls-sirius'))
-folder_lnls_fac = _os.environ.get('LNLS_FAC', _os.path.join(folder_root, 'lnls-fac'))
-folder_lnls_ima = _os.environ.get('LNLS_IMA', _os.path.join(folder_root, 'lnls-ima'))
-
-folder_fac_code = _os.path.join(folder_root, 'lnls-fac')
-folder_fac_data = _os.path.join(folder_root, 'data')
-
-folder_lnls_sirius_csslnls = _os.path.join(folder_lnls_sirius, 'org.csstudio.product')
-folder_lnls_sirius_discs = _os.path.join(folder_lnls_sirius, 'discs-management')
-folder_lnls_sirius_csconstants = _os.path.join(folder_lnls_sirius, 'control-system-constants')
+folder_root        = _os.environ.get('ROOT_GROUP',  default='/home/fac_files')
+folder_epics_base  = _os.environ.get('EPICS_DIR',   default='/usr/local/epics')
+folder_lnls_sirius = _os.environ.get('LNLS_SIRIUS', default=_os.path.join(folder_root, 'lnls-sirius'))
+folder_lnls_fac    = _os.environ.get('LNLS_FAC',    default=_os.path.join(folder_root, 'lnls-fac'))
+folder_lnls_ima    = _os.environ.get('LNLS_IMA',    default=_os.path.join(folder_root, 'lnls-ima'))
+folder_fac_code    = _os.path.join(folder_root, 'lnls-fac')
+folder_fac_data    = _os.path.join(folder_root, 'data')
+folder_lnls_sirius_csslnls      = _os.path.join(folder_lnls_sirius, 'org.csstudio.product')
+folder_lnls_sirius_discs        = _os.path.join(folder_lnls_sirius, 'discs-management')
+folder_lnls_sirius_csconstants  = _os.path.join(folder_lnls_sirius, 'control-system-constants')
 folder_lnls_sirius_dev_packages = _os.path.join(folder_lnls_sirius, 'dev-packages')
-folder_lnls_sirius_hla = _os.path.join(folder_lnls_sirius, 'hla')
+folder_lnls_sirius_hla          = _os.path.join(folder_lnls_sirius, 'hla')
 
+# these are temporary definitions. they shoul be moved elsewhere...
+folder_excitation_data = _os.path.join(folder_lnls_sirius_csconstants, 'magnets', 'excitation-data')
+folder_pulse_curves = _os.path.join(folder_lnls_sirius_csconstants, 'magnets', 'pulse-curve-data')
 
 # --- support applications IPs/URLs ---
 
-server_url_rbac = _os.environ.get('SIRIUS_URL_RBAC', 'https://10.0.7.55:8445')
-server_url_ns = _os.environ.get('SIRIUS_URL_NS', 'http://10.0.7.55:8080/names')
-server_url_ccdb = _os.environ.get('SIRIUS_URL_CCDB', 'http://10.0.7.55:8083')
-server_url_cables = _os.environ.get('SIRIUS_URL_CABLES', 'http://10.0.7.55:8086')
-server_url_web = _os.environ.get('SIRIUS_URL_WEB', 'http://10.0.7.55')
+server_url_rbac   = _os.environ.get('SIRIUS_URL_RBAC',   default='https://10.0.7.55:8445')
+server_url_ns     = _os.environ.get('SIRIUS_URL_NS',     default='http://10.0.7.55:8080/names')
+server_url_ccdb   = _os.environ.get('SIRIUS_URL_CCDB',   default='http://10.0.7.55:8083')
+server_url_cables = _os.environ.get('SIRIUS_URL_CABLES', default='http://10.0.7.55:8086')
+server_url_web    = _os.environ.get('SIRIUS_URL_WEB',    default='http://10.0.7.55')
+
+# --- repositories ---
+
+org_folders = {
+    'lnls-fac':folder_lnls_fac,
+    'lnls-ima':folder_lnls_ima,
+    'lnls-sirius':folder_lnls_sirius,
+}
+
+repo_names = {
+
+    'lnls-fac': ['apsuite',
+                 'tools',
+                 'collective_effects',
+                 'job_manager',
+                 'pyaccel',
+                 'trackcpp',
+                 'fieldmaptrack',
+                 'lnls',
+                 'sands-adaptation',
+                 'tracy_sirius',
+                 'mathphys',
+                 'scripts',
+                 'va',
+                 'MatlabMiddleLayer',
+                 'models'],
+
+    'lnls-ima': ['bo-correctors',
+                 'bo-sextupoles',
+                 'tb-dipoles',
+                 'bo-dipoles',
+                 'si-dipoles-b1',
+                 'si-quadrupoles-q14',
+                 'bo-quadrupoles-qd',
+                 'si-dipoles-b2',
+                 'si-quadrupoles-q20',
+                 'bo-quadrupoles-qf',
+                 'si-fast-correctors',
+                 'si-sextupoles-s15'],
+
+    'lnls-sirius': ['control-system-constants',
+                    'discs-management',
+                    'org.csstudio.product',
+                    'dev-packages',
+                    'hla'],
+
+}
