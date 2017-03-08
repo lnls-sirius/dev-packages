24741381cf4562498ea362195963c3b149eb5900
ximenes <xresende@gmail.com>
2017-03-08 11:56:49 -0300

    Add sirius_pv_time_serie in epics/__init__.py

--- siriuspy/siriuspy/epics/__init__.py
+++ siriuspy/siriuspy/epics/__init__.py
@@ -2,8 +2,9 @@ from .sirius_pv import *
 from .sirius_pvs_set import *
 from .group_pvs import *
 from .derived_pv import *
+from .sirius_pv_time_serie import *
 
-
+del sirius_pv_time_serie
 del derived_pv
 del group_pvs
 del sirius_pvs_set
