e496d3059069d1e4940621fa27325e7bb834ea15
Ximenes Resende <xresende@gmail.com>
2017-03-16 01:45:07 -0300

    Create namedtupple.py

---
+++ siriuspy/siriuspy/namedtupple.py
@@ -0,0 +1,8 @@
+import collections.namedtupple as _namedtupple
+
+EnumTypes = _namedtupple('EnumTypes', ('OffOnTyp',
+                                       'OffOnWaitTyp',
+                                       'DsblEnblTyp',
+                                       'PSOpModeTyp',
+                                       'RmtLocType',
+                                       'SOFBOpModeTyp',))
