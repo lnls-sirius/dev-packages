300637f120719bc1426d9ee6603df57c395ab381
Ximenes Resende <xresende@gmail.com>
2017-03-16 01:48:38 -0300

    Update __init__.py

--- siriuspy/siriuspy/__init__.py
+++ siriuspy/siriuspy/__init__.py
@@ -5,6 +5,7 @@ from . import macapp_ccdb
 from . import macapp_web
 from . import magnet
 from . import naming_system
+from . import namedtupple
 from . import dev_types
 from . import epics
 
@@ -15,4 +16,5 @@ with open(_os.path.join(__path__[0], 'VERSION'), 'r') as _f:
 del _os
 
 
-__all__ = ['envars','util','macapp_ns','macapp_ccdb','macapp_web','magnet','naming_system','epics']
+__all__ = ['envars','util','macapp_ns','macapp_ccdb','macapp_web','magnet','naming_system',
+           'namedtupple','dev_types','epics']
