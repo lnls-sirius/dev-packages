5a786d627d3cf689725896a627b417c9d653ea50
ximenes <xresende@gmail.com>
2017-04-13 19:12:12 -0300

    Reverting SIOC port number scheme in si-ap-currlt

--- si-ap-currlt/si-ap-currlt/si-ap-currlt.py
+++ si-ap-currlt/si-ap-currlt/si-ap-currlt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-import siriuspy as _siriuspy
-_siriuspy.util.set_ioc_ca_port_number('si-ap-currlt')
+#import siriuspy as _siriuspy
+#_siriuspy.util.set_ioc_ca_port_number('si-ap-currlt')
 
 import pcaspy as _pcaspy
 import pcaspy.tools as _pcaspy_tools
