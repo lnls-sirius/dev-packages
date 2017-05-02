61191c18a5ecb80172939b721a8d13045e357b00
ana <ana.clara@lnls.br>
2017-05-02 14:05:49 -0300

    Just keep the designer_plugin.py file to future siriusdm designer plugins

---
+++ siriusdm/siriusdm/designer_plugin.py
@@ -0,0 +1,2 @@
+# If you want to use these widgets in the QtDesigner, add this path to PYQTDESIGNERPATH:
+# export PYQTDESIGNERPATH=$PYQTDESIGNERPATH:/home/fac_files/lnls-sirius/dev-packages/siriusdm/siriusdm
