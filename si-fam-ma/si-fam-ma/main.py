cd8bc5b904c74c7493f26cd098cb7f65e55b8761
Guilherme <guilherme.prado@lnls.br>
2017-06-13 19:20:39 -0300

    Updating ioc for sirius magnets

--- si-fam-ma/si-fam-ma/main.py
+++ si-fam-ma/si-fam-ma/main.py
@@ -2,6 +2,10 @@ import pvs as _pvs
 import time as _time
 import siriuspy as _siriuspy
 import uuid as _uuid
+import re as _re
+from siriuspy.search import MASearch as _MASearch
+import threading
+from epics import caget
 
 # Coding guidelines:
 # =================
@@ -18,13 +22,13 @@ class App:
 
     ma_devices = _pvs.get_ma_devices()
     pvs_database = _pvs.get_pvs_database()
-    strengths = ['Energy', 'KL', 'SL', 'Kick']
+    strengths = ['Energy', 'KL', 'SL', 'Kick', 'KLRef']
     writable_fields = ['SP', 'Sel']
 
     def __init__(self, driver):
 
         _siriuspy.util.print_ioc_banner(
-            ioc_name = 'si-fam-ma',
+            ioc_name = 'si-ma',
             db = App.pvs_database,#[_pvs._PREFIX],
             description = 'SI Magnet Power Supply Soft IOC',
             version = __version__,
@@ -33,11 +37,6 @@ class App:
         self._driver = driver
         self._set_callback()
 
-    def _set_callback(self):
-        for family, device in App.ma_devices.items():
-            #device.set_callback(self._mycallback)
-            device.callback = self._mycallback
-            device._controller.update_state()
 
     @property
     def driver(self):
@@ -45,65 +44,15 @@ class App:
 
     def process(self, interval):
         _time.sleep(interval)
-        #print(_siriuspy.util.get_timestamp())
 
     def read(self, reason):
+        """ Reads from IOC database """
         return None
-    #     if 'Version-Cte' in reason: return None
-    #     #Get property and field
-    #     device, propty, field = self._get_dev_propty(reason)
-    #     print(propty)
-    #     if propty not in App.strengths: return None
-    #     #Check if property is a 'strength' and get attribute name
-    #     attr = 'strength_' + field.lower()
-    #     #Return Magnet attribute based on the property and field requested
-    #     ma = self.ma_devices[device]
-    #     return getattr(ma, attr)
-
-        # ma, ps, propty = self._get_dev_propty(reason)
-        # if propty == 'CtrlMode-Mon':
-        #     return ps.ctrlmode_mon
-        # elif propty == 'PwrState-Sel':
-        #     return ps.pwrstate_sel
-        # elif propty == 'PwtState-Sts':
-        #     return ps.pwrstate_sts
-        # elif propty == 'OpMode-Sel':
-        #     return ps.opmode_sel
-        # elif propty == 'OpMode-Sts':
-        #     return ps.opmode_sts
-        # elif propty == 'Current-RB':
-        #     return ps.current_rb
-        # elif propty == 'Current-SP':
-        #     return ps.current_sp
-        # elif propty in ('KL-SP','SL-SP'):
-        #     return ma.kl_sp
-        # elif propty == ('KL-RB','SL-RB'):
-        #     return ma.kl_rb
-        # return None
-
-    @staticmethod
-    def _get_dev_propty(reason):
-        ''' Returns 'property' and 'field' of a PV '''
-        device, pfield = reason.split(':')
-        propty, field = pfield.split('-')
-        return (device, propty, field)
-        # family, propty = reason.split(':')
-        # ma = App.ma_devices[family]
-        # ps = ma.get_ps('SI-Fam:PS-'+family) # there is only one PS anyway
-        # return ma, ps, propty
-
-    def _mycallback(self, pvname, value, **kwargs):
-        _, reason = pvname.split('PS-')
-        prev_value = self._driver.getParam(reason)
-        if value != prev_value:
-             #print(reason, value)
-             self._driver.setParam(reason, value)
-             self._driver.updatePVs()
 
     def write(self, reason, value):
         """ Write value to reason and let callback update PV database. """
         #Get property and field
-        device, propty, field = self._get_dev_propty(reason)
+        sub_section, discipline, device, propty, field = self._break_name(reason)
         #Check if field is writable
         if field not in App.writable_fields:
             return
@@ -113,17 +62,54 @@ class App:
         else:
             attr = propty + '_' + field
         attr = attr.lower()
-        #Set Magnet attribute based on the property, field and value passed
-        ma = self.ma_devices[device]
-        return setattr(ma, attr, value)
-        # ma, ps, propty = self._get_dev_propty(reason)
-        # if propty == 'PwrState-Sel':
-        #     ps.pwrstate_sel = value
-        # elif propty == 'OpMode-Sel':
-        #     ps.opmode_sel = value
-        # elif propty == 'Current-SP':
-        #     ps.current_sp = value
-        # elif propty in ('KL-SP','SL-SP'):
-        #     ma.kl_sp = value
-        # else:
-        #     return
+        #Update MA Object
+        slot_name = sub_section + ':' + discipline + '-' + device
+        ma = self.ma_devices[slot_name]
+        setattr(ma, attr, value)
+        #Update IOC database
+        self._driver.setParam(reason, value)
+        self._driver.updatePVs()
+        #Update trim objects
+        # if      sub_section == "Fam" and \
+        #         propty in (['Current'] + App.strengths) and \
+        #         field == 'SP':
+        #
+        #         self._update_trim(reason)
+
+        return
+
+    def _break_name(self, reason):
+        ''' Breaks a reason into its sub parts '''
+        sub_section, discdev, pfield = reason.split(':')
+        propty, field = pfield.split('-')
+        discipline, device = discdev.split('-')
+        return (sub_section, discipline, device, propty, field)
+
+    def _update_trim(self, reason):
+        """ Update magnet trim objects """
+        trims = self._get_trims(reason)
+        trims = [x.replace(":PS", ":MA").split('SI-')[1] for x in trims]
+        if trims:
+            _, _, _, propty, field= self._break_name(reason)
+            for trim in trims:
+                trim_reason = trim + ':' + propty + '-' + field
+                ma = self.ma_devices[trim]
+                value = getattr(ma, "strength_" + field.lower())
+                self._driver.setParam(trim_reason, value)
+                self._driver.updatePVs()
+
+    def _get_trims(self, reason):
+        pvname = _pvs._PREFIX + ':'.join(reason.split(':')[:2])
+        return _MASearch.conv_maname_2_trims(pvname)
+
+    def _set_callback(self):
+        for family, device in App.ma_devices.items():
+            device.callback = self._mycallback
+            device._controller.update_state()
+
+    def _mycallback(self, pvname, value, **kwargs):
+        _, reason = pvname.split('SI-')
+        prev_value = self._driver.getParam(reason)
+        if value != prev_value:
+            self._driver.setParam(reason, value)
+            self._driver.updatePVs()
