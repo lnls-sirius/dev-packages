cbd04f78b3a5ac71d299adbb964815176b6f89ac
ximenes <xresende@gmail.com>
2017-08-02 16:03:02 -0300

    Fixing conflicts

--- siriuspy/siriuspy/envars.py
+++ siriuspy/siriuspy/envars.py
@@ -3,7 +3,6 @@ import os as _os
 
 
 # --- folders ---
-<<<<<<< HEAD
 folder_root = _os.environ.get('ROOT_GROUP',
                               default='/home/fac_files')
 folder_epics_base = _os.environ.get('EPICS_DIR',
@@ -42,42 +41,6 @@ server_url_cables = _os.environ.get('SIRIUS_URL_CABLES',
                                     default='http://cables:8086')
 server_url_consts = _os.environ.get('SIRIUS_URL_CONSTS',
                                     default='http://sirius-consts.lnls.br')
-=======
-
-folder_root        = _os.environ.get('ROOT_GROUP',  default='/home/fac_files')
-folder_epics_base  = _os.environ.get('EPICS_DIR',   default='/usr/local/epics')
-folder_lnls_sirius = _os.environ.get('LNLS_SIRIUS', \
-                        default=_os.path.join(folder_root, 'lnls-sirius'))
-folder_lnls_fac    = _os.environ.get('LNLS_FAC',    \
-                        default=_os.path.join(folder_root, 'lnls-fac'))
-folder_lnls_ima    = _os.environ.get('LNLS_IMA',    \
-                        default=_os.path.join(folder_root, 'lnls-ima'))
-folder_fac_code    = _os.path.join(folder_root, 'lnls-fac')
-folder_fac_data    = _os.path.join(folder_root, 'data')
-folder_lnls_sirius_csslnls      = _os.path.join(folder_lnls_sirius, \
-                                    'org.csstudio.product')
-folder_lnls_sirius_discs        = _os.path.join(folder_lnls_sirius, \
-                                    'discs-management')
-folder_lnls_sirius_csconstants  = _os.path.join(folder_lnls_sirius, \
-                                    'control-system-constants')
-folder_lnls_sirius_dev_packages = _os.path.join(folder_lnls_sirius, \
-                                    'dev-packages')
-folder_lnls_sirius_hla          = _os.path.join(folder_lnls_sirius, 'hla')
-
-# --- support applications IPs/URLs ---
-
-server_url_rbac   = _os.environ.get('SIRIUS_URL_RBAC',   \
-                        default='https://rbac:8445')
-server_url_ns     = _os.environ.get('SIRIUS_URL_NS',     \
-                        default='http://naming-service-wildfly:8089/names')
-server_url_ccdb   = _os.environ.get('SIRIUS_URL_CCDB',   \
-                        default='http://ccdb:8083')
-server_url_cables = _os.environ.get('SIRIUS_URL_CABLES', \
-                        default='http://cables:8086')
-server_url_consts = _os.environ.get('SIRIUS_URL_CONSTS', \
-                        default='http://sirius-consts.lnls.br')
-
->>>>>>> ximenes-ps
 vaca_prefix = _os.environ.get('VACA_PREFIX',   default='VA-')
 
 # --- repositories ---
--- siriuspy/siriuspy/timesys/__init__.py
+++ siriuspy/siriuspy/timesys/__init__.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-__all__ = ['time_simul', 'time_data']
-=======
 """Timesys subpackage."""
 
 __all__ = ('time_simul', 'time_data')
->>>>>>> ximenes-ps
