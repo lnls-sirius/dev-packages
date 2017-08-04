44360ab26d7e26280393a2e28d42f7484cf70a69
ximenes <xresende@gmail.com>
2017-08-04 19:07:48 -0300

    Update files for as-ma IOCs

--- as-ma/systemd/ioc-as-ma-tb-dipole-fam.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Loads bashrc-sirius
-# To run final IOC the line setting environment variable VACA_PREFIX
-# in bashrc-sirius should be commented out.
-
-source ~/.bashrc
-#source /usr/local/etc/bashrc-sirius
-./as-ma.py "TB-" "TB" "Fam" "MA" "B.*"
