9c8f4e5bc2a4b701e6031340f1676270a8845eda
Fernando <fernandohds564@gmail.com>
2017-08-04 13:22:06 -0300

    Set _LOCAL variable of bpms crates and time_data modules to False.

--- siriuspy/siriuspy/timesys/time_data.py
+++ siriuspy/siriuspy/timesys/time_data.py
@@ -14,7 +14,7 @@ if _importlib.find_loader('matplotlib') is not None:
     import matplotlib.cm as _cmap
 
 _timeout = 1.0
-_LOCAL = True
+_LOCAL = False
 
 
 class Events:
@@ -33,7 +33,7 @@ class Events:
     LL_RGX = _re.compile('Event([0-9A-E]{2})([a-z-\.]*)', _re.IGNORECASE)
     HL_RGX = _re.compile('('+'|'.join(list(HL2LL_MAP.keys())) +
                          ')([a-z-\.]*)', _re.IGNORECASE)
-    HL_PREF = 'AS-Glob:TI-EVG:'
+    HL_PREF = 'AS-Glob:timing_devices_mapping:'
 
     LL_CODES = list(range(50)) + list(range(80, 120)) + list(range(160, 256))
     LL_EVENTS = []
