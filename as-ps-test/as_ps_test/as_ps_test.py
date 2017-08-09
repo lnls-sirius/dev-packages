3d7aa426caf4624a5479dfdf0af546457e5c9f90
ximenes <xresende@gmail.com>
2017-08-09 17:19:00 -0300

    Update as_ps_test to new service scheme

---
+++ as-ps-test/as_ps_test/as_ps_test.py
@@ -0,0 +1,63 @@
+"""PS Test IOC."""
+
+import pcaspy as _pcaspy
+import pcaspy.tools as _pcaspy_tools
+import signal as _signal
+import as_ps_test.main as _main
+import siriuspy.util as _util
+import sys as _sys
+
+INTERVAL = 0.1
+stop_event = False  # _multiprocessing.Event()
+
+
+def _stop_now(signum, frame):
+    global stop_event
+    signames = _util.get_signal_names()
+    print(signames[signum] + ' received at ' + _util.get_timestamp())
+    _sys.stdout.flush()
+    _sys.stderr.flush()
+    stop_event = True
+
+
+class _PCASDriver(_pcaspy.Driver):
+
+    def __init__(self):
+        super().__init__()
+        self.app = _main.App(self)
+
+    def read(self, reason):
+        value = self.app.read(reason)
+        if value is None:
+            return super().read(reason)
+        else:
+            return value
+
+    def write(self, reason, value):
+        return self.app.write(reason, value)
+
+
+def run():
+    """Main function."""
+    # define abort function
+    _signal.signal(_signal.SIGINT, _stop_now)
+
+    # create a new simple pcaspy server and driver to respond client's requests
+    server = _pcaspy.SimpleServer()
+    for prefix, database in _main.App.get_pvs_database().items():
+        server.createPV(prefix, database)
+    pcas_driver = _PCASDriver()
+
+    # initiate a new thread responsible for listening for client connections
+    server_thread = _pcaspy_tools.ServerThread(server)
+    server_thread.start()
+
+    # main loop
+    # while not stop_event.is_set():
+    while not stop_event:
+        pcas_driver.app.process(INTERVAL)
+
+    print('exiting...')
+    # sends stop signal to server thread
+    server_thread.stop()
+    server_thread.join()
---
+++ as-ps-test/scripts/sirius-ioc-as-ps-test.py
@@ -0,0 +1,6 @@
+#!/usr/local/bin/python-sirius -u
+"""AS PS Test IOC executable."""
+
+from as_ps_test import as_ps_test as ioc_module
+
+ioc_module.run()
