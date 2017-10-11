98fd8b97f44b59cd50d375d94ba335ad013ed533
ximenes <xresende@gmail.com>
2017-10-11 02:00:24 -0300

    Add siriuspy/ramp/comm.py
    - also generalized MagnetFactory and _MagnetPowetSupply in order to allow for creation of objects with lock=False and with use_vaca and vaca_prefix.

--- siriuspy/siriuspy/factory.py
+++ siriuspy/siriuspy/factory.py
@@ -15,17 +15,29 @@ class MagnetFactory:
     """Return a magnet object."""
 
     @staticmethod
-    def factory(maname):
+    def factory(maname, use_vaca=True, vaca_prefix=None, lock=True):
         """Return the right magnet object given its name."""
         maname = _SiriusPVName(maname)
         pulsed_discipline = _re.compile("PM")
         dipole = _re.compile("B.*")
         trim = _re.compile("SI-\d{2}\w\d:MA-(QD|QF|Q\d).*")
         if pulsed_discipline.match(maname.discipline):
-            return PulsedMagnetPowerSupply(maname, use_vaca=True)
+            return PulsedMagnetPowerSupply(maname,
+                                           use_vaca=use_vaca,
+                                           vaca_prefix=vaca_prefix,
+                                           lock=lock)
         elif dipole.match(maname.dev_type):
-            return MagnetPowerSupplyDipole(maname, use_vaca=True)
+            return MagnetPowerSupplyDipole(maname,
+                                           use_vaca=use_vaca,
+                                           vaca_prefix=vaca_prefix,
+                                           lock=lock)
         elif trim.match(maname):
-            return MagnetPowerSupplyTrim(maname, use_vaca=True)
+            return MagnetPowerSupplyTrim(maname,
+                                         use_vaca=use_vaca,
+                                         vaca_prefix=vaca_prefix,
+                                         lock=lock)
         else:
-            return MagnetPowerSupply(maname, use_vaca=True)
+            return MagnetPowerSupply(maname,
+                                     use_vaca=use_vaca,
+                                     vaca_prefix=vaca_prefix,
+                                     lock=lock)
