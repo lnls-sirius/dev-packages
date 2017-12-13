26fa83ede80da816b94c24927b3fcf8c6480e2a3
Guilherme <guilherme.prado@lnls.br>
2017-12-13 10:03:43 -0200

    Fixed psdata test

--- siriuspy/tests/v1/pwrsupply/test_psdata.py
+++ siriuspy/tests/v1/pwrsupply/test_psdata.py
@@ -22,18 +22,24 @@ from siriuspy.pwrsupply.data import PSData
 class TestPSDataProperties(unittest.TestCase):
     """Test PSData properties are set correctly."""
 
-    @mock.patch('siriuspy.pwrsupply.data._get_ps_propty_database',
-                autospec=True)
-    @mock.patch('siriuspy.pwrsupply.data._PSSearch', autospec=True)
-    def setUp(self, mock_search, mock_db):
+    def setUp(self):
         """Common setup for all test.
 
         Mock the search class PSSearch and the function to get the ps
         database.
         """
+        # Create mock
+        search_patcher = mock.patch(
+            'siriuspy.pwrsupply.data._PSSearch', autospec=True)
+        db_patcher = mock.patch(
+            'siriuspy.pwrsupply.data._get_ps_propty_database', autospec=True)
+        self.addCleanup(search_patcher.stop)
+        self.addCleanup(db_patcher.stop)
+        self.search_mock = search_patcher.start()
+        self.db_mock = db_patcher.start()
         # Fake properties
         self.properties = {
-            'psname': 'FakePS',
+            'psname': 'Fake-SI-Fam:PS-B1B2-1',
             'pstype': 'FakePSType',
             'polarity': 'FakePSPolarity',
             'magfunc': 'FakePSMagFunc',
@@ -47,43 +53,42 @@ class TestPSDataProperties(unittest.TestCase):
             'FakeField3': "string"}
 
         # Set functions return value
-        mock_search.get_psnames.return_value = [self.properties['psname']]
-        mock_search.conv_psname_2_pstype.return_value = \
+        self.search_mock.get_psnames.return_value = [self.properties['psname']]
+        self.search_mock.check_psname_ispulsed.return_value = False
+        self.search_mock.conv_psname_2_pstype.return_value = \
             self.properties['pstype']
-        mock_search.conv_pstype_2_polarity.return_value = \
+        self.search_mock.conv_pstype_2_polarity.return_value = \
             self.properties['polarity']
-        mock_search.conv_pstype_2_magfunc.return_value = \
+        self.search_mock.conv_pstype_2_magfunc.return_value = \
             self.properties['magfunc']
-        mock_search.conv_pstype_2_splims.return_value = \
+        self.search_mock.conv_pstype_2_splims.return_value = \
             self.properties['splims']
-        mock_search.get_splims_unit.return_value = \
+        self.search_mock.get_splims_unit.return_value = \
             self.properties['splims_unit']
-        mock_search.conv_psname_2_excdata.return_value = \
+        self.search_mock.conv_psname_2_excdata.return_value = \
             self.properties['excdata']
 
-        mock_db.return_value = self.db
+        self.db_mock.return_value = self.db
 
         # Object to be tested
         self.data = PSData(self.properties['psname'])
-        self.mock_search = mock_search
-        self.mock_db = mock_db
 
     def test_setup(self):
         """Test if all calls to get PSData properties are made."""
-        self.mock_search.get_psnames.assert_called()
-        self.mock_search.conv_psname_2_pstype.assert_called_with(
+        self.search_mock.get_psnames.assert_called()
+        self.search_mock.conv_psname_2_pstype.assert_called_with(
             self.properties['psname'])
-        self.mock_search.conv_pstype_2_polarity.assert_called_with(
+        self.search_mock.conv_pstype_2_polarity.assert_called_with(
             self.properties['pstype'])
-        self.mock_search.conv_pstype_2_magfunc.assert_called_with(
+        self.search_mock.conv_pstype_2_magfunc.assert_called_with(
             self.properties['pstype'])
-        self.mock_search.conv_pstype_2_splims.assert_called_with(
+        self.search_mock.conv_pstype_2_splims.assert_called_with(
             self.properties['pstype'])
-        self.mock_search.get_splims_unit.assert_called()
-        self.mock_search.conv_psname_2_excdata.assert_called_with(
+        self.search_mock.get_splims_unit.assert_called()
+        self.search_mock.conv_psname_2_excdata.assert_called_with(
             self.properties['psname'])
 
-        self.mock_db.assert_called_with(self.properties['pstype'])
+        self.db_mock.assert_called_with(self.properties['pstype'])
 
     def test_psname(self):
         """Test wether psname property is set correctly."""
@@ -123,8 +128,49 @@ class TestPSDataProperties(unittest.TestCase):
         self.assertEqual(self.data.propty_database, self.db)
 
 
+class TestPSDataDb(unittest.TestCase):
+    """Test right database is called."""
+
+    def setUp(self):
+        """Common setup for all tests."""
+        # Create mock
+        search_patcher = mock.patch(
+            'siriuspy.pwrsupply.data._PSSearch', autospec=True)
+        ps_db_patcher = mock.patch(
+            'siriuspy.pwrsupply.data._get_ps_propty_database', autospec=True)
+        pu_db_patcher = mock.patch(
+            'siriuspy.pwrsupply.data._get_pu_propty_database', autospec=True)
+        self.addCleanup(search_patcher.stop)
+        self.addCleanup(ps_db_patcher.stop)
+        self.addCleanup(pu_db_patcher.stop)
+        self.search_mock = search_patcher.start()
+        self.ps_db_mock = ps_db_patcher.start()
+        self.pu_db_mock = pu_db_patcher.start()
+
+        self.psname = "Fake-SI-Fam:PS-QDA"
+        self.puname = "Fake-SI-Fam:PU-QDA"
+        self.pstype = "FakeType"
+
+        self.search_mock.get_psnames.return_value = [self.psname, self.puname]
+        self.search_mock.conv_psname_2_pstype.return_value = self.pstype
+
+    def test_ps_db(self):
+        """Test ps db is called when a ps is passed."""
+        self.search_mock.check_psname_ispulsed.return_value = False
+        data = PSData(self.psname)
+        self.pu_db_mock.assert_not_called()
+        self.ps_db_mock.assert_called_once_with(pstype=self.pstype)
+
+    def test_pu_db(self):
+        """Test pu db is called when a pu is passed."""
+        self.search_mock.check_psname_ispulsed.return_value = True
+        data = PSData(self.psname)
+        self.pu_db_mock.assert_called_once_with(pstype=self.pstype)
+        self.ps_db_mock.assert_not_called()
+
+
 class TestPSDataException(unittest.TestCase):
-    """Test PSDAta failes when PS is not found."""
+    """Test PSDAta fails when PS is not found."""
 
     @mock.patch('siriuspy.pwrsupply.data._PSSearch', autospec=True)
     def test_init(self, mock_search):
