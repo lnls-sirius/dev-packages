1703aa98f42ddff34c63a51680873d8c9cad134b
ximenes <xresende@gmail.com>
2018-01-08 16:17:00 -0200

    Fix check of other as_ps_test instances

--- as-ps-test/as_ps_test/as_ps_test.py
+++ as-ps-test/as_ps_test/as_ps_test.py
@@ -1,11 +1,12 @@
 """PS Test IOC."""
 
+import sys as _sys
+import signal as _signal
 import pcaspy as _pcaspy
 import pcaspy.tools as _pcaspy_tools
-import signal as _signal
-import as_ps_test.main as _main
 import siriuspy.util as _util
-import sys as _sys
+import as_ps_test.main as _main
+import as_ps_test.pvs as _pvs
 
 INTERVAL = 0.1
 stop_event = False  # _multiprocessing.Event()
@@ -45,6 +46,15 @@ def run():
 
     # define IOC and initializes it
     _main.App.init_class()
+
+    # check if IOC is already running
+    p = next(iter(_main.App.pvs_database.keys()))
+    pvname = _pvs._PREFIX + next(iter(_main.App.pvs_database[p].keys()))
+    running = _util.check_pv_online(
+        pvname=pvname, use_prefix=False, timeout=0.5)
+    if running:
+        print('Another PS IOC is already running!')
+        return
 
     # create a new simple pcaspy server and driver to respond client's requests
     server = _pcaspy.SimpleServer()
