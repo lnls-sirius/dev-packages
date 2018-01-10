b41729a447b9783579f1f4f0abe16841ea42efd2
Guilherme <guilherme.prado@lnls.br>
2018-01-10 16:33:44 -0200

    Implemented epics classes in pwrsupply model

--- siriuspy/siriuspy/pwrsupply/model.py
+++ siriuspy/siriuspy/pwrsupply/model.py
@@ -1,12 +1,17 @@
 """Define a Pulsed Power Supply class to handle IOC connections."""
+import re as _re
 
-# PowerSupply
+from epics import PV as _PV
+
+from siriuspy.epics.computed_pv import ComputedPV as _ComputedPV
 from siriuspy.pwrsupply.data import PSData as _PSData
-# Magnet
 from siriuspy.magnet.data import MAData as _MAData
+from siriuspy.magnet import util as _mutil
 from siriuspy.namesys import SiriusPVName as _SiriusPVName
 from siriuspy.envars import vaca_prefix as _VACA_PREFIX
 from . import controller as _controller
+from siriuspy.factory import NormalizerFactory as _NormalizerFactory
+from siriuspy.pwrsupply import sync as _sync
 
 
 class PSComm:
@@ -36,20 +41,201 @@ class PowerSupply(PSComm):
 class PSEpics(PSComm):
     """Power supply with Epics communication."""
 
-    def __init__(self, psname):
-        """Init method."""
-        pass
+    def __init__(self, psname, fields=None, use_vaca=True):
+        """Create epics PVs and expose them through public controller API."""
+        # Attributes use build a full PV address
+        self._psname = psname
+        # self._sort_fields()
+        if use_vaca:
+            self._prefix = _VACA_PREFIX
+        else:
+            self._prefix = ''
+        # Get pv db
+        self._base_db = self._get_base_db()
+        # Get fields, if none is passed they'll will be retrieved from the db
+        if fields is None:
+            self._fields = self._get_fields()
+        else:
+            self._fields = fields
+        # Holds PVs objects
+        self._pvs = dict()
+        self._create_pvs()
+
+    # Public PSComm interface API
+    def read(self, field):
+        """Read a field value."""
+        if self._pvs[field].connected:
+            return self._pvs[field].get()
+        else:
+            print("Not connected")
+            return None
+
+    def write(self, field, value):
+        """Write a value to a field."""
+        if self._pvs[field].connected:
+            return self._pvs[field].put(value)
+        else:
+            print("Not connected")
+            return None
+
+    def add_callback(self, func):
+        """Add callback to field."""
+        if not callable(func):
+            raise ValueError("Tried to set non callable as a callback")
+        else:
+            for pv in self._pvs.values():
+                pv.add_callback(func)
+
+    def get_database(self, prefix=""):
+        """Fill base DB with values and limits read from PVs.
+
+        Optionally add a prefix to the dict keys.
+        """
+        db = self._fill_database()
+
+        if prefix:
+            prefixed_db = {}
+            for key, value in db.items():
+                prefixed_db[prefix + ":" + key] = value
+            return prefixed_db
+        else:
+            return db
+
+    # Private
+    def _create_pvs(self):
+        # Normally create normal PV objects
+        # In case more than one source is supplied creates a SyncPV
+        # In case the device is a Magnet with a normalized force being supplied
+        # as one of the fields, a NormalizedPV is created
+        for field in self._fields:
+            self._pvs[field] = self._create_pv(field)
+            # return _PV(self._prefix + self.psname + ":" + field)
+
+    def _create_pv(self, field):
+        return _PV(self._prefix + self._psname + ":" + field)
+
+    def _get_base_db(self):
+        return _PSData(self._psname).propty_database
+
+    def _get_fields(self):
+        return self._base_db.keys()
+
+    def _fill_database(self):
+        db = dict()
+        db.update(self._base_db)
+        for field in db:
+            value = self.read(field)
+            if value is not None:
+                db[field]["value"] = value
+
+        return db
 
 
 class MAEpics(PSComm):
     """Magnet power supply with Epics communication."""
 
-    def __init__(self, psnames):
-        """Init method."""
-        pass
+    _is_strength = _re.compile('(Energy|KL|SL|Kick).+$')
+    _is_multi_ps = _re.compile('(SI|BO)-\w{2,4}:MA-B.*$')
+
+    def __init__(self, maname, lock=True, **kwargs):
+        """Create epics PVs and expose them through public controller API."""
+        # Attributes use build a full PV address
+        self._maname = _SiriusPVName(maname)
+        self._madata = _MAData(maname)
+        super().__init__(
+            psname=self._maname.replace("MA", "PS").replace("PM", "PU"),
+            **kwargs)
+
+    # Virtual Methods
+    def _create_pvs(self):
+        self._sort_fields()
+        super()._create_pvs()
+
+    def _create_pv(self, field):
+        # Build either a real or computed PV
+        if MAEpics._is_strength.match(field):  # NormalizedPV
+            pvname = self._prefix + self._maname + ":" + field
+            str_obj = self._get_normalizer(self._maname)
+            pvs = self._get_str_pv(field)
+            return _ComputedPV(pvname, str_obj, *pvs)
+        else:
+            if len(self._psnames()) > 1:  # SyncPV
+                # Sync object used to sync pvs
+                sync = self._get_sync_obj(field)
+                # Real PVs(names) supplied to ComputedPV
+                pvs = [self._prefix + device_name + ":" + field
+                       for device_name in self._psnames()]
+                # Name of the ComputedPV
+                pvname = self._psname + ":" + field
+                # Create a virtual PV (ComputedPV)
+                return _ComputedPV(pvname, sync, *pvs)
+            else:
+                return super()._create_pv(field)
+
+    def _get_base_db(self):
+        return self._madata.get_database(self._madata.psnames[0])
+
+    # Class methods
+    def _get_normalizer(self, device_name):
+        # Return Normalizer object
+        return _NormalizerFactory.factory(device_name)
+
+    def _get_sync_obj(self, field):
+        # Return SyncWrite or SyncRead object
+        if "SP" in field or "Sel" in field or "Cmd" in field:
+            return _sync.SyncWrite()
+        else:
+            return _sync.SyncRead()
+
+    def _get_str_pv(self, field):
+        ma_class = _mutil.magnet_class(self._maname)
+        if 'dipole' == ma_class:
+            return [self._pvs[field.replace('Energy', 'Current')], ]
+        elif 'pulsed' == ma_class:
+            dipole_name = _mutil.get_section_dipole_name(self._maname)
+            dipole = self._prefix + dipole_name
+            return [self._pvs[field.replace('Kick', 'Voltage')],
+                    dipole + ":" + field.replace('Kick', 'Current')]
+        elif 'trim' == ma_class:
+            dipole_name = _mutil.get_section_dipole_name(self._maname)
+            dipole = self._prefix + dipole_name
+            fam_name = _mutil.get_magnet_family_name(self._maname)
+            fam = self._prefix + fam_name
+            field = field.replace('KL', 'Current')
+            return [self._pvs[field],
+                    dipole + ':' + field,
+                    fam + ':' + field]
+        else:
+            dipole_name = _mutil.get_section_dipole_name(self._maname)
+            dipole = self._prefix + dipole_name
+            field = field.replace('KL', 'Current').replace('SL', 'Current')\
+                .replace('Kick', 'Current')
+            return [self._pvs[field],
+                    dipole + ":" + field]
+
+    def _psnames(self):
+        ma_class = _mutil.magnet_class(self._maname)
+        if 'dipole' == ma_class:
+            if 'SI' == self._maname.sec:
+                return ['SI-Fam:PS-B1B2-1', 'SI-Fam:PS-B1B2-2']
+            elif 'BO' == self._maname.sec:
+                return ['BO-Fam:PS-B-1', 'BO-Fam:PS-B-2']
+        elif 'pulsed' == ma_class:
+            return [self._maname.replace(':PM', ':PU')]
+
+        return [self._maname.replace(':MA', ':PS')]
+
+    # def _sort_fields(self):
+    #     fields = []
+    #     for field in self._fields:
+    #         if not self._is_strength.match(field):
+    #             fields.insert(0, field)
+    #         else:
+    #             fields.append(field)
+    #
+    #     self.fields = fields
 
 
-#
 # class PSEpicsController(Controller):
 #     """Create real PVs to control PS attributes."""
 #
