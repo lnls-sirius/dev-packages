b588eb16135fb97fc0fc194fa63c71566fac2ede
Fernando <fernandohds564@gmail.com>
2018-04-11 12:24:17 -0300

    Actually the correct name is Const, not Consts.

--- ll_classes.py
+++ ll_classes.py
@@ -12,10 +12,10 @@ from siriuspy.namesys import SiriusPVName as _PVName
 from siriuspy.csdevice import timesys as _cstime
 from siriuspy.search import LLTimeSearch as _LLTimeSearch
 
-_RFFREQ = _cstime.Consts.RF_FREQUENCY
-_RFDIV = _cstime.Consts.RF_DIVISION
-_ACFREQ = _cstime.Consts.AC_FREQUENCY
-_FDEL = _cstime.Consts.FINE_DELAY
+_RFFREQ = _cstime.Const.RF_FREQUENCY
+_RFDIV = _cstime.Const.RF_DIVISION
+_ACFREQ = _cstime.Const.AC_FREQUENCY
+_FDEL = _cstime.Const.FINE_DELAY
 
 INTERVAL = 0.1
 _DELAY_UNIT_CONV = 1e-6
