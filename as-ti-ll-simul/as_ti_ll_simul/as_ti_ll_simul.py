70a1cce0ef0f0c00a8720cfdb5666a57a1b8d953
Fernando <fernandohds564@gmail.com>
2018-04-12 17:49:58 -0300

    Insert check of other running instances of Timing IOCs before initializing a new one.

--- as-ti-ll-simul/as_ti_ll_simul/as_ti_ll_simul.py
+++ as-ti-ll-simul/as_ti_ll_simul/as_ti_ll_simul.py
@@ -138,7 +138,15 @@ def run(debug=False):
     # Creates App object
     ioc_name = 'AS-TI-LL-SIMUL'
     db = App.get_database()
-    db.update({fname+'Version-Cte': {'type': 'string', 'value': __version__}})
+    db[ioc_name+':Version-Cte'] = {'type': 'string', 'value': __version__}
+
+    # check if IOC is already running
+    running = _util.check_pv_online(
+        pvname=PREFIX + list(db.keys())[0], use_prefix=False, timeout=0.5)
+    if running:
+        _log.error('Another ' + ioc_name + ' is already running!')
+        return
+
     _log.info('Generating database file.')
     _util.save_ioc_pv_list(ioc_name.lower(), PREFIX, db)
     _log.info('File generated with {0:d} pvs.'.format(len(db)))
