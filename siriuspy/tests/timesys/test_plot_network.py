3bb197a5f7b9cd5e68a0243b2f57ff13d921fa81
Fernando <fernandohds564@gmail.com>
2018-04-12 09:34:49 -0300

    Implement tests relative to commit aed3aa1.

---
+++ siriuspy/tests/timesys/test_plot_network.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python-sirius
+
+"""Unittest module for ll_time_search.py."""
+
+import unittest
+import re
+# from unittest import mock
+
+from siriuspy import util
+from siriuspy.timesys import plot_network
+
+mock_flag = True
+
+public_interface = ('PlotNetwork', )
+
+
+class TestModule(unittest.TestCase):
+    """Test module interface."""
+
+    def test_public_interface(self):
+        """Test module's public interface."""
+        valid = util.check_public_interface_namespace(
+                                            plot_network, public_interface)
+        self.assertTrue(valid)
+
+
+class TestPlotNetwork(unittest.TestCase):
+    """Test PlotNetwork class."""
+
+    public_interface = ('plot', )
+
+    def test_public_interface(self):
+        """Test class public interface."""
+        valid = util.check_public_interface_namespace(
+            plot_network.PlotNetwork, TestPlotNetwork.public_interface)
+        self.assertTrue(valid)
+
+    def test_plot(self):
+        """Test plot."""
+        # TODO: implement test!
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
