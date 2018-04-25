443e1c0622baf66256d7056bb33402b62290dfd9
Ana <ana.clara@lnls.br>
2018-04-25 16:57:04 -0300

    Migrate currinfo pvs databases to siriuspy.csdevice.

--- as-ap-currinfo/as_ap_currinfo/current/pvs.py
+++ as-ap-currinfo/as_ap_currinfo/current/pvs.py
@@ -2,6 +2,7 @@
 
 from siriuspy.envars import vaca_prefix as _vaca_prefix
 from siriuspy import util as _util
+from siriuspy.csdevice.currinfo import get_current_database as _get_database
 
 
 _COMMIT_HASH = _util.get_last_commit_hash()
@@ -21,49 +22,28 @@ def select_ioc(acc):
 
 def get_pvs_section():
     """Return Soft IOC transport line."""
-    global _ACC
     return _ACC
 
 
 def get_pvs_vaca_prefix():
     """Return Soft IOC vaca prefix."""
-    global _PREFIX_VACA
     return _PREFIX_VACA
 
 
 def get_pvs_prefix():
     """Return Soft IOC prefix."""
-    global _PREFIX
     return _PREFIX
 
 
 def get_pvs_database():
     """Return IOC database."""
-    global _ACC, _COMMIT_HASH
-
-    pvs_database = {
-        'Version-Cte':     {'type': 'string', 'value': _COMMIT_HASH},
-        'Current-Mon':     {'type': 'float', 'value': 0.0, 'prec': 3,
-                            'unit': 'mA'},
-        'StoredEBeam-Mon': {'type': 'enum', 'value': 0,
-                            'enums': ['False', 'True']},
-    }
-
-    if _ACC == 'SI':
-        pvs_database['DCCT-Sel'] = {'type': 'enum', 'value': 0,
-                                    'enums': ['Avg', '13C4', '14C4']}
-        pvs_database['DCCT-Sts'] = {'type': 'enum', 'value': 0,
-                                    'enums': ['Avg', '13C4', '14C4']}
-        pvs_database['DCCTFltCheck-Sel'] = {'type': 'enum', 'value': 1,
-                                            'enums': ['Off', 'On']}
-        pvs_database['DCCTFltCheck-Sts'] = {'type': 'enum', 'value': 1,
-                                            'enums': ['Off', 'On']}
+    pvs_database = _get_database(_ACC)
+    pvs_database['Version-Cte']['value'] = _COMMIT_HASH
     return pvs_database
 
 
 def print_banner_and_save_pv_list():
     """Print Soft IOC banner."""
-    global _COMMIT_HASH, _PREFIX_VACA, _ACC, _DEVICE, _PREFIX
     _util.print_ioc_banner(
         ioc_name=_ACC.lower()+'-ap-currinfo-current',
         db=get_pvs_database(),
--- as-ap-currinfo/as_ap_currinfo/lifetime/pvs.py
+++ as-ap-currinfo/as_ap_currinfo/lifetime/pvs.py
@@ -2,6 +2,7 @@
 
 from siriuspy.envars import vaca_prefix as _vaca_prefix
 from siriuspy import util as _util
+from siriuspy.csdevice.currinfo import get_lifetime_database as _get_database
 
 
 _COMMIT_HASH = _util.get_last_commit_hash()
@@ -21,50 +22,28 @@ def select_ioc(acc):
 
 def get_pvs_section():
     """Return Soft IOC transport line."""
-    global _ACC
     return _ACC
 
 
 def get_pvs_vaca_prefix():
     """Return Soft IOC vaca prefix."""
-    global _PREFIX_VACA
     return _PREFIX_VACA
 
 
 def get_pvs_prefix():
     """Return Soft IOC prefix."""
-    global _PREFIX
     return _PREFIX
 
 
 def get_pvs_database():
     """Return IOC database."""
-    pvs_database = {
-        'Version-Cte':     {'type': 'string', 'value': _COMMIT_HASH},
-        'Lifetime-Mon':    {'type': 'float', 'value': 0.0, 'prec': 0,
-                            'unit': 's'},
-        'BuffSizeMax-SP':  {'type': 'int', 'lolim': 0, 'hilim': 360000,
-                            'value': 0},
-        'BuffSizeMax-RB':  {'type': 'int', 'value': 0},
-        'BuffSize-Mon':	   {'type': 'int', 'value': 0},
-        'SplIntvl-SP':     {'type': 'int', 'unit': 's',  'lolim': 0,
-                            'hilim': 360000, 'low': 0, 'high': 3600,
-                            'lolo': 0, 'hihi': 3600, 'value': 10},
-        'SplIntvl-RB':	   {'type': 'int', 'value': 10, 'unit': 's'},
-        'BuffRst-Cmd':     {'type': 'int', 'value': 0},
-        'BuffAutoRst-Sel': {'type': 'enum', 'value': 1,
-                            'enums': ['PVsTrig', 'DCurrCheck', 'Off']},
-        'BuffAutoRst-Sts': {'type': 'enum', 'value': 1,
-                            'enums': ['PVsTrig', 'DCurrCheck', 'Off']},
-        'DCurrFactor-Cte': {'type': 'float', 'value': 0.003, 'prec': 2,
-                            'unit': 'mA'}
-    }
+    pvs_database = _get_database()
+    pvs_database['Version-Cte']['value'] = _COMMIT_HASH
     return pvs_database
 
 
 def print_banner_and_save_pv_list():
     """Print Soft IOC banner."""
-    global _COMMIT_HASH, _PREFIX_VACA, _ACC, _DEVICE, _PREFIX
     _util.print_ioc_banner(
         ioc_name=_ACC.lower()+'-ap-currinfo-lifetime',
         db=get_pvs_database(),
