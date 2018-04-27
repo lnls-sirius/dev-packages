793d9f0d1cfa55272be781bf6d243a6f18083e65
Ana <ana.clara@lnls.br>
2018-04-27 09:38:29 -0300

    Migrate opticscorr pvs databases to siriuspy.csdevice.

--- as-ap-opticscorr/as_ap_opticscorr/chrom/pvs.py
+++ as-ap-opticscorr/as_ap_opticscorr/chrom/pvs.py
@@ -2,6 +2,9 @@
 
 from siriuspy.envars import vaca_prefix as _vaca_prefix
 from siriuspy import util as _util
+from siriuspy.csdevice.opticscorr import (
+    Const as _Const,
+    get_chrom_database as _get_database)
 
 
 _COMMIT_HASH = _util.get_last_commit_hash()
@@ -19,98 +22,40 @@ def select_ioc(acc):
     _DEVICE = _ACC + '-Glob:AP-ChromCorr:'
     _PREFIX = _PREFIX_VACA + _DEVICE
     if _ACC == 'BO':
-        _SFAMS = ['SF', 'SD']
+        _SFAMS = _Const.BO_SFAMS_CHROMCORR
     elif _ACC == 'SI':
-        _SFAMS = ['SFA1', 'SFA2', 'SDA1', 'SDA2', 'SDA3',
-                  'SFB1', 'SFB2', 'SDB1', 'SDB2', 'SDB3',
-                  'SFP1', 'SFP2', 'SDP1', 'SDP2', 'SDP3']
+        _SFAMS = _Const.SI_SFAMS_CHROMCORR
 
 
 def get_pvs_section():
     """Return Soft IOC section/accelerator."""
-    global _ACC
     return _ACC
 
 
 def get_pvs_vaca_prefix():
     """Return Soft IOC vaca prefix."""
-    global _PREFIX_VACA
     return _PREFIX_VACA
 
 
 def get_pvs_prefix():
     """Return Soft IOC prefix."""
-    global _PREFIX
     return _PREFIX
 
 
 def get_corr_fams():
     """Return list of magnet families used on correction."""
-    global _SFAMS
     return _SFAMS
 
 
 def get_pvs_database():
     """Return IOC database."""
-    global _SFAMS, _COMMIT_HASH
-    corrmat_size = len(_SFAMS)*2
-
-    pvs_database = {
-        'Version-Cte':          {'type': 'string', 'value': _COMMIT_HASH,
-                                 'scan': 0.25},
-
-        'Log-Mon':              {'type': 'string', 'value': 'Starting...'},
-
-        'ChromX-SP':            {'type': 'float', 'value': 0, 'prec': 6,
-                                 'hilim': 10, 'lolim': -10, 'high': 10,
-                                 'low': -10, 'hihi': 10, 'lolo': -10},
-        'ChromX-RB':            {'type': 'float', 'value': 0, 'prec': 6},
-        'ChromY-SP':            {'type': 'float', 'value': 0, 'prec': 6,
-                                 'hilim': 10, 'lolim': -10, 'high': 10,
-                                 'low': -10, 'hihi': 10, 'lolo': -10},
-        'ChromY-RB':            {'type': 'float', 'value': 0, 'prec': 6},
-
-        'ApplyCorr-Cmd':        {'type': 'int', 'value': 0},
-
-        'ConfigName-SP':        {'type': 'string', 'value': ''},
-        'ConfigName-RB':        {'type': 'string', 'value': ''},
-        'RespMat-Mon':          {'type': 'float', 'count': corrmat_size,
-                                 'value': corrmat_size*[0], 'prec': 6, 'unit':
-                                 'Chrom x SFams (Matrix of add method)'},
-        'NominalChrom-Mon':     {'type': 'float', 'count': 2, 'value': 2*[0],
-                                 'prec': 6},
-        'NominalSL-Mon':        {'type': 'float', 'count': len(_SFAMS),
-                                 'value': len(_SFAMS)*[0], 'prec': 6},
-
-        'ConfigMA-Cmd':         {'type': 'int', 'value': 0},
-
-        'Status-Mon':           {'type': 'int', 'value': 0x1f},
-        'Status-Cte':           {'type': 'string', 'count': 5, 'value':
-                                 ('MA Connection', 'MA PwrState', 'MA OpMode',
-                                  'MA CtrlMode', 'Timing Config')},
-    }
-
-    for fam in _SFAMS:
-        pvs_database['SL' + fam + '-Mon'] = {
-            'type': 'float', 'value': 0, 'prec': 4, 'unit': '1/m^2',
-            'lolim': 0, 'hilim': 0, 'low': 0, 'high': 0, 'lolo': 0, 'hihi': 0}
-
-    if _ACC == 'SI':
-        pvs_database['CorrMeth-Sel'] = {'type': 'enum', 'value': 0, 'enums':
-                                        ['Proportional', 'Additional']}
-        pvs_database['CorrMeth-Sts'] = {'type': 'enum', 'value': 0, 'enums':
-                                        ['Proportional', 'Additional']}
-        pvs_database['SyncCorr-Sel'] = {'type': 'enum', 'value': 0,
-                                        'enums': ['Off', 'On']}
-        pvs_database['SyncCorr-Sts'] = {'type': 'enum', 'value': 0,
-                                        'enums': ['Off', 'On']}
-        pvs_database['ConfigTiming-Cmd'] = {'type': 'int', 'value': 0}
+    pvs_database = _get_database(_ACC)
+    pvs_database['Version-Cte']['value'] = _COMMIT_HASH
     return pvs_database
 
 
 def print_banner_and_save_pv_list():
     """Print Soft IOC banner."""
-    global _COMMIT_HASH, _PREFIX_VACA, _ACC, _DEVICE, _PREFIX
     _util.print_ioc_banner(
         ioc_name=_ACC+'-AP-ChromCorr',
         db=get_pvs_database(),
--- as-ap-opticscorr/as_ap_opticscorr/tune/pvs.py
+++ as-ap-opticscorr/as_ap_opticscorr/tune/pvs.py
@@ -2,6 +2,9 @@
 
 from siriuspy.envars import vaca_prefix as _vaca_prefix
 from siriuspy import util as _util
+from siriuspy.csdevice.opticscorr import (
+    Const as _Const,
+    get_tune_database as _get_database)
 
 
 _COMMIT_HASH = _util.get_last_commit_hash()
@@ -19,112 +22,40 @@ def select_ioc(acc):
     _DEVICE = _ACC + '-Glob:AP-TuneCorr:'
     _PREFIX = _PREFIX_VACA + _DEVICE
     if _ACC == 'BO':
-        _QFAMS = ['QF', 'QD']
+        _QFAMS = _Const.BO_QFAMS_TUNECORR
     elif _ACC == 'SI':
-        _QFAMS = ['QFA', 'QFB', 'QFP',
-                  'QDA', 'QDB1', 'QDB2', 'QDP1', 'QDP2']
+        _QFAMS = _Const.SI_QFAMS_TUNECORR
 
 
 def get_pvs_section():
     """Return Soft IOC section/accelerator."""
-    global _ACC
     return _ACC
 
 
 def get_pvs_vaca_prefix():
     """Return Soft IOC vaca prefix."""
-    global _PREFIX_VACA
     return _PREFIX_VACA
 
 
 def get_pvs_prefix():
     """Return Soft IOC prefix."""
-    global _PREFIX
     return _PREFIX
 
 
 def get_corr_fams():
     """Return list of magnet families used on correction."""
-    global _QFAMS
     return _QFAMS
 
 
 def get_pvs_database():
     """Return IOC database."""
-    global _COMMIT_HASH, _ACC, _QFAMS
-    corrmat_size = len(_QFAMS)*2
-
-    pvs_database = {
-        'Version-Cte':          {'type': 'string', 'value': _COMMIT_HASH},
-
-        'Log-Mon':              {'type': 'string', 'value': 'Starting...'},
-
-        'DeltaTuneX-SP':        {'type': 'float', 'value': 0, 'prec': 6,
-                                 'hilim': 1, 'lolim': -1, 'high': 1, 'low': -1,
-                                 'hihi': 1, 'lolo': -1},
-        'DeltaTuneX-RB':        {'type': 'float', 'value': 0, 'prec': 6,
-                                 'hilim': 1, 'lolim': -1, 'high': 1, 'low': -1,
-                                 'hihi': 1, 'lolo': -1},
-        'DeltaTuneY-SP':        {'type': 'float', 'value': 0, 'prec': 6,
-                                 'hilim': 1, 'lolim': -1, 'high': 1, 'low': -1,
-                                 'hihi': 1, 'lolo': -1},
-        'DeltaTuneY-RB':        {'type': 'float', 'value': 0, 'prec': 6,
-                                 'hilim': 1, 'lolim': -1, 'high': 1, 'low': -1,
-                                 'hihi': 1, 'lolo': -1},
-
-        'ApplyCorr-Cmd':        {'type': 'int', 'value': 0},
-
-        'ConfigName-SP':        {'type': 'string', 'value': ''},
-        'ConfigName-RB':        {'type': 'string', 'value': ''},
-        'RespMat-Mon':          {'type': 'float', 'count': corrmat_size,
-                                 'value': corrmat_size*[0], 'prec': 6, 'unit':
-                                 'Tune x QFams (Nominal Response Matrix)'},
-        'NominalKL-Mon':        {'type': 'float', 'count': len(_QFAMS),
-                                 'value': len(_QFAMS)*[0], 'prec': 6},
-
-        'CorrFactor-SP':        {'type': 'float', 'value': 0, 'unit': '%',
-                                 'prec': 1, 'hilim': 1000, 'lolim': -1000,
-                                 'high': 1000, 'low': -1000, 'hihi': 1000,
-                                 'lolo': -1000},
-        'CorrFactor-RB':        {'type': 'float', 'value': 0, 'unit': '%',
-                                 'prec': 1, 'hilim': 1000, 'lolim': -1000,
-                                 'high': 1000, 'low': -1000, 'hihi': 1000,
-                                 'lolo': -1000},
-
-        'ConfigMA-Cmd':         {'type': 'int', 'value': 0},
-
-        'SetNewRefKL-Cmd':      {'type': 'int', 'value': 0},
-
-        'Status-Mon':           {'type': 'int', 'value': 0x1f},
-        'Status-Cte':           {'type': 'string', 'count': 5, 'value':
-                                 ('MA Connection', 'MA PwrState', 'MA OpMode',
-                                  'MA CtrlMode', 'Timing Config')},
-    }
-
-    for fam in _QFAMS:
-        pvs_database['RefKL' + fam + '-Mon'] = {'type': 'float', 'value': 0,
-                                                'prec': 6, 'unit': '1/m'}
-
-        pvs_database['DeltaKL' + fam + '-Mon'] = {
-            'type': 'float', 'value': 0, 'prec': 6, 'unit': '1/m',
-            'lolim': 0, 'hilim': 0, 'low': 0, 'high': 0, 'lolo': 0, 'hihi': 0}
-
-    if _ACC == 'SI':
-        pvs_database['CorrMeth-Sel'] = {'type': 'enum', 'value': 0, 'enums':
-                                        ['Proportional', 'Additional']}
-        pvs_database['CorrMeth-Sts'] = {'type': 'enum', 'value': 0, 'enums':
-                                        ['Proportional', 'Additional']}
-        pvs_database['SyncCorr-Sel'] = {'type': 'enum', 'value': 0,
-                                        'enums': ['Off', 'On']}
-        pvs_database['SyncCorr-Sts'] = {'type': 'enum', 'value': 0,
-                                        'enums': ['Off', 'On']}
-        pvs_database['ConfigTiming-Cmd'] = {'type': 'int', 'value': 0}
+    pvs_database = _get_database(_ACC)
+    pvs_database['Version-Cte']['value'] = _COMMIT_HASH
     return pvs_database
 
 
 def print_banner_and_save_pv_list():
     """Print Soft IOC banner."""
-    global _COMMIT_HASH, _PREFIX_VACA, _ACC, _PREFIX, _DEVICE
     _util.print_ioc_banner(
         ioc_name=_ACC+'-AP-TuneCorr',
         db=get_pvs_database(),
