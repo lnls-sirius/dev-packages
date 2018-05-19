07d33c17bfdb05c06843b737753dec40e82964ed
ximenes <xresende@gmail.com>
2018-05-19 20:18:44 -0300

    Add exception to ramp subpackage

---
+++ siriuspy/siriuspy/ramp/exceptions.py
@@ -0,0 +1,37 @@
+"""Module with ramp exceptions."""
+
+
+class RampError(Exception):
+    """Gerenarl exception in Ramp."""
+
+    pass
+
+
+class RampCouldNotConnect(RampError):
+    """Could not connect with server."""
+
+    pass
+
+
+class RampConfigError(RampError):
+    """General exception for config manipulations."""
+
+    pass
+
+
+class RampConfigNameNotDefined(RampConfigError):
+    """Configuration name not defined."""
+
+    pass
+
+
+class RampConfigFormatError(RampConfigError):
+    """Configuration value with inconsistent format."""
+
+    pass
+
+
+class RampConfigNotFound(RampConfigError):
+    """Configuration not found in server."""
+
+    pass
