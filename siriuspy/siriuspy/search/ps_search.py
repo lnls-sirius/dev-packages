b8f4cc9a47c06b7f13f9d97e33c5cd03da0847f7
ximenes <xresende@gmail.com>
2018-06-20 08:31:05 -0300

    Fix ps_search to deal with more psmodels

--- siriuspy/siriuspy/search/ps_search.py
+++ siriuspy/siriuspy/search/ps_search.py
@@ -242,7 +242,7 @@ class PSSearch:
         """Return SP limits unit."""
         if PSSearch._pstype_2_splims_dict is None:
             PSSearch._reload_pstype_2_splims_dict()
-        if psmodel in ('FBP', 'FAC', 'FAP', 'FAC_2S', 'FAC_2P4S',
+        if psmodel in ('FBP', 'FAC', 'FAP', 'FAC_2S_DCDC', 'FAC_2P4S_DCDC',
                        'FAC_2P4S_DCDC'):
             return PSSearch._splims_ps_unit
         else:
