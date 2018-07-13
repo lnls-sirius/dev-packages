43dabc1b6638ef99e0f0a30739f8f702e4f2ccc4
Fernando <fernandohds564@gmail.com>
2018-07-13 13:19:06 -0300

    DEV: Add method get_namedtuple to csdevice/const.py module.

--- siriuspy/siriuspy/csdevice/const.py
+++ siriuspy/siriuspy/csdevice/const.py
@@ -3,6 +3,23 @@
 from collections import namedtuple as _namedtuple
 
 
+def get_namedtuple(name, field_names, values=None):
+    """Return an instance of a namedtuple Class.
+
+    Inputs:
+        - name:  Defines the name of the Class (str).
+        - field_names:  Defines the field names of the Class (iterable).
+        - values (optional): Defines field values . If not given, the value of
+            each field will be its index in 'field_names' (iterable).
+
+    Raises ValueError if at least one of the field names are invalid.
+    Raises TypeError when len(values) != len(field_names)
+    """
+    if values is None:
+        values = range(len(field_names))
+    return _namedtuple(name, field_names)(*values)
+
+
 class Const:
     """Const class defining power supply constants."""
 
