f7fdfd821a5f5da0bf1c755c8d4ab9ee2c188374
Fernando <fernandohds564@gmail.com>
2018-07-13 13:21:55 -0300

    MNT: Fix misuse of collections.namedtuple in csdevice.const.Const class.

--- siriuspy/siriuspy/csdevice/const.py
+++ siriuspy/siriuspy/csdevice/const.py
@@ -26,24 +26,7 @@ class Const:
     @staticmethod
     def add_field(field_name, values):
         """Add field with constant values in const namespace."""
-        for i in range(len(values)):
-            if not Const._checked_ok(values[i]):
-                raise ValueError('Invalid field value "{}"'.format(values[i]))
-            Const._add_const(field_name, values[i], i)
-
-    @staticmethod
-    def _add_const(group, const, i):
-        if not hasattr(Const, group):
-            setattr(Const, group, _namedtuple(group, ''))
-        obj = getattr(Const, group)
-        setattr(obj, const, i)
-
-    @staticmethod
-    def _checked_ok(value):
-        if not isinstance(value, str):
-            return False
-        if not value.replace('_','').isalnum():
-            return False
-        if value[0].isnumeric():
-            return False
-        return True
+        if hasattr(Const, field_name):
+            raise NameError('Field "{}" already exists.'.format(field_name))
+        const = get_namedtuple(field_name, values)
+        setattr(Const, field_name, const)
