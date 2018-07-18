39ca388af8feb077cadb0d802b2806c124c1b0bc
Fernando <fernandohds564@gmail.com>
2018-07-18 09:12:36 -0300

    API: Organize csdevice.bpms module for standartization.

--- siriuspy/siriuspy/csdevice/bpms.py
+++ siriuspy/siriuspy/csdevice/bpms.py
@@ -1,223 +1,206 @@
-from copy import deepcopy as _copy
+from copy import deepcopy as _dcopy
 import numpy as _np
+from siriuspy.csdevice.const import get_namedtuple as _get_namedtuple
 
-op_modes = ['MultiBunch', 'SinglePass']
-data_names = {
-    'GEN': ['A', 'B', 'C', 'D', 'Q', 'Sum', 'X', 'Y'],
-    'PM': ['A', 'B', 'C', 'D', 'Q', 'Sum', 'X', 'Y'],
-    'SP': ['A', 'B', 'C', 'D'],
-}
+OpModes = _get_namedtuple('OpModes', ('MultiBunch', 'SinglePass'))
+Polarity = _get_namedtuple('Polarity', ('positive', 'negative'))
+EnblTyp = _get_namedtuple('EnblTyp', ('Disable', 'Enable'))
+ConnTyp = _get_namedtuple('ConnTyp', ('Disconnect', 'Connect'))
+AcqRepeat = _get_namedtuple('AcqRepeat', ('normal', 'repetitive'))
+AcqEvents = _get_namedtuple('AcqEvents', ('start', 'stop', 'abort', 'reset'))
+AcqDataTyp = _get_namedtuple(
+                    'AcqDataTyp', ('A', 'B', 'C', 'D', 'X', 'Y', 'Sum', 'Q'))
+AcqTyp = _get_namedtuple(
+            'AcqTyp', ('adc', 'adcswp', 'tbt', 'sofb', 'tbtpha', 'fofbpha'))
+AcqStates = _get_namedtuple(
+            'AcqStates', ('Idle', 'Waiting', 'Acquiring', 'Error', 'Aborted'))
+AcqTrigTyp = _get_namedtuple(
+                        'AcqTrigTyp', ('now', 'external', 'data', 'software'))
+AcqTrigExter = _get_namedtuple(
+                'AcqTrigExter', ('Trig1', 'Trig2', 'Trig3', 'Trig4', 'Trig5'))
+FFTWindowTyp = _get_namedtuple(
+            'FFTWindowTyp', ('Square', 'Hanning', 'Parzen', 'Welch', 'QuadW'))
+FFTConvDirection = _get_namedtuple('FFTConvDirection', ('Forward', 'Backward'))
+FFTAvgSubtract = _get_namedtuple(
+                    'FFTAvgSubtract', ('No Subtraction', 'Average', 'Linear'))
 
-polarity = ['positive', 'negative']
-acq_repeat = ['normal', 'repetitive']
-acq_events = ['start', 'stop', 'abort', 'reset']
+FFTWritableProps = ['INDX', 'MXIX', 'WIND', 'CDIR', 'ASUB', 'SPAN']
 
-acq_datasel_types = ['A', 'B', 'C', 'D', 'X', 'Y', 'Sum', 'Q']
-acq_types = ['adc', 'adcswp', 'tbt', 'sofb', 'tbtpha', 'fofbpha']
-acq_states = ['Idle', 'Waiting', 'Acquiring', 'Error', 'Aborted']
-acq_trig_types = ['now', 'external', 'data', 'software']
-acq_trig_exter = ['Trig1', 'Trig2', 'Trig3', 'Trig4', 'Trig5']
 
-config_pvs = {
-    'Channel-Sel': {
-        'type': 'enum', 'enums': acq_types, 'value': 0},
-    'Channel-Sts': {
-        'type': 'enum', 'enums': acq_types, 'value': 0},
-    'Shots-SP': {
-        'type': 'int', 'value': 1, 'low': 0, 'high': 65536},
-    'Shots-RB': {
-        'type': 'int', 'value': 1, 'low': 0, 'high': 65536},
-    'TriggerHwDly-SP': {
-        'type': 'float', 'value': 0.0, 'low': 0.0, 'high': 1e9},
-    'TriggerHwDly-RB': {
-        'type': 'float', 'value': 0.0, 'low': 0.0, 'high': 1e9},
-    'UpdateTime-SP': {
-        'type': 'float', 'value': 1.0, 'low': 0.0, 'high': 1e9},
-    'UpdateTime-RB': {
-        'type': 'float', 'value': 1.0, 'low': 0.0, 'high': 1e9},
-    'SamplesPre-SP': {
-        'type': 'int', 'value': 1000, 'low': 0, 'high': 100000},
-    'SamplesPre-RB': {
-        'type': 'int', 'value': 1000, 'low': 0, 'high': 100000},
-    'SamplesPost-SP': {
-        'type': 'int', 'value': 1000, 'low': 0, 'high': 100000},
-    'SamplesPost-RB': {
-        'type': 'int', 'value': 1000, 'low': 0, 'high': 100000},
-    'TriggerEvent-Sel': {
-        'type': 'enum', 'enums': acq_events, 'value': 0},
-    'TriggerEvent-Sts': {
-        'type': 'enum', 'enums': acq_events, 'value': 0},
-    'Status-Sts': {
-        'type': 'enum', 'enums': acq_states, 'value': 0},
-    'Trigger-Sel': {
-        'type': 'enum', 'enums': acq_trig_types, 'value': 1},
-    'Trigger-Sts': {
-        'type': 'enum', 'enums': acq_trig_types, 'value': 1},
-    'TriggerRep-Sel': {
-        'type': 'enum', 'enums': acq_repeat, 'value': 0},
-    'TriggerRep-Sts': {
-        'type': 'enum', 'enums': acq_repeat, 'value': 0},
-    'TriggerExternalChan-Sel': {
-        'type': 'enum', 'enums': acq_trig_exter, 'value': 0},
-    'TriggerExternalChan-Sts': {
-        'type': 'enum', 'enums': acq_trig_exter, 'value': 0},
-    'TriggerDataChan-Sel': {
-        'type': 'enum', 'enums': acq_types, 'value': 0},
-    'TriggerDataChan-Sts': {
-        'type': 'enum', 'enums': acq_types, 'value': 0},
-    'TriggerDataSel-Sel': {
-        'type': 'enum', 'enums': acq_datasel_types, 'value': 0},
-    'TriggerDataSel-Sts': {
-        'type': 'enum', 'enums': acq_datasel_types, 'value': 0},
-    'TriggerDataThres-SP': {
-        'type': 'int', 'value': 1, 'low': 0, 'high': 2**16 - 1},
-    'TriggerDataThres-RB': {
-        'type': 'int', 'value': 1, 'low': 0, 'high': 2**16 - 1},
-    'TriggerDataPol-Sel': {
-        'type': 'enum', 'enums': polarity, 'value': 0},
-    'TriggerDataPol-Sts': {
-        'type': 'enum', 'enums': polarity, 'value': 0},
-    'TriggerDataHyst-SP': {
-        'type': 'int', 'value': 0, 'low': 0, 'high': 2**16 - 1},
-    'TriggerDataHyst-RB': {
-        'type': 'int', 'value': 0, 'low': 0, 'high': 2**16 - 1},
-    }
+def get_bpm_pvs(prefix):
+    data_db = {'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12}
+    db = {
+        'ACQBPMMode-Sel': {
+            'type': 'enum', 'enums': OpModes._fields, 'value': 0},
+        'ACQBPMMode-Sts': {
+            'type': 'enum', 'enums': OpModes._fields, 'value': 0},
 
-pvs_definitions = {
-    'ACQBPMMode-Sel': {
-        'type': 'enum', 'enums': op_modes, 'value': 0},
-    'ACQBPMMode-Sts': {
-        'type': 'enum', 'enums': op_modes, 'value': 0},
+        'asyn.ENBL': {
+            'type': 'enum', 'enums': EnblTyp._fields, 'value': 0},
+        'asyn.CNCT': {
+            'type': 'enum', 'enums': ConnTyp._fields, 'value': 0},
+        'INFOFOFBRate-SP': {
+            'type': 'int', 'value': 1910, 'low': 0, 'high': 2**32-1},
+        'INFOFOFBRate-RB': {
+            'type': 'int', 'value': 1910, 'low': 0, 'high': 2**32-1},
+        'INFOHarmonicNumber-SP': {
+            'type': 'int', 'value': 864, 'low': 0, 'high': 2**32-1},
+        'INFOHarmonicNumber-RB': {
+            'type': 'int', 'value': 864, 'low': 0, 'high': 2**32-1},
+        'INFOMONITRate-SP': {
+            'type': 'int', 'value': 21965000, 'low': 0, 'high': 2**32-1},
+        'INFOMONITRate-RB': {
+            'type': 'int', 'value': 21965000, 'low': 0, 'high': 2**32-1},
+        'INFOTBTRate-SP': {
+            'type': 'int', 'value': 382, 'low': 0, 'high': 2**32-1},
+        'INFOTBTRate-RB': {
+            'type': 'int', 'value': 382, 'low': 0, 'high': 2**32-1},
 
-    'asyn.ENBL': {
-        'type': 'enum', 'enums': ['Disable', 'Enable'], 'value': 0},
-    'asyn.CNCT': {
-        'type': 'enum', 'enums': ['Disconnect', 'Connect'], 'value': 0},
-    'INFOFOFBRate-SP': {
-        'type': 'int', 'value': 1910, 'low': 0, 'high': 2**32-1},
-    'INFOFOFBRate-RB': {
-        'type': 'int', 'value': 1910, 'low': 0, 'high': 2**32-1},
-    'INFOHarmonicNumber-SP': {
-        'type': 'int', 'value': 864, 'low': 0, 'high': 2**32-1},
-    'INFOHarmonicNumber-RB': {
-        'type': 'int', 'value': 864, 'low': 0, 'high': 2**32-1},
-    'INFOMONITRate-SP': {
-        'type': 'int', 'value': 21965000, 'low': 0, 'high': 2**32-1},
-    'INFOMONITRate-RB': {
-        'type': 'int', 'value': 21965000, 'low': 0, 'high': 2**32-1},
-    'INFOTBTRate-SP': {
-        'type': 'int', 'value': 382, 'low': 0, 'high': 2**32-1},
-    'INFOTBTRate-RB': {
-        'type': 'int', 'value': 382, 'low': 0, 'high': 2**32-1},
+        'PosQOffset-SP': _dcopy(data_db), 'PosQOffset-RB': _dcopy(data_db),
+        'PosXOffset-SP': _dcopy(data_db), 'PosXOffset-RB': _dcopy(data_db),
+        'PosYOffset-SP': _dcopy(data_db), 'PosYOffset-RB': _dcopy(data_db),
+        'PosKq-SP': _dcopy(data_db), 'PosKq-RB': _dcopy(data_db),
+        'PosKsum-SP': _dcopy(data_db), 'PosKsum-RB': _dcopy(data_db),
+        'PosKx-SP': _dcopy(data_db), 'PosKx-RB': _dcopy(data_db),
+        'PosKy-SP': _dcopy(data_db), 'PosKy-RB': _dcopy(data_db),
+        'PosX-Mon': _dcopy(data_db), 'PosY-Mon': _dcopy(data_db),
+        'Sum-Mon': _dcopy(data_db), 'PosQ-Mon': _dcopy(data_db),
+        'AmplA-Mon': _dcopy(data_db), 'AmplB-Mon': _dcopy(data_db),
+        'AmplC-Mon': _dcopy(data_db), 'AmplD-Mon': _dcopy(data_db),
 
-    'PosQOffset-SP': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'PosQOffset-RB': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'PosXOffset-SP': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'PosXOffset-RB': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'PosYOffset-SP': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'PosYOffset-RB': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'PosKq-SP': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'PosKq-RB': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'PosKsum-SP': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'PosKsum-RB': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'PosKx-SP': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'PosKx-RB': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'PosKy-SP': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'PosKy-RB': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
+        'SPPosX-Mon': _dcopy(data_db), 'SPPosY-Mon': _dcopy(data_db),
+        'SPSum-Mon': _dcopy(data_db), 'SPPosQ-Mon': _dcopy(data_db),
+        'SPAmplA-Mon': _dcopy(data_db), 'SPAmplB-Mon': _dcopy(data_db),
+        'SPAmplC-Mon': _dcopy(data_db), 'SPAmplD-Mon': _dcopy(data_db),
+        }
 
-    'PosX-Mon': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'PosY-Mon': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'Sum-Mon': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'PosQ-Mon': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'AmplA-Mon': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'AmplB-Mon': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'AmplC-Mon': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'AmplD-Mon': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
+    data_names = {
+        'GEN': ['A', 'B', 'C', 'D', 'Q', 'Sum', 'X', 'Y'],
+        'PM': ['A', 'B', 'C', 'D', 'Q', 'Sum', 'X', 'Y'],
+        'SP': ['A', 'B', 'C', 'D'],
+        }
+    data_db = {
+        'type': 'float', 'value': _np.array(100000*[0.0]), 'count': 100000}
 
-    'SPPosX-Mon': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'SPPosY-Mon': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'SPSum-Mon': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'SPPosQ-Mon': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'SPAmplA-Mon': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'SPAmplB-Mon': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'SPAmplC-Mon': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    'SPAmplD-Mon': {
-        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12},
-    }
+    for acq_tp in ['GEN', 'SP', 'PM']:
+        for prop in data_names[acq_tp]:
+            nm = acq_tp + '_' + prop
+            db[nm + 'ArrayData-Mon'] = _dcopy(data_db)
+            db.update(get_statistic_pvs(nm))
+            db.update(get_fft_pvs(nm))
 
-fft_window_types = ["Square", "Hanning", "Parzen", "Welch", "QuadW"]
-fft_conv_direction = ['Forward', 'Backward']
-fft_average_subtraction = ["No Subtraction", "Average", "Linear"]
+    for acq_md in ['ACQ', 'ACQ_PM']:
+        db.update(get_config_pvs(acq_md))
 
-fft_writable_props = ['INDX', 'MXIX', 'WIND', 'CDIR', 'ASUB', 'SPAN']
+    for k, v in db.items():
+        if 'low' in v:
+            v['lolo'] = v['low']
+            v['lolim'] = v['low']
+        if 'high' in v:
+            v['hihi'] = v['high']
+            v['hilim'] = v['high']
 
-acq_data_prop_db = {
-    'type': 'float', 'value': _np.array(100000*[0.0]), 'count': 100000}
-acq_data_stat_db = {'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12}
-acq_int_db = {'type': 'int', 'value': 1, 'low': 0, 'high': 100000}
-for acq_tp in ['GEN', 'SP', 'PM']:
-    for prop in data_names[acq_tp]:
-        nm = acq_tp + '_' + prop
-        pvs_loc = dict()
-        pvs_loc[nm+'ArrayData-Mon'] = _copy(acq_data_prop_db)
-        pvs_loc[nm + '_STATSMaxValue_RBV'] = _copy(acq_data_stat_db)
-        pvs_loc[nm + '_STATSMeanValue_RBV'] = _copy(acq_data_stat_db)
-        pvs_loc[nm + '_STATSMinValue_RBV'] = _copy(acq_data_stat_db)
-        pvs_loc[nm + '_STATSSigma_RBV'] = _copy(acq_data_stat_db)
-        pvs_loc[nm + 'FFTFreq-Mon'] = _copy(acq_data_prop_db)
-        pvs_loc[nm + 'FFTData.SPAN'] = _copy(acq_int_db)
-        pvs_loc[nm + 'FFTData.AMP'] = _copy(acq_data_prop_db)
-        pvs_loc[nm + 'FFTData.PHA'] = _copy(acq_data_prop_db)
-        pvs_loc[nm + 'FFTData.SIN'] = _copy(acq_data_prop_db)
-        pvs_loc[nm + 'FFTData.COS'] = _copy(acq_data_prop_db)
-        pvs_loc[nm + 'FFTData.WAVN'] = _copy(acq_data_prop_db)
-        pvs_loc[nm + 'FFTData.INDX'] = _copy(acq_int_db)
-        pvs_loc[nm + 'FFTData.MXIX'] = _copy(acq_int_db)
-        pvs_loc[nm + 'FFTData.WIND'] = {
-                'type': 'enum', 'enums': fft_window_types, 'value': 0}
-        pvs_loc[nm + 'FFTData.CDIR'] = {
-                'type': 'enum', 'enums': fft_conv_direction, 'value': 0}
-        pvs_loc[nm + 'FFTData.ASUB'] = {
-                'type': 'enum', 'enums': fft_average_subtraction, 'value': 0}
-        pvs_definitions.update(pvs_loc)
+    return {prefix + k: v for k, v in db.items()}
 
-for acq_md in ['ACQ', 'ACQ_PM']:
-    for key, value in config_pvs.items():
-        pvs_definitions[acq_md+key] = _copy(value)
 
-for k, v in pvs_definitions.items():
-    if 'low' in v:
-        v['lolo'] = v['low']
-        v['lolim'] = v['low']
-    if 'high' in v:
-        v['hihi'] = v['high']
-        v['hilim'] = v['high']
+def get_config_pvs(prefix):
+    db = {
+        'Channel-Sel': {
+            'type': 'enum', 'enums': AcqTyp._fields, 'value': 0},
+        'Channel-Sts': {
+            'type': 'enum', 'enums': AcqTyp._fields, 'value': 0},
+        'Shots-SP': {
+            'type': 'int', 'value': 1, 'low': 0, 'high': 65536},
+        'Shots-RB': {
+            'type': 'int', 'value': 1, 'low': 0, 'high': 65536},
+        'TriggerHwDly-SP': {
+            'type': 'float', 'value': 0.0, 'low': 0.0, 'high': 1e9},
+        'TriggerHwDly-RB': {
+            'type': 'float', 'value': 0.0, 'low': 0.0, 'high': 1e9},
+        'UpdateTime-SP': {
+            'type': 'float', 'value': 1.0, 'low': 0.0, 'high': 1e9},
+        'UpdateTime-RB': {
+            'type': 'float', 'value': 1.0, 'low': 0.0, 'high': 1e9},
+        'SamplesPre-SP': {
+            'type': 'int', 'value': 1000, 'low': 0, 'high': 100000},
+        'SamplesPre-RB': {
+            'type': 'int', 'value': 1000, 'low': 0, 'high': 100000},
+        'SamplesPost-SP': {
+            'type': 'int', 'value': 1000, 'low': 0, 'high': 100000},
+        'SamplesPost-RB': {
+            'type': 'int', 'value': 1000, 'low': 0, 'high': 100000},
+        'TriggerEvent-Sel': {
+            'type': 'enum', 'enums': AcqEvents._fields, 'value': 0},
+        'TriggerEvent-Sts': {
+            'type': 'enum', 'enums': AcqEvents._fields, 'value': 0},
+        'Status-Sts': {
+            'type': 'enum', 'enums': AcqStates._fields, 'value': 0},
+        'Trigger-Sel': {
+            'type': 'enum', 'enums': AcqTrigTyp._fields, 'value': 1},
+        'Trigger-Sts': {
+            'type': 'enum', 'enums': AcqTrigTyp._fields, 'value': 1},
+        'TriggerRep-Sel': {
+            'type': 'enum', 'enums': AcqRepeat._fields, 'value': 0},
+        'TriggerRep-Sts': {
+            'type': 'enum', 'enums': AcqRepeat._fields, 'value': 0},
+        'TriggerExternalChan-Sel': {
+            'type': 'enum', 'enums': AcqTrigExter._fields, 'value': 0},
+        'TriggerExternalChan-Sts': {
+            'type': 'enum', 'enums': AcqTrigExter._fields, 'value': 0},
+        'TriggerDataChan-Sel': {
+            'type': 'enum', 'enums': AcqTyp._fields, 'value': 0},
+        'TriggerDataChan-Sts': {
+            'type': 'enum', 'enums': AcqTyp._fields, 'value': 0},
+        'TriggerDataSel-Sel': {
+            'type': 'enum', 'enums': AcqDataTyp._fields, 'value': 0},
+        'TriggerDataSel-Sts': {
+            'type': 'enum', 'enums': AcqDataTyp._fields, 'value': 0},
+        'TriggerDataThres-SP': {
+            'type': 'int', 'value': 1, 'low': 0, 'high': 2**16 - 1},
+        'TriggerDataThres-RB': {
+            'type': 'int', 'value': 1, 'low': 0, 'high': 2**16 - 1},
+        'TriggerDataPol-Sel': {
+            'type': 'enum', 'enums': Polarity._fields, 'value': 0},
+        'TriggerDataPol-Sts': {
+            'type': 'enum', 'enums': Polarity._fields, 'value': 0},
+        'TriggerDataHyst-SP': {
+            'type': 'int', 'value': 0, 'low': 0, 'high': 2**16 - 1},
+        'TriggerDataHyst-RB': {
+            'type': 'int', 'value': 0, 'low': 0, 'high': 2**16 - 1},
+        }
+    return {prefix + k: v for k, v in db.items()}
+
+
+def get_fft_pvs(prefix):
+    data_db = {
+        'type': 'float', 'value': _np.array(100000*[0.0]), 'count': 100000}
+    acq_int_db = {'type': 'int', 'value': 1, 'low': 0, 'high': 100000}
+    db = dict()
+    db[prefix + 'FFTFreq-Mon'] = _dcopy(data_db)
+    db[prefix + 'FFTData.SPAN'] = _dcopy(acq_int_db)
+    db[prefix + 'FFTData.AMP'] = _dcopy(data_db)
+    db[prefix + 'FFTData.PHA'] = _dcopy(data_db)
+    db[prefix + 'FFTData.SIN'] = _dcopy(data_db)
+    db[prefix + 'FFTData.COS'] = _dcopy(data_db)
+    db[prefix + 'FFTData.WAVN'] = _dcopy(data_db)
+    db[prefix + 'FFTData.INDX'] = _dcopy(acq_int_db)
+    db[prefix + 'FFTData.MXIX'] = _dcopy(acq_int_db)
+    db[prefix + 'FFTData.WIND'] = {
+            'type': 'enum', 'enums': FFTWindowTyp._fields, 'value': 0}
+    db[prefix + 'FFTData.CDIR'] = {
+            'type': 'enum', 'enums': FFTConvDirection._fields, 'value': 0}
+    db[prefix + 'FFTData.ASUB'] = {
+            'type': 'enum', 'enums': FFTAvgSubtract._fields, 'value': 0}
+
+    return db
+
+
+def get_statistic_pvs(prefix):
+    acq_data_stat_db = {
+        'type': 'float', 'value': 0.0, 'low': -1e12, 'high': 1e12}
+    db = dict()
+    db[prefix + '_STATSMaxValue_RBV'] = _dcopy(acq_data_stat_db)
+    db[prefix + '_STATSMeanValue_RBV'] = _dcopy(acq_data_stat_db)
+    db[prefix + '_STATSMinValue_RBV'] = _dcopy(acq_data_stat_db)
+    db[prefix + '_STATSSigma_RBV'] = _dcopy(acq_data_stat_db)
+    return db
