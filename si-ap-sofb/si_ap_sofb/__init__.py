7ebd8a945f94def0017ce27339c4e689ebcfa68b
Fernando <fernandohds564@gmail.com>
2018-08-15 18:44:48 -0300

    API: Rename files appropriately.

--- si-ap-sofb/si_ap_sofb/__init__.py
+++
@@ -1,5 +0,0 @@
-from si_ap_sofb import (si_ap_sofb, correctors, main,
-                        matrix, orbit, definitions)
-
-__all__ = ('si_ap_sofb', 'correctors', 'main',
-           'matrix', 'orbit', 'definitions')
