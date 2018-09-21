e90b045bb87ee29a2c4bab80c0205fff67c52ce7
Fernando <fernandohds564@gmail.com>
2018-09-21 16:07:54 -0300

    API: Remove Const class from const module in csdevice package.

--- siriuspy/siriuspy/csdevice/const.py
+++ siriuspy/siriuspy/csdevice/const.py
@@ -17,16 +17,5 @@ def get_namedtuple(name, field_names, values=None):
     """
     if values is None:
         values = range(len(field_names))
+    field_names = [f.replace(' ', '_') for f in field_names]
     return _namedtuple(name, field_names)(*values)
-
-
-class Const:
-    """Const class defining power supply constants."""
-
-    @staticmethod
-    def add_field(field_name, values):
-        """Add field with constant values in const namespace."""
-        if hasattr(Const, field_name):
-            raise NameError('Field "{}" already exists.'.format(field_name))
-        const = get_namedtuple(field_name, values)
-        setattr(Const, field_name, const)
