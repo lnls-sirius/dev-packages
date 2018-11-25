82ef881e8fc7b3bf822418beee0fa376d950a999
ximenes <xresende@gmail.com>
2018-11-24 22:24:53 -0200

    Update previous added todo comment

--- siriuspy/siriuspy/mathphys/__init__.py
+++ siriuspy/siriuspy/mathphys/__init__.py
@@ -2,6 +2,7 @@
 
 
 # TODO: update subpackage with 2019 redefinition of SI base units and consts!
+# see https://en.wikipedia.org/wiki/2019_redefinition_of_SI_base_units
 
 from . import base_units
 from . import units
