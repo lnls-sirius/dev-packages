675a4e465054ec2577eba7d14a95e6137abcb6f2
Fernando <fernandohds564@gmail.com>
2018-12-14 17:50:57 -0200

    TIM.ENH: Add Low Level and High Level classes to package interface.

--- siriuspy/siriuspy/timesys/__init__.py
+++ siriuspy/siriuspy/timesys/__init__.py
@@ -2,8 +2,9 @@
 from .plot_network import PlotNetwork
 from .static_table import create_static_table, read_data_from_google, \
     read_data_from_local_excel_file
+from .hl_classes import HLEvent, HLTrigger
+from .ll_classes import LLEvent, get_evg_name, get_ll_trigger
 
-del plot_network
-del static_table
+del plot_network, static_table, hl_classes, ll_classes
 
 __all__ = ('time_simul', )
