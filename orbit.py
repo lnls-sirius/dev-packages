1abe6d578cf6b94eb77455875a7417c2d8650180
Fernando <fernandohds564@gmail.com>
2019-03-07 09:02:56 -0300

    SOFB.BUG: check if accelerator is ring before comparing operation mode.

--- orbit.py
+++ orbit.py
@@ -706,7 +706,7 @@ class EpicsOrbit(BaseOrbit):
             _log.error(msg[5:])
             return False
         for bpm in self.bpms:
-            if self._mode == self._csorb.OrbitMode.MultiTurn:
+            if self.isring and self._mode == self._csorb.OrbitMode.MultiTurn:
                 bpm.mode = _csbpm.OpModes.MultiBunch
             else:
                 bpm.mode = _csbpm.OpModes.SinglePass
