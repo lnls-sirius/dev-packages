9b2ca187c575e16e45ffe5b22b0b26520f399a6b
Fernando <fernandohds564@gmail.com>
2019-03-23 16:15:39 -0300

    SERVCONF.ENH: Add Egun and Modulator PVs to global_config.

--- siriuspy/siriuspy/servconf/types/global_config.py
+++ siriuspy/siriuspy/servconf/types/global_config.py
@@ -21,6 +21,23 @@ def get_dict():
 # in the configuration. The second numeric parameter in the pair is the
 # delay [s] the client should wait before setting the next PV.
 
+_pvs_li_egunmod = [
+    ['egun:biasps:switch', 0, 0],
+    ['egun:biasps:voltoutsoft', -60.0, 0.0],   # Volt
+    ['egun:filaps:switch', 0, 0],
+    ['egun:hvps:currentoutsoft', 0.003, 0.0],  # mA
+    ['egun:hvps:enable', 0, 0.0],
+    ['egun:pulseps:multiselect', 0, 0.0],
+    ['egun:pulseps:multiswitch', 0, 0.0],
+    ['egun:pulseps:singleselect', 0, 0.0],
+    ['egun:pulseps:singleswitch', 0, 0.0],
+    ['egun:pulseps:poweroutsoft', 0.0, 0.0],  # Volt
+    ['sirius_md:WRITE_I', 100.0, 0.0],        # mA
+    ['sirius_md_02:WRITE_I', 100.0, 0.0],     # mA
+    ['sirius_md:WRITE_V', 0.0, 0.0],          # kV
+    ['sirius_md_02:WRITE_V', 0.0, 0.0],       # kV
+    ]
+
 _pvs_li_llrf = [
     ['LA-RF:LLRF:BUN1:SET_STREAM', 0, 0.0],
     ['LA-RF:LLRF:BUN1:SET_CH1_DELAY', 0, 0.0],
