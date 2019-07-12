5d9a27ae66e9b6552a0b97e8f21395d6ec88311b
ximenes <xresende@gmail.com>
2019-07-12 11:13:56 -0300

    Cleanup code in maepics

--- siriuspy/siriuspy/pwrsupply/maepics.py
+++ siriuspy/siriuspy/pwrsupply/maepics.py
@@ -1,7 +1,8 @@
-"""Define Power Supply classes."""
+"""Define MA Epics Class."""
 
 import re as _re
-import time as _time
+
+# TODO: test using siriuspy.epics.pv.PV instead
 from epics import PV as _PV
 
 from siriuspy.envars import vaca_prefix as _VACA_PREFIX
@@ -13,19 +14,12 @@ from siriuspy.epics.computed_pv import ComputedPV as _ComputedPV
 from siriuspy.search.ma_search import MASearch as _MASearch
 from siriuspy.pwrsupply.data import PSData as _PSData
 from siriuspy.pwrsupply import sync as _sync
-from siriuspy.pwrsupply.bsmp import ConstFBP as _c
 from siriuspy.magnet.data import MAData as _MAData
 from siriuspy.magnet import util as _mutil
-from .status import PSCStatus as _PSCStatus
 
 
 class PSCommInterface:
     """Communication interface class for power supplies."""
-
-    # TODO: should this class have its own python module?
-    # TODO: this class is not specific to PS! its name should be updated to
-    # something line CommInterface or IOCConnInterface. In this case the class
-    # should be moved to siriuspy.util or another python module.
 
     # --- public interface ---
 
@@ -67,46 +61,38 @@ class PSCommInterface:
 class PSEpics(PSCommInterface):
     """Power supply with Epics communication."""
 
-    # TODO: should we merge this base class into MAEpics?
-
-    def __init__(self, psname, fields=None, use_vaca=True):
+    def __init__(self, psname, fields=None):
         """Create epics PVs and expose them through public controller API."""
         PSCommInterface.__init__(self)
-        # Attributes use build a full PV address
+
+        # attributes used to build a full PV address
         self._psname = psname
-        # self._sort_fields()
-        if use_vaca:
-            self._prefix = _VACA_PREFIX
-        else:
-            self._prefix = ''
-        # Get pv db
+        self._prefix = _VACA_PREFIX
+
+        # get pv db
         self._base_db = self._get_base_db()
-        # Get fields, if none is passed they'll will be retrieved from the db
+
+        # get fields, if none is passed they'll will be retrieved from the db
         if fields is None:
             self._fields = self._get_fields()
         else:
             self._fields = fields
-        # Holds PVs objects
-        self._pvs = dict()
+
+        # dictionary with PV objects
         self._create_pvs()
 
     # --- PSCommInterface implementation ---
 
     def read(self, field):
         """Read a field value."""
-        # if field not in self.valid_fields:
-        #     return None
         if self._pvs[field].connected:
             return self._pvs[field].get()
         else:
-            # print("Not connected")
             return None
 
     def write(self, field, value):
         """Write a value to a field."""
         # Check wether value is valid and return 0
-        # if field not in self.valid_fields:
-        #     return None
         if self._pvs[field].connected:
             return self._pvs[field].put(value)
         else:
@@ -118,9 +104,7 @@ class PSEpics(PSCommInterface):
         if not callable(func):
             raise ValueError("Tried to set non callable as a callback")
         else:
-            for pvname, pv in self._pvs.items():
-                # field = pvname.split(':')[-1]
-                # if field in self.valid_fields:
+            for pv in self._pvs.values():
                 pv.add_callback(func)
 
     def _connected(self):
@@ -154,8 +138,8 @@ class PSEpics(PSCommInterface):
         # In case the device is a Magnet with a normalized force being supplied
         # as one of the fields, a NormalizedPV is created
         self._sort_fields()
+        self._pvs = dict()
         for field in self._fields:
-            # if field in self.valid_fields:
             self._pvs[field] = self._create_pv(field)
 
     def _create_pv(self, field):
@@ -197,14 +181,7 @@ class PSEpics(PSCommInterface):
         return db
 
     def _sort_fields(self):
-        fields = []
-        for field in self._fields:
-            if not self._is_strength.match(field):
-                fields.insert(0, field)
-            else:
-                fields.append(field)
-
-        self._fields = fields
+        pass
 
 
 class MAEpics(PSEpics):
@@ -219,9 +196,11 @@ class MAEpics(PSEpics):
 
     def __init__(self, maname, lock=False, **kwargs):
         """Create epics PVs and expose them through public controller API."""
-        # Attributes use build a full PV address
+        # Attributes used to build a full PV address
         self._maname = _SiriusPVName(maname)
+        self._maclass = _mutil.magnet_class(self._maname)
         self._madata = _MAData(maname)
+
         self._lock = lock
         self._computed_pvs_queue = _QueueThread()
         super().__init__(
@@ -230,21 +209,25 @@ class MAEpics(PSEpics):
 
     # --- virtual methods ---
 
-    def _create_pvs(self):
-        self._sort_fields()
-        super()._create_pvs()
+    # def _create_pvs(self):
+    #     self._sort_fields()
+    #     super()._create_pvs()
 
     def _create_pv(self, field):
-        # Build either a real or computed PV
+        # Build either a base or computed PV
         if MAEpics._is_strength.match(field):
-            # 1) STRENGTH magnet fields
+            # 1) STRENGTH fields
             # an intermediate computed_pv is created in order for the
             # strength to be calculated from currents.
             pvname = self._prefix + self._maname + ":" + field
             str_obj = self._get_normalizer(self._maname)
-            pvs = self._get_str_pv(field)
+            str_pvs = self._get_str_pv(field)
+            # print('ComputedPV:       ', pvname)
+            # print(' strength_object: ', str_obj)
+            # print(' sytength pvs:    ', str_pvs)
+            # print()
             return _ComputedPV(pvname, str_obj,
-                               self._computed_pvs_queue, pvs)
+                               self._computed_pvs_queue, str_pvs)
         else:
             psnames = _MASearch.conv_psmaname_2_psnames(self._maname)
             if len(psnames) > 1:  # SyncPV
@@ -300,45 +283,67 @@ class MAEpics(PSEpics):
             return _sync.SyncRead()
 
     def _get_str_pv(self, field):
-        ma_class = _mutil.magnet_class(self._maname)
-        if 'dipole' == ma_class:
+        if self._maclass == 'dipole':
+            # field pv
             field = field.replace('Energy', 'Current')
-            return [self._pvs[field], ]
-        elif 'pulsed' == ma_class:
+            field_pv = self._pvs[field]
+            # return list
+            return [field_pv, ]
+        elif self._maclass == 'pulsed':
+            # field pv
+            field_pv = self._pvs[field.replace('Kick', 'Voltage')]
+            # dipole pvname
             dipole_name = _mutil.get_section_dipole_name(self._maname)
             dipole = self._prefix + dipole_name
-            dipole_pv = dipole + ':' + field.replace('Kick', 'Energy')
-            return [self._pvs[field.replace('Kick', 'Voltage')],
-                    dipole_pv]
-        elif 'trim' == ma_class:
+            dipole_pvname = dipole + ':' + field.replace('Kick', 'Energy')
+            # return list
+            return [field_pv, dipole_pvname]
+        elif self._maclass == 'trim':
+            # field pv
             field = field.replace('KL', 'Current')
+            field_pv = self._pvs[field]
+            # dipole pvname
             dipole_name = _mutil.get_section_dipole_name(self._maname)
             dipole = self._prefix + dipole_name
-            dipole_pv = dipole + ':' + field.replace('Current', 'Energy')
+            dipole_pvname = dipole + ':' + field.replace('Current', 'Energy')
+            # family pvname
             fam_name = _mutil.get_magnet_family_name(self._maname)
             fam = self._prefix + fam_name
-            family_pv = fam + ':' + field.replace('Current', 'KL')
-
+            family_pvname = fam + ':' + field.replace('Current', 'KL')
+            # NOTE: optimization
             # use Ref-Mon, instead of -Mon
             # (this is not necessary anymore for efficiency standpoint, since
-            #  now only the main current pv is being used to trigger
-            #  conversion. The line below may be commented out or deleted. )
-            dipole_pv = dipole_pv.replace('Energy-Mon', 'EnergyRef-Mon')
-            family_pv = family_pv.replace('KL-Mon', 'KLRef-Mon')
-
-            return [self._pvs[field], dipole_pv, family_pv]
+            # now only the main current pv is being used to trigger
+            # conversion. The line below may be commented out or deleted. )
+            dipole_pvname = dipole_pvname.replace('Energy-Mon', 'EnergyRef-Mon')
+            family_pvname = family_pvname.replace('KL-Mon', 'KLRef-Mon')
+            # return list
+            return [field_pv, dipole_pvname, family_pvname]
         else:
+            # field pv
             field = field.replace('KL', 'Current')
             field = field.replace('SL', 'Current')
             field = field.replace('Kick', 'Current')
+            field_pv = self._pvs[field]
+            # dipole pvname
             dipole_name = _mutil.get_section_dipole_name(self._maname)
             dipole = self._prefix + dipole_name
-            dipole_pv = dipole + ':' + field.replace('Current', 'Energy')
-
+            dipole_pvname = dipole + ':' + field.replace('Current', 'Energy')
+            # NOTE: optimization
             # use Ref-Mon, instead of -Mon
             # (this is not necessary anymore for efficiency standpoint, since
-            #  now only the main current pv is being used to trigger
-            #  conversion. The line below may be commented out or deleted. )
-            dipole_pv = dipole_pv.replace('Energy-Mon', 'EnergyRef-Mon')
+            # now only the main current pv is being used to trigger
+            # conversion. The line below may be commented out or deleted. )
+            dipole_pvname = dipole_pvname.replace('Energy-Mon', 'EnergyRef-Mon')
+            # return list
+            return [field_pv, dipole_pvname]
 
-            return [self._pvs[field], dipole_pv]
+    def _sort_fields(self):
+        # send all strength fields to end of list
+        fields = []
+        for field in self._fields:
+            if not self._is_strength.match(field):
+                fields.insert(0, field)
+            else:
+                fields.append(field)
+        self._fields = fields
