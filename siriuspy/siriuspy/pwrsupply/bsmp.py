dd33480d2894e379d2bf1053038fef11c0f7db74
ximenes <xresende@gmail.com>
2019-08-20 15:16:28 -0300

    Update psmodel FAC_2p4S_DCDC
    - add PVs IIBInductorTemperature-Mon, IIBHeatSinkTemperature-Mon, IIBInductorTemperature-Mon, IIBHeatSinkTemperature-Mon, IIBInductorTemperature-Mon, IIBHeatSinkTemperature-Mon, IIBInductorTemperature-Mon, IIBHeatSinkTemperature-Mon, IIBInductorTemperature-Mon, IIBHeatSinkTemperature-Mon, IIBInductorTemperature-Mon, IIBHeatSinkTemperature-Mon, IIBInductorTemperature-Mon, IIBHeatSinkTemperature-Mon, IIBInductorTemperature-Mon, IIBHeatSinkTemperature-Mon, IntlkIIB1-Mon, IntlkIIB2-Mon, IntlkIIB3-Mon, IntlkIIB4-Mon, IntlkIIB5-Mon, IntlkIIB6-Mon, IntlkIIB7-Mon, IntlkIIB8-Mon,

--- siriuspy/siriuspy/pwrsupply/bsmp.py
+++ siriuspy/siriuspy/pwrsupply/bsmp.py
@@ -313,19 +313,6 @@ class ConstFAC_DCDC(ConstBSMP):
 class ConstFAC_2P4S_DCDC(ConstBSMP):
     """Namespace for organizing power supply FAC_2P4S_DCDC BSMP constants."""
 
-    # --- undefined variables
-    V_UNDEF14 = 14
-    V_UNDEF15 = 15
-    V_UNDEF16 = 16
-    V_UNDEF17 = 17
-    V_UNDEF18 = 18
-    V_UNDEF19 = 19
-    V_UNDEF20 = 20
-    V_UNDEF21 = 21
-    V_UNDEF22 = 22
-    V_UNDEF23 = 23
-    V_UNDEF24 = 24
-
     # --- FAC_2P4S_DCDC variables ---
     V_PS_SOFT_INTERLOCKS = 25
     V_PS_HARD_INTERLOCKS = 26
@@ -359,6 +346,70 @@ class ConstFAC_2P4S_DCDC(ConstBSMP):
     V_DUTY_CYCLE_6 = 54
     V_DUTY_CYCLE_7 = 55
     V_DUTY_CYCLE_8 = 56
+    V_I_INPUT_IIB_1 = 57
+    V_I_OUTPUT_IIB_1 = 58
+    V_V_INPUT_IIB_1 = 59
+    V_TEMP_INDUCTOR_IIB_1 = 60
+    V_TEMP_HEATSINK_IIB_1 = 61
+    V_DRIVER_ERROR_1_IIB_1 = 62
+    V_DRIVER_ERROR_2_IIB_1 = 63
+    V_I_INPUT_IIB_2 = 64
+    V_I_OUTPUT_IIB_2 = 65
+    V_V_INPUT_IIB_2 = 66
+    V_TEMP_INDUCTOR_IIB_2 = 67
+    V_TEMP_HEATSINK_IIB_2 = 68
+    V_DRIVER_ERROR_1_IIB_2 = 69
+    V_DRIVER_ERROR_2_IIB_2 = 70
+    V_I_INPUT_IIB_3 = 71
+    V_I_OUTPUT_IIB_3 = 72
+    V_V_INPUT_IIB_3 = 73
+    V_TEMP_INDUCTOR_IIB_3 = 74
+    V_TEMP_HEATSINK_IIB_3 = 75
+    V_DRIVER_ERROR_1_IIB_3 = 76
+    V_DRIVER_ERROR_2_IIB_3 = 77
+    V_I_INPUT_IIB_4 = 78
+    V_I_OUTPUT_IIB_4 = 79
+    V_V_INPUT_IIB_4 = 80
+    V_TEMP_INDUCTOR_IIB_4 = 81
+    V_TEMP_HEATSINK_IIB_4 = 82
+    V_DRIVER_ERROR_1_IIB_4 = 83
+    V_DRIVER_ERROR_2_IIB_4 = 84
+    V_I_INPUT_IIB_5 = 85
+    V_I_OUTPUT_IIB_5 = 86
+    V_V_INPUT_IIB_5 = 87
+    V_TEMP_INDUCTOR_IIB_5 = 88
+    V_TEMP_HEATSINK_IIB_5 = 89
+    V_DRIVER_ERROR_1_IIB_5 = 90
+    V_DRIVER_ERROR_2_IIB_5 = 91
+    V_I_INPUT_IIB_6 = 92
+    V_I_OUTPUT_IIB_6 = 93
+    V_V_INPUT_IIB_6 = 94
+    V_TEMP_INDUCTOR_IIB_6 = 95
+    V_TEMP_HEATSINK_IIB_6 = 96
+    V_DRIVER_ERROR_1_IIB_6 = 97
+    V_DRIVER_ERROR_2_IIB_6 = 98
+    V_I_INPUT_IIB_6 = 99
+    V_I_OUTPUT_IIB_7 = 100
+    V_V_INPUT_IIB_7 = 101
+    V_TEMP_INDUCTOR_IIB_7 = 102
+    V_TEMP_HEATSINK_IIB_7 = 103
+    V_DRIVER_ERROR_1_IIB_7 = 104
+    V_DRIVER_ERROR_2_IIB_7 = 105
+    V_I_INPUT_IIB_8 = 106
+    V_I_OUTPUT_IIB_8 = 107
+    V_V_INPUT_IIB_8 = 108
+    V_TEMP_INDUCTOR_IIB_8 = 109
+    V_TEMP_HEATSINK_IIB_8 = 110
+    V_DRIVER_ERROR_1_IIB_8 = 111
+    V_DRIVER_ERROR_2_IIB_8 = 112
+    V_IIB_INTERLOCKS_1 = 113
+    V_IIB_INTERLOCKS_2 = 114
+    V_IIB_INTERLOCKS_3 = 115
+    V_IIB_INTERLOCKS_4 = 116
+    V_IIB_INTERLOCKS_5 = 117
+    V_IIB_INTERLOCKS_6 = 118
+    V_IIB_INTERLOCKS_7 = 119
+    V_IIB_INTERLOCKS_8 = 120
 
 
 class ConstFAC_2S_DCDC(ConstBSMP):
@@ -945,7 +996,6 @@ class EntitiesFAC_2P4S_DCDC(_Entities):
         {'eid': 11, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
         {'eid': 12, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
         {'eid': 13, 'waccess': False, 'count': 4, 'var_type': _Types.T_FLOAT},
-        # --- undefined variables
         {'eid': 14, 'waccess': False, 'count': 1, 'var_type': _Types.T_UINT8},
         {'eid': 15, 'waccess': False, 'count': 1, 'var_type': _Types.T_UINT8},
         {'eid': 16, 'waccess': False, 'count': 1, 'var_type': _Types.T_UINT8},
@@ -953,11 +1003,12 @@ class EntitiesFAC_2P4S_DCDC(_Entities):
         {'eid': 18, 'waccess': False, 'count': 1, 'var_type': _Types.T_UINT8},
         {'eid': 19, 'waccess': False, 'count': 1, 'var_type': _Types.T_UINT8},
         {'eid': 20, 'waccess': False, 'count': 1, 'var_type': _Types.T_UINT8},
+        # --- undefined variables
         {'eid': 21, 'waccess': False, 'count': 1, 'var_type': _Types.T_UINT8},
         {'eid': 22, 'waccess': False, 'count': 1, 'var_type': _Types.T_UINT8},
         {'eid': 23, 'waccess': False, 'count': 1, 'var_type': _Types.T_UINT8},
         {'eid': 24, 'waccess': False, 'count': 1, 'var_type': _Types.T_UINT8},
-        # --- FAC-2P4S-specific variables
+        # --- FAC-2P4S-DCDC-specific variables
         {'eid': 25, 'waccess': False, 'count': 1, 'var_type': _Types.T_UINT32},
         {'eid': 26, 'waccess': False, 'count': 1, 'var_type': _Types.T_UINT32},
         {'eid': 27, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
@@ -990,6 +1041,70 @@ class EntitiesFAC_2P4S_DCDC(_Entities):
         {'eid': 54, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
         {'eid': 55, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
         {'eid': 56, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 56, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 56, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 56, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 60, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 61, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 62, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 63, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 64, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 65, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 66, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 67, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 68, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 69, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 70, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 71, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 72, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 73, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 74, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 75, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 76, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 77, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 78, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 79, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 80, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 81, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 82, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 83, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 84, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 85, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 86, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 87, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 88, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 89, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 90, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 91, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 92, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 93, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 94, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 95, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 96, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 97, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 98, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 99, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 100, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 101, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 102, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 103, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 104, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 105, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 106, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 107, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 108, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 109, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 110, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 111, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 112, 'waccess': False, 'count': 1, 'var_type': _Types.T_FLOAT},
+        {'eid': 113, 'waccess': False, 'count': 1, 'var_type': _Types.T_UINT32},
+        {'eid': 114, 'waccess': False, 'count': 1, 'var_type': _Types.T_UINT32},
+        {'eid': 115, 'waccess': False, 'count': 1, 'var_type': _Types.T_UINT32},
+        {'eid': 116, 'waccess': False, 'count': 1, 'var_type': _Types.T_UINT32},
+        {'eid': 117, 'waccess': False, 'count': 1, 'var_type': _Types.T_UINT32},
+        {'eid': 118, 'waccess': False, 'count': 1, 'var_type': _Types.T_UINT32},
+        {'eid': 119, 'waccess': False, 'count': 1, 'var_type': _Types.T_UINT32},
+        {'eid': 120, 'waccess': False, 'count': 1, 'var_type': _Types.T_UINT32},
     )
 
     Curves = (
