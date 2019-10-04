2c6085188447000f431c4a8279a69b4f8fd49bad
Fernando <fernandohds564@gmail.com>
2019-10-04 14:52:24 -0300

    SOFB.ENH: Expose most import classes of the package.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,8 @@
 from . import as_ap_sofb, correctors, main, matrix, orbit
+from .main import SOFB
+from .matrix import EpicsMatrix
+from .correctors import EpicsCorrectors
+from .orbit import EpicsOrbit
 
 __all__ = (
     'as_ap_sofb', 'correctors', 'main', 'matrix', 'orbit')
