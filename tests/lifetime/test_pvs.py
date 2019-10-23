6b559af73580e8d68f5c1eed846d0d520d05f63f
anacso17 <anacso1777@gmail.com>
2019-10-23 10:07:10 -0300

    currinfo.MNT: remove BO lifetime and cleanup lifetime ioc code

--- tests/lifetime/test_pvs.py
+++ tests/lifetime/test_pvs.py
@@ -9,8 +9,6 @@ import as_ap_currinfo.lifetime.pvs as pvs
 
 
 valid_interface = (
-    'select_ioc',
-    'get_pvs_section',
     'get_pvs_vaca_prefix',
     'get_pvs_prefix',
     'get_pvs_database',
@@ -24,8 +22,7 @@ class TestASAPCurrInfoLifetimePvs(unittest.TestCase):
     def setUp(self):
         """Set tests up."""
         csdevice_patcher = mock.patch(
-            "as_ap_currinfo.lifetime.pvs._get_database",
-            autospec=True)
+            "as_ap_currinfo.lifetime.pvs._get_database", autospec=True)
         self.addCleanup(csdevice_patcher.stop)
         self.mock_csdevice = csdevice_patcher.start()
 
@@ -35,33 +32,12 @@ class TestASAPCurrInfoLifetimePvs(unittest.TestCase):
                                                       print_flag=True)
         self.assertTrue(valid)
 
-    def test_select_ioc(self):
-        """Test select_ioc."""
-        pvs.select_ioc('Accelerator')
-        acc = pvs.get_pvs_section()
-        vaca_prefix = pvs.get_pvs_vaca_prefix()
-        prefix = pvs.get_pvs_prefix()
-        self.assertIsInstance(acc, str)
-        self.assertIsInstance(vaca_prefix, str)
-        self.assertIsInstance(prefix, str)
-        self.assertIn(acc.lower(), prefix.lower())
-        self.assertIn(vaca_prefix.lower(), prefix.lower())
-        self.assertIn('CurrInfo', prefix)
-
-    def test_get_pvs_section(self):
-        """Test get_pvs_section."""
-        pvs.select_ioc('Accelerator')
-        self.assertIsInstance(pvs.get_pvs_section(), str)
-        self.assertEqual(pvs.get_pvs_section().lower(),
-                         'Accelerator'.lower())
-
     def test_get_pvs_vaca_prefix(self):
         """Test get_pvs_vaca_prefix."""
         self.assertIsInstance(pvs.get_pvs_vaca_prefix(), str)
 
     def test_get_pvs_prefix(self):
         """Test get_pvs_prefix."""
-        pvs.select_ioc('Accelerator')
         self.assertIsInstance(pvs.get_pvs_prefix(), str)
         self.assertIn('CurrInfo', pvs.get_pvs_prefix())
 
