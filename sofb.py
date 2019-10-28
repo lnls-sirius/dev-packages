8228238860e5857cbc25f724e5c08331231188be
Fernando <fernandohds564@gmail.com>
2019-10-28 10:20:30 -0300

    PML.BUG: Increase time SOFB waits before checking orbit accumulation is ready.

--- sofb.py
+++ sofb.py
@@ -73,7 +73,7 @@ class SOFB:
     def wait(self, timeout=10):
         inter = 0.05
         n = int(timeout/inter)
-        _time.sleep(inter)
+        _time.sleep(4*inter)
         for _ in range(n):
             if self._npts_rb.value >= self._npts_sp.value:
                 break
