5649bba7d7f2e55d1a66be37363d837ec142e861
Fernando <fernandohds564@gmail.com>
2019-12-02 17:45:39 -0300

    SOFB.BUG: Fix length of transport lines.

--- siriuspy/siriuspy/csdevice/orbitcorr.py
+++ siriuspy/siriuspy/csdevice/orbitcorr.py
@@ -152,7 +152,7 @@ class SOFBTLines(ConstTLines):
         self.EVT_ACQ_NAME = 'Dig' + self.acc
         self.MTX_SZ = self.NR_CORRS * (2 * self.NR_BPMS)
         self.NR_SING_VALS = min(self.NR_CORRS, 2 * self.NR_BPMS)
-        self.C0 = 22 if self.acc == 'TB' else 30  # in meters
+        self.C0 = 21.2477 if self.acc == 'TB' else 26.8933  # in meters
         self.T0 = self.C0 / 299792458  # in seconds
         self.ENERGY = 0.144 if self.acc in {'TB', 'BO'} else 3.0
 
