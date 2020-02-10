52656e5138ab7781a0a7531178c5735bd7ce130b
Fernando <fernandohds564@gmail.com>
2020-02-10 15:58:48 -0300

    PMLMAGS.BUG: Fix magnets import in __init__ file.

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from .rf import RF
 from .sofb import SOFB
 from .kicker import Kicker
 from .septum import Septum
-from .magnets import TrimQuad, Corrector
+from .magnets import Sextupole, Quadrupole, Corrector
 from .screen import Screen
 from .bpm import BPM
 from .ict import ICT, TranspEff
