9e9a9b0136777b2f2fb26e1c37d1cbaf9fff3298
ximenes <xresende@gmail.com>
2020-03-06 16:17:23 -0300

    Update devices.llrf

--- siriuspy/siriuspy/devices/__init__.py
+++ siriuspy/siriuspy/devices/__init__.py
@@ -6,7 +6,7 @@ from .dcct import DCCT
 from .egun import EGBias, EGFilament, EGHVPS
 from .ict import ICT, TranspEff
 from .kicker import Kicker
-from .li_llrf import LiLLRF
+from .llrf import LLRF
 from .pwrsupply import PowerSupply
 from .rf import RFGen, RFLL, RFPowMon, RFCav
 from .screen import Screen
