3ddb7d0548baacd66cf185bc9dd28edb3e3be2b0
ximenes <xresende@gmail.com>
2020-03-21 15:53:45 -0300

    epics: pv_fake has to be imported

--- siriuspy/siriuspy/epics/__init__.py
+++ siriuspy/siriuspy/epics/__init__.py
@@ -3,11 +3,9 @@
 CONNECTION_TIMEOUT = 0.050  # [s]
 
 from .pv import PV
-from .pv_fake import PVFake
 from .pv_time_serie import *
 from .properties import *
 
 del pv
-del pv_fake
 del pv_time_serie
 del properties
