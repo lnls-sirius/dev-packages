03cc4cbd92bded75b336e0764bdeed2f7bec5f16
anacso17 <anacso1777@gmail.com>
2020-03-27 17:22:52 -0300

    TST: fix currinfo tests

--- siriuspy/tests/currinfo/test_main.py
+++ siriuspy/tests/currinfo/test_main.py
@@ -1,55 +1,62 @@
-# #!/usr/bin/env python-sirius
+#!/usr/bin/env python-sirius
 
-# """Module to test AS-AP-CurrInfo Current Soft IOC main module."""
+"""Module to test AS-AP-CurrInfo Current Soft IOC main module."""
 
-# import unittest
-# import siriuspy.util as util
-# from siriuspy.currinfo.csdev import Const
-# from siriuspy.currinfo.main import SIApp
-
-
-# valid_interface = (
-#     'pvs_database',
-#     'init_database',
-#     'process',
-#     'read',
-#     'write',
-# )
+import unittest
+from unittest.mock import MagicMock
+import siriuspy.util as util
+from siriuspy.currinfo.csdev import Const
+from siriuspy.currinfo.main import SIApp
+from siriuspy.clientarch import ClientArchiver
 
 
-# class TestASAPCurrInfoCurrentMain(unittest.TestCase):
-#     """Test AS-AP-CurrInfo Soft IOC."""
-
-#     def _test_public_interface(self):
-#         """Test module's public interface."""
-#         valid = util.check_public_interface_namespace(
-#             SIApp, valid_interface, print_flag=True)
-#         self.assertTrue(valid)
-
-#     def _test_write_DCCTFltCheck(self):
-#         """Test write DCCTFltCheck-Sel."""
-#         app = SIApp()
-#         app.write('DCCTFltCheck-Sel', Const.DCCTFltCheck.On)
-#         self.assertEqual(app._dcctfltcheck_mode, Const.DCCTFltCheck.On)
-
-#     def _test_write_DCCT_FltCheckOn(self):
-#         """Test write DCCT-Sel."""
-#         app = SIApp()
-#         app._dcctfltcheck_mode = Const.DCCTFltCheck.On
-#         init_status = app._dcct_mode
-#         app.write('DCCT-Sel', Const.DCCT.Avg)
-#         app.write('DCCT-Sel', Const.DCCT.DCCT13C4)
-#         app.write('DCCT-Sel', Const.DCCT.DCCT14C4)
-#         end_status = app._dcct_mode
-#         self.assertEqual(init_status, end_status)
-
-#     def _test_write_DCCT_FltCheckOff(self):
-#         """Test write DCCT-Sel."""
-#         app = SIApp()
-#         app.write('DCCTFltCheck-Sel', Const.DCCTFltCheck.Off)
-#         app.write('DCCT-Sel', Const.DCCT.DCCT13C4)
-#         self.assertEqual(app._dcct_mode, Const.DCCT.DCCT13C4)
+valid_interface = (
+    'pvs_database',
+    'init_database',
+    'process',
+    'read',
+    'write',
+)
 
 
-# if __name__ == "__main__":
-#     unittest.main()
+class TestASAPCurrInfoCurrentMain(unittest.TestCase):
+    """Test AS-AP-CurrInfo Soft IOC."""
+
+    def setUp(self):
+        """Set Up tests."""
+        self.class2mock = ClientArchiver()
+        self.class2mock.getData = MagicMock(return_value=None)
+
+    def test_public_interface(self):
+        """Test module's public interface."""
+        valid = util.check_public_interface_namespace(
+            SIApp, valid_interface, print_flag=True)
+        self.assertTrue(valid)
+
+    def test_write_DCCTFltCheck(self):
+        """Test write DCCTFltCheck-Sel."""
+        app = SIApp()
+        app.write('DCCTFltCheck-Sel', Const.DCCTFltCheck.On)
+        self.assertEqual(app._dcctfltcheck_mode, Const.DCCTFltCheck.On)
+
+    def test_write_DCCT_FltCheckOn(self):
+        """Test write DCCT-Sel."""
+        app = SIApp()
+        app._dcctfltcheck_mode = Const.DCCTFltCheck.On
+        init_status = app._dcct_mode
+        app.write('DCCT-Sel', Const.DCCT.Avg)
+        app.write('DCCT-Sel', Const.DCCT.DCCT13C4)
+        app.write('DCCT-Sel', Const.DCCT.DCCT14C4)
+        end_status = app._dcct_mode
+        self.assertEqual(init_status, end_status)
+
+    def test_write_DCCT_FltCheckOff(self):
+        """Test write DCCT-Sel."""
+        app = SIApp()
+        app.write('DCCTFltCheck-Sel', Const.DCCTFltCheck.Off)
+        app.write('DCCT-Sel', Const.DCCT.DCCT13C4)
+        self.assertEqual(app._dcct_mode, Const.DCCT.DCCT13C4)
+
+
+if __name__ == "__main__":
+    unittest.main()
