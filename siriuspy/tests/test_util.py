ea46f581ed9dfa49f7c75efc99d2b5a5f88a7411
Fernando <fernandohds564@gmail.com>
2020-03-30 08:48:37 -0300

    UTIL.STY: Fix style in util test file.

--- siriuspy/tests/test_util.py
+++ siriuspy/tests/test_util.py
@@ -4,12 +4,13 @@
 
 from io import StringIO
 import logging
-import numpy as np
 from unittest import mock, TestCase
+import numpy as np
+
 import siriuspy.util as util
 
 
-public_interface = (
+PUB_INTERFACE = (
     'conv_splims_labels',
     'get_last_commit_hash',
     'get_timestamp',
@@ -34,7 +35,7 @@ class TestUtil(TestCase):
 
     def test_public_interface(self):
         """Test module's public interface."""
-        valid = util.check_public_interface_namespace(util, public_interface)
+        valid = util.check_public_interface_namespace(util, PUB_INTERFACE)
         self.assertTrue(valid)
 
     def test_conv_splims_labels(self):
@@ -45,8 +46,8 @@ class TestUtil(TestCase):
 
     def test_get_last_commit_hash(self):
         """Test get_signal_names."""
-        lc = util.get_last_commit_hash()
-        self.assertIsInstance(lc, str)
+        lst_comm = util.get_last_commit_hash()
+        self.assertIsInstance(lst_comm, str)
 
     def test_get_timestamp(self):
         """Test get_timestamp."""
@@ -96,97 +97,95 @@ class TestUtil(TestCase):
         text += '\n'  # empty line between data
         text += '# comment line 5 after empty line\n'  # comment between data
         text += 'datum7\n'  # line with data
-        data, parameters = util.read_text_data(text)
-        self.assertEqual(len(parameters), 3)
-        self.assertEqual(len(parameters['parameter1']), 1)
-        self.assertEqual(parameters['parameter1'][0], 'v1')
-        self.assertEqual(len(parameters['parameter2']), 1)
-        self.assertEqual(parameters['parameter2'][0], 'v2')
-        self.assertEqual(len(parameters['parameter3']), 3)
-        self.assertEqual(parameters['parameter3'][0], 'v3')
-        self.assertEqual(parameters['parameter3'][1], 'v4')
-        self.assertEqual(parameters['parameter3'][2], 'v5')
+        _, params = util.read_text_data(text)
+        self.assertEqual(len(params), 3)
+        self.assertEqual(len(params['parameter1']), 1)
+        self.assertEqual(params['parameter1'][0], 'v1')
+        self.assertEqual(len(params['parameter2']), 1)
+        self.assertEqual(params['parameter2'][0], 'v2')
+        self.assertEqual(len(params['parameter3']), 3)
+        self.assertEqual(params['parameter3'][0], 'v3')
+        self.assertEqual(params['parameter3'][1], 'v4')
+        self.assertEqual(params['parameter3'][2], 'v5')
 
     def test_print_ioc_banner(self):
         """Test print_ioc_banner."""
         dbase = {}
-        dbase.update({'PV01': None, 'PV02': None, 'PV03': None, 'PV04': None, })
-        dbase.update({'PV05': None, 'PV06': None, 'PV07': None, 'PV08': None, })
-        fi = StringIO()
+        dbase.update(
+            {'PV01': None, 'PV02': None, 'PV03': None, 'PV04': None, })
+        dbase.update(
+            {'PV05': None, 'PV06': None, 'PV07': None, 'PV08': None, })
+        fil = StringIO()
         logging.root.handlers = []
-        util.configure_log_file(stream=fi)
-        util.print_ioc_banner('test-ioc', dbase,
-                              'Test-ioc for util module unittest',
-                              '1.0.0', 'PREFIX')
-        text = fi.getvalue()
+        util.configure_log_file(stream=fil)
+        util.print_ioc_banner(
+            'test-ioc', dbase, 'Test-ioc for util module unittest',
+            '1.0.0', 'PREFIX')
+        text = fil.getvalue()
         self.assertEqual(len(text.splitlines()), 17)
-        dbase.update({'PV09': None, 'PV10': None, 'PV11': None, 'PV12': None, })
-        dbase.update({'PV13': None, 'PV14': None, 'PV15': None, 'PV16': None, })
-        util.print_ioc_banner('test-ioc', dbase,
-                              'Test-ioc for util module unittest',
-                              '1.0.0', 'PREFIX')
-        text = fi.getvalue()
+        dbase.update(
+            {'PV09': None, 'PV10': None, 'PV11': None, 'PV12': None, })
+        dbase.update(
+            {'PV13': None, 'PV14': None, 'PV15': None, 'PV16': None, })
+        util.print_ioc_banner(
+            'test-ioc', dbase, 'Test-ioc for util module unittest',
+            '1.0.0', 'PREFIX')
+        text = fil.getvalue()
         self.assertEqual(len(text.splitlines()), 25+17)
 
     def test_configure_log_file(self):
         """Test configure_log_file."""
-        fi = StringIO()
+        fil = StringIO()
         logging.root.handlers = []
-        util.configure_log_file(stream=fi)
+        util.configure_log_file(stream=fil)
         logging.info('test')
         logging.debug('test')
-        text = fi.getvalue()
+        text = fil.getvalue()
         self.assertEqual(len(text.splitlines()), 1)
 
-        fi = StringIO()
+        fil = StringIO()
         logging.root.handlers = []
-        util.configure_log_file(stream=fi, debug=True)
+        util.configure_log_file(stream=fil, debug=True)
         logging.info('test')
         logging.debug('test')
-        text = fi.getvalue()
+        text = fil.getvalue()
         self.assertEqual(len(text.splitlines()), 2)
 
     def test_beam_rigidity(self):
         """Test beam_rigidity."""
-        e0 = 510998.92760316096/1e9  # electron rest energy [GeV]
+        en0 = 510998.92760316096/1e9  # electron rest energy [GeV]
         tol = 1e-12
         # float arg
-        r, b, g = util.beam_rigidity(3.0)
-        self.assertIsInstance(r, float)
-        self.assertAlmostEqual(r, 10.00692271077752, delta=tol)
-        self.assertAlmostEqual(b, 0.9999999854933386, delta=tol)
-        self.assertAlmostEqual(g, 5870.853807994258, delta=tol)
-        r, *_ = util.beam_rigidity(energy=1.001*e0)
-        self.assertIsInstance(r, float)
-        self.assertAlmostEqual(r, 7.624701218711276e-05, delta=tol)
-        r, b, g = util.beam_rigidity(0.0)
-        self.assertEqual(r, 0.0)
-        # self.assertRaises(ValueError, util.beam_rigidity, energy=0.0)
-        r, b, g = util.beam_rigidity(0.999*e0)
-        self.assertEqual(r, 0.0)
-        # self.assertRaises(ValueError, util.beam_rigidity, energy=0.999*e0)
+        ret, beta, gamma = util.beam_rigidity(3.0)
+        self.assertIsInstance(ret, float)
+        self.assertAlmostEqual(ret, 10.00692271077752, delta=tol)
+        self.assertAlmostEqual(beta, 0.9999999854933386, delta=tol)
+        self.assertAlmostEqual(gamma, 5870.853807994258, delta=tol)
+        ret, *_ = util.beam_rigidity(energy=1.001*en0)
+        self.assertIsInstance(ret, float)
+        self.assertAlmostEqual(ret, 7.624701218711276e-05, delta=tol)
+        ret, *_ = util.beam_rigidity(0.0)
+        self.assertEqual(ret, 0.0)
+        ret, *_ = util.beam_rigidity(0.999*en0)
+        self.assertEqual(ret, 0.0)
         # list arg
-        r, *_ = util.beam_rigidity([0.5, 3.0])
-        self.assertIsInstance(r, np.ndarray)
-        self.assertAlmostEqual(r[0], 1.6678196049882876, delta=tol)
-        self.assertAlmostEqual(r[1], 10.00692271077752, delta=tol)
+        ret, *_ = util.beam_rigidity([0.5, 3.0])
+        self.assertIsInstance(ret, np.ndarray)
+        self.assertAlmostEqual(ret[0], 1.6678196049882876, delta=tol)
+        self.assertAlmostEqual(ret[1], 10.00692271077752, delta=tol)
         # tuple arg
-        r, *_ = util.beam_rigidity((0.5, 3.0))
-        self.assertIsInstance(r, np.ndarray)
-        self.assertAlmostEqual(r[0], 1.6678196049882876, delta=tol)
-        self.assertAlmostEqual(r[1], 10.00692271077752, delta=tol)
+        ret, *_ = util.beam_rigidity((0.5, 3.0))
+        self.assertIsInstance(ret, np.ndarray)
+        self.assertAlmostEqual(ret[0], 1.6678196049882876, delta=tol)
+        self.assertAlmostEqual(ret[1], 10.00692271077752, delta=tol)
         # numpy.ndarray arg
-        r, *_ = util.beam_rigidity(np.array([0.5, 3.0]))
-        self.assertIsInstance(r, np.ndarray)
-        self.assertAlmostEqual(r[0], 1.6678196049882876, delta=tol)
-        self.assertAlmostEqual(r[1], 10.00692271077752, delta=tol)
-        r, *_ = util.beam_rigidity(energy=np.array([1.001*e0, 3.0]))
-        self.assertIsInstance(r, np.ndarray)
-        self.assertAlmostEqual(r[0], 7.624701218711276e-05, delta=tol)
-        # self.assertRaises(ValueError, util.beam_rigidity,
-        #                   energy=np.array([0.0, 3]))
-        # self.assertRaises(ValueError, util.beam_rigidity,
-        #                   energy=np.array([0.999*e0, 3.0]))
+        ret, *_ = util.beam_rigidity(np.array([0.5, 3.0]))
+        self.assertIsInstance(ret, np.ndarray)
+        self.assertAlmostEqual(ret[0], 1.6678196049882876, delta=tol)
+        self.assertAlmostEqual(ret[1], 10.00692271077752, delta=tol)
+        ret, *_ = util.beam_rigidity(energy=np.array([1.001*en0, 3.0]))
+        self.assertIsInstance(ret, np.ndarray)
+        self.assertAlmostEqual(ret[0], 7.624701218711276e-05, delta=tol)
 
     def test_check_pv_online(self):
         """Test check_pv_online."""
@@ -207,8 +206,8 @@ class TestUtil(TestCase):
         self.assertEqual("KL", util.get_strength_label("quadrupole"))
         self.assertEqual("KL", util.get_strength_label("quadrupole-skew"))
         self.assertEqual("SL", util.get_strength_label("sextupole"))
-        self.assertEqual("Kick",
-                         util.get_strength_label("corrector-horizontal"))
+        self.assertEqual(
+            "Kick", util.get_strength_label("corrector-horizontal"))
         self.assertEqual("Kick", util.get_strength_label("corrector-vertical"))
         self.assertRaises(ValueError, util.get_strength_label, magfunc='')
 
@@ -219,11 +218,13 @@ class TestUtil(TestCase):
         self.assertEqual("1/m", util.get_strength_units("quadrupole-skew"))
         self.assertEqual("1/m^2", util.get_strength_units("sextupole"))
         self.assertEqual(
-            "urad", util.get_strength_units("corrector-horizontal", None, False))
+            "urad",
+            util.get_strength_units("corrector-horizontal", None, False))
         self.assertEqual(
             "urad", util.get_strength_units("corrector-vertical", None, False))
         self.assertEqual(
-            "mrad", util.get_strength_units("corrector-horizontal", None, True))
+            "mrad",
+            util.get_strength_units("corrector-horizontal", None, True))
         self.assertEqual(
             "mrad", util.get_strength_units("corrector-vertical", None, True))
         self.assertRaises(ValueError, util.get_strength_units, magfunc='')
@@ -257,37 +258,41 @@ class TestUtil(TestCase):
         self.assertEqual(1, util.get_bit(v=0x10, bit_pos=4))
 
     def test_mode(self):
+        """Test test_mode."""
         self.assertEqual((3, 2), util.mode([1, 2, 3, 4, 5, 6, 3]))
 
     def test_check_public_interface_namespace(self):
         """Test check_public_interface_namespace."""
-        class namespace:
+        class NameSpace:
+            """."""
+
             A = None
             B = None
+
         public_interface = ('A', 'B', )
         valid = util.check_public_interface_namespace(
-            namespace, public_interface)
+            NameSpace, public_interface)
         self.assertTrue(valid)
-        namespace.C = None
+        NameSpace.C = None
         valid = util.check_public_interface_namespace(
-            namespace, public_interface, print_flag=False)
+            NameSpace, public_interface, print_flag=False)
         self.assertFalse(valid)
         public_interface = ('A', 'B', 'missing_symbol')
         valid = util.check_public_interface_namespace(
-            namespace, public_interface, print_flag=False)
+            NameSpace, public_interface, print_flag=False)
         self.assertFalse(valid)
 
     def test_get_namedtuple(self):
         """Test get_namedtuple."""
         fields = ('a', 'b', 'c')
         values = (1, 4, 5)
-        a = util.get_namedtuple('A', fields, values)
-        self.assertTrue(a.__class__.__name__ == 'A')
-        self.assertTrue(a._fields == fields)
-        self.assertTrue(a.a == 1 and a.b == 4 and a.c == 5)
+        ntp = util.get_namedtuple('A', fields, values)
+        self.assertTrue(ntp.__class__.__name__ == 'A')
+        self.assertTrue(ntp._fields == fields)
+        self.assertTrue(ntp.a == 1 and ntp.b == 4 and ntp.c == 5)
         fields = ('a', 'b', 'c')
-        a = util.get_namedtuple('A', fields)
-        self.assertTrue(a.a == 0 and a.b == 1 and a.c == 2)
+        ntp = util.get_namedtuple('A', fields)
+        self.assertTrue(ntp.a == 0 and ntp.b == 1 and ntp.c == 2)
         fields = ('a a', 'b b', 'c')
-        a = util.get_namedtuple('A', fields)
-        self.assertTrue(a._fields == ('a_a', 'b_b', 'c'))
+        ntp = util.get_namedtuple('A', fields)
+        self.assertTrue(ntp._fields == ('a_a', 'b_b', 'c'))
