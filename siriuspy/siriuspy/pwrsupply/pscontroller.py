819b0bac886f3f8c0a492ff9bb99b76e0c8bc94d
ximenes <xresende@gmail.com>
2020-04-04 19:37:22 -0300

    Refcator prwsupply.pscontroller classes
    - code clenup and variable renaming
    - convert _init_setpoints into a public method that needes invokation

--- siriuspy/siriuspy/pwrsupply/pscontroller.py
+++ siriuspy/siriuspy/pwrsupply/pscontroller.py
@@ -2,11 +2,10 @@
 
 
 class PSController:
-    """Class used to communicate with PS controller.
+    """Power supply controller.
 
-    PRUController
-    Fields method property holds objects trant translate EPICS fields to a BSMP
-    variable.
+        Objects of this class are used to communicate with power supply devices
+    by invoking properties and methods of the PRU controller.
     """
 
     _ignored_fields = {
@@ -16,22 +15,26 @@ class PSController:
         'KL-SP', 'KL-RB', 'KLRef-Mon', 'KL-Mon',
         'SL-SP', 'SL-RB', 'SLRef-Mon', 'SL-Mon'}
 
-    def __init__(self, readers, functions,
-                 pru_controller, psname2dev):
+    def __init__(self, readers, writers,
+                 pru_controller, devname2devid):
         """Initialize PS controller.
 
         readers : objects from classes in fields module that are responsible
                   for reading power supply parameters
-        functions : objects from classes in functions
+        writers : objects from classes in writers
 
         """
         self._readers = readers
-        self._writers = functions
+        self._writers = writers
         self._pru_controller = pru_controller
-        self._psname2dev = psname2dev
+
+        self._devname2devid = devname2devid
+
+        # get all controller fields
         self._fields = self._get_fields()
+
+        # shortcut to fields not to be ignored
         self._fields_not_ignored = self._fields - self._ignored_fields
-        self._init_setpoints()
 
     @property
     def pru_controller(self):
@@ -43,12 +46,12 @@ class PSController:
         """Field of ps controller."""
         return self._fields
 
-    def read(self, device_name, field):
+    def read(self, devname, field):
         """Read pv value."""
-        pvname = device_name + ':' + field
+        pvname = devname + ':' + field
         if field == 'CtrlLoop-Sts':
-            sts = self._readers[device_name + ':CtrlLoop-Sts']
-            sel = self._readers[device_name + ':CtrlLoop-Sel']
+            sts = self._readers[devname + ':CtrlLoop-Sts']
+            sel = self._readers[devname + ':CtrlLoop-Sel']
             if sts.read() != sel.read():
                 sel.apply(sts.read())
 
@@ -57,47 +60,61 @@ class PSController:
             return reader.read()
         raise AttributeError('Could not find reader for "{}"'.format(field))
 
-    def read_all_fields(self, device_name):
+    def read_all_fields(self, devname):
         """Read non-ignored-field values from device."""
         values = dict()
         for field in self._fields_not_ignored:
-            pvname = device_name + ':' + field
-            value = self.read(device_name, field)
+            pvname = devname + ':' + field
+            value = self.read(devname, field)
             values[pvname] = value
         return values
 
-    def write(self, device_name, field, value):
+    def write(self, devname, field, value):
         """Write value to pv."""
-        pvname = device_name + ':' + field
+        pvname = devname + ':' + field
         if pvname in self._writers:
             self._writers[pvname].execute(value)
 
-    def check_connected(self, device_name):
+    def check_connected(self, devname):
         """Check if device is connected."""
-        dev_id = self._psname2dev[device_name]
-        return self._pru_controller.check_connected(dev_id)
+        devid = self._devname2devid[devname]
+        return self._pru_controller.check_connected(devid)
 
-    def _init_setpoints(self):
-        for key, reader in self._readers.items():
+    def init_setpoints(self):
+        """Initialize controller setpoint fields."""
+        for key, reader_sp in self._readers.items():
+
+            # ignore non-setpoint fields
+            if not key.endswith(('-Sel', '-SP')):
+                continue
+
             # ignore strength fields
             *_, prop = key.split(':')
             if prop in PSController._ignored_fields:
                 continue
-            if key.endswith(('-Sel', '-SP')):
-                # get corresponding readback property
-                rb_field = PSController._get_readback_field(key)
-                # check if reader exists
-                rb_reader = self._readers[rb_field]
-                if rb_reader is None:
-                    raise AttributeError(
-                        'Could not find reader for "{}"'.format(rb_field))
-                # read value
-                value = rb_reader.read()
-                if key.endswith('OpMode-Sel'):
-                    # OpModel-Sel is shifted in 3 units relative OpMode-Sts
-                    if value is not None:
-                        value = 0 if value < 3 else value - 3
-                reader.apply(value)
+
+            # initiliaze setpoint fields using corresponding readers
+
+            # use corresponding readback field
+            field_rb = PSController._get_readback_field(key)
+
+            # check if reader exists
+            reader_rb = self._readers[field_rb]
+            if reader_rb is None:
+                raise AttributeError(
+                    'Could not find reader for "{}"'.format(field_rb))
+
+            # read readback value
+            value = reader_rb.read()
+            if key.endswith('OpMode-Sel'):
+                # OpModel-Sel is shifted in 3 units relative OpMode-Sts
+                if value is not None:
+                    value = 0 if value < 3 else value - 3
+
+            # apply value to setpoint using its reader
+            reader_sp.apply(value)
+
+    # --- private methods ---
 
     def _get_fields(self):
         fields = set()
@@ -124,17 +141,17 @@ class StandardPSController(PSController):
         'CycleAuxParam-SP',  # start index of auxparams
     ]
 
-    def write(self, device_name, field, value):
+    def write(self, devname, field, value):
         """Override write method."""
-        pvname = device_name + ':' + field
+        pvname = devname + ':' + field
         if pvname not in self._writers:
             return
         if field == 'OpMode-Sel':
             writer = self._writers[pvname]
-            self._set_opmode(writer, value)
+            StandardPSController._set_opmode(writer, value)
         elif field in StandardPSController._siggen_parms:
             idx = StandardPSController._siggen_parms.index(field)
-            values = self._get_siggen_arg_values(device_name)
+            values = self._get_siggen_arg_values(devname)
             if field == 'CycleAuxParam-SP':
                 values[idx:] = value
             else:
@@ -144,19 +161,22 @@ class StandardPSController(PSController):
             # NOTE: Should we set Current-SP to zero at Power On ? This may be
             # generating inconsistent behaviour when loading configuration in
             # HLA...
-            # if self._readers[device_name + ':PwrState-Sel'].value == 0:
-            #     self._readers[device_name + ':Current-SP'].apply(0.0)
+            # if self._readers[devname + ':PwrState-Sel'].value == 0:
+            #     self._readers[devname + ':Current-SP'].apply(0.0)
             self._writers[pvname].execute(value)
         else:
             self._writers[pvname].execute(value)
 
-    def _set_opmode(self, writer, op_mode):
+    # --- private methods ---
+
+    @staticmethod
+    def _set_opmode(writer, op_mode):
         writer.execute(op_mode)
 
-    def _get_siggen_arg_values(self, device_name):
+    def _get_siggen_arg_values(self, devname):
         """Get cfg_siggen args and execute it."""
-        args = [self._readers[device_name + ':' + arg].read()
+        args = [self._readers[devname + ':' + arg].read()
                 for arg in StandardPSController._siggen_parms[:-1]]
         aux = StandardPSController._siggen_parms[-1]
-        args.extend(self._readers[device_name + ':' + aux].read())
+        args.extend(self._readers[devname + ':' + aux].read())
         return args
