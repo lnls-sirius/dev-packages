1119893e10c416589525609b6d3b7759daa08de4
anacso17 <anacso1777@gmail.com>
2020-04-07 17:38:10 -0300

    opticscorr.ENH: add new version of Tune and Chrom corr IOCs - Reimplement Tune and Chrom Apps using BaseApp - Add Chromaticity measurement - Add Correction Configuration measurement

---
+++ siriuspy/siriuspy/opticscorr/tune.py
@@ -0,0 +1,219 @@
+"""Main module of AS-AP-TuneCorr IOC."""
+
+import numpy as _np
+
+from ..namesys import SiriusPVName as _SiriusPVName
+
+from .csdev import Const as _Const
+from .base import BaseApp as _BaseApp
+
+
+class TuneCorrApp(_BaseApp):
+    """Main application for handling tune correction."""
+
+    _optics_param = 'tune'
+
+    def __init__(self, acc):
+        """Class constructor."""
+        super().__init__(acc)
+
+        # consts
+        self._delta_tunex = 0.0
+        self._delta_tuney = 0.0
+
+        self._set_new_refkl_cmd_count = 0
+
+        if self._acc == 'SI':
+            self._meas_config_dkl_qf = 0.0500
+            self._meas_config_dkl_qd = 0.0500
+
+        # Connect to Quadrupoles Families
+        self._psfam_refkl = {fam: 0 for fam in self._psfams}
+        self._lastcalc_deltakl = {fam: 0 for fam in self._psfams}
+        for fam in self._psfams:
+            self._psfam_intstr_rb_pvs[fam].add_callback(
+                self._callback_init_refkl)
+            self._psfam_intstr_rb_pvs[fam].add_callback(
+                self._callback_estimate_deltatune)
+
+        self.map_pv2write.update({
+            'DeltaTuneX-SP': self.set_dtune_x,
+            'DeltaTuneY-SP': self.set_dtune_y,
+            'SetNewRefKL-Cmd': self.cmd_set_newref,
+            'MeasConfigDeltaKLQF-SP': self.set_meas_config_dkl_qf,
+            'MeasConfigDeltaKLQD-SP': self.set_meas_config_dkl_qd,
+        })
+
+    def update_corrparams(self):
+        """Set initial correction parameters PVs values."""
+        self.run_callbacks('RespMat-Mon', self._nominal_matrix)
+        self.run_callbacks('NominalKL-Mon', self._psfam_nom_intstr)
+
+    # ------ handle pv write methods -------
+
+    def set_dtune_x(self, value):
+        """Set DeltaTuneX."""
+        self._delta_tunex = value
+        self.run_callbacks('DeltaTuneX-RB', value)
+        self._calc_intstrength()
+        return True
+
+    def set_dtune_y(self, value):
+        """Set DeltaTuneY."""
+        self._delta_tuney = value
+        self.run_callbacks('DeltaTuneY-RB', value)
+        self._calc_intstrength()
+        return True
+
+    def cmd_set_newref(self, value):
+        """SetNewRefKL command."""
+        if self._update_ref():
+            self._set_new_refkl_cmd_count += 1
+            self.run_callbacks(
+                'SetNewRefKL-Cmd', self._set_new_refkl_cmd_count)
+        return False
+
+    def set_meas_config_dkl_qf(self, value):
+        """Set MeasConfigDeltaKLQF."""
+        if value == self._meas_config_dkl_qf:
+            return False
+        self._meas_config_dkl_qf = value
+        self.run_callbacks('MeasConfigDeltaKLQF-RB', value)
+        return True
+
+    def set_meas_config_dkl_qd(self, value):
+        """Set MeasConfigDeltaKLQD."""
+        if value == self._meas_config_dkl_qd:
+            return False
+        self._meas_config_dkl_qd = value
+        self.run_callbacks('MeasConfigDeltaKLQD-RB', value)
+        return True
+
+    # ---------- auxiliar methods ----------
+
+    def _handle_corrparams_2_read(self, params):
+        """Edit correction params."""
+        nom_matrix = [item for sublist in params['matrix'] for item in sublist]
+        nom_kl = params['nominal KLs']
+        nom_deltakl = [0.0, 0.0]
+        return nom_matrix, nom_kl, nom_deltakl
+
+    def _handle_corrparams_2_save(self):
+        matrix = _np.array(self._nominal_matrix)
+        matrix = _np.reshape(matrix, [2, len(self._psfams)])
+
+        value = {'matrix': matrix,
+                 'nominal KLs': self._psfam_nom_intstr}
+        return value
+
+    def _calc_intstrength(self):
+        method = 0 \
+            if self._corr_method == _Const.CorrMeth.Proportional \
+            else 1
+        grouping = '2knobs' \
+            if self._corr_group == _Const.CorrGroup.TwoKnobs \
+            else 'svd'
+        lastcalc_deltakl = self._opticscorr.calculate_delta_intstrengths(
+            method=method, grouping=grouping,
+            delta_opticsparam=[self._delta_tunex, self._delta_tuney])
+
+        self.run_callbacks('Log-Mon', 'Calculated KL values.')
+
+        for fam in self._psfams:
+            fam_idx = self._psfams.index(fam)
+            self._lastcalc_deltakl[fam] = lastcalc_deltakl[fam_idx]
+            self.run_callbacks(
+                'DeltaKL'+fam+'-Mon', self._lastcalc_deltakl[fam])
+
+    def _apply_corr(self):
+        if self._is_status_ok():
+            kls = {fam: self._psfam_refkl[fam]+self._lastcalc_deltakl[fam]
+                   for fam in self._psfams}
+            self._apply_intstrength(kls)
+            self.run_callbacks('Log-Mon', 'Applied correction.')
+
+            if self._sync_corr == _Const.SyncCorr.On:
+                self._event_exttrig_cmd.put(0)
+                self.run_callbacks('Log-Mon', 'Generated trigger.')
+            return True
+
+        self.run_callbacks('Log-Mon', 'ERR: ApplyDelta-Cmd failed.')
+        return False
+
+    def _get_optics_param(self):
+        """Return optics parameter."""
+        return self._get_tunes()
+
+    def _get_delta_intstrength(self, fam):
+        """Get delta to apply in each family."""
+        if 'QF' in fam:
+            deltakl = self._meas_config_dkl_qf
+        else:
+            deltakl = self._meas_config_dkl_qd
+        return deltakl
+
+    def _update_ref(self):
+        if (self._status & 0x1) == 0:  # Check connection
+            # update references
+            for fam in self._psfams:
+                value = self._psfam_intstr_rb_pvs[fam].get()
+                if value is None:
+                    self.run_callbacks(
+                        'Log-Mon',
+                        'ERR: Received a None value from {}.'.format(fam))
+                    return False
+                self._psfam_refkl[fam] = value
+                self.run_callbacks(
+                    'RefKL' + fam + '-Mon', self._psfam_refkl[fam])
+
+                self._lastcalc_deltakl[fam] = 0
+                self.run_callbacks('DeltaKL' + fam + '-Mon', 0)
+
+            # the deltas from new kl references are zero
+            self._delta_tunex = 0
+            self._delta_tuney = 0
+            self.run_callbacks('DeltaTuneX-SP', self._delta_tunex)
+            self.run_callbacks('DeltaTuneX-RB', self._delta_tunex)
+            self.run_callbacks('DeltaTuneY-SP', self._delta_tuney)
+            self.run_callbacks('DeltaTuneY-RB', self._delta_tuney)
+
+            self._estimate_current_deltatune()
+
+            self.run_callbacks('Log-Mon', 'Updated KL references.')
+            return True
+
+        self.run_callbacks(
+            'Log-Mon', 'ERR: Some magnet family is disconnected.')
+        return False
+
+    def _estimate_current_deltatune(self):
+        psfam_deltakl = len(self._psfams)*[0]
+        for fam in self._psfams:
+            fam_idx = self._psfams.index(fam)
+            psfam_deltakl[fam_idx] = \
+                self._psfam_intstr_rb[fam] - self._psfam_refkl[fam]
+        delta_tunex, delta_tuney = \
+            self._opticscorr.calculate_opticsparam(psfam_deltakl)
+        self.run_callbacks('DeltaTuneX-Mon', delta_tunex)
+        self.run_callbacks('DeltaTuneY-Mon', delta_tuney)
+
+    # ---------- callbacks ----------
+
+    def _callback_init_refkl(self, pvname, value, cb_info, **kws):
+        """Initialize RefKL-Mon pvs and remove this callback."""
+        # Get reference
+        if value is None:
+            return
+        fam = _SiriusPVName(pvname).dev
+        self._psfam_refkl[fam] = value
+        self.run_callbacks('RefKL'+fam+'-Mon', self._psfam_refkl[fam])
+
+        # Remove callback
+        cb_info[1].remove_callback(cb_info[0])
+
+    def _callback_estimate_deltatune(self, pvname, value, **kws):
+        if value is None:
+            return
+        fam = _SiriusPVName(pvname).dev
+        self._psfam_intstr_rb[fam] = value
+        self._estimate_current_deltatune()
