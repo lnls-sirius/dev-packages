a04494c1c791e714b67ef62ec8ebe2b545531e75
ximenes <xresende@gmail.com>
2020-04-18 16:48:07 -0300

    Create as_rf config type

---
+++ siriuspy/siriuspy/clientconfigdb/types/as_rf.py
@@ -0,0 +1,29 @@
+"""AS Global configuration."""
+from copy import deepcopy as _dcopy
+
+from .global_config import _pvs_as_rf
+from .global_config import _pvs_bo_rf
+from .global_config import _pvs_si_rf
+
+
+def get_dict():
+    """Return configuration type dictionary."""
+    module_name = __name__.split('.')[-1]
+    _dict = {
+        'config_type_name': module_name,
+        'value': _dcopy(_template_dict),
+        'check': False,
+    }
+    return _dict
+
+
+# When using this type of configuration to set the machine,
+# the list of PVs should be processed in the same order they are stored
+# in the configuration. The second numeric parameter in the pair is the
+# delay [s] the client should wait before setting the next PV.
+
+
+_template_dict = {
+    'pvs':
+    _pvs_as_rf + _pvs_bo_rf + _pvs_si_rf
+    }
