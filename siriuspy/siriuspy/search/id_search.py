19df0452f1894e96cdb32ba7b82131e2ca495448
anacso17 <anacso1777@gmail.com>
2020-06-04 14:19:10 -0300

    IDSearch.ENH: change to use beamline <-> id relation

--- siriuspy/siriuspy/search/id_search.py
+++ siriuspy/siriuspy/search/id_search.py
@@ -4,37 +4,42 @@
 class IDSearch:
     """ID Search Class."""
 
-    _beamline2subsection = {
-        'Carnauba': '06SB',
-        'Caterete': '07SP',
-        'Ema': '08SB',
-        'Manaca': '09SA',
-        'Sabia': '10SB',
-        'Ipe': '11SP',
-        'PGM++': '12SB',
+    _beamline2idname = {
+        'CARNAUBA': 'SI-06SB:ID-Delta21',
+        'CATERETE': 'SI-07SP:ID-Delta21',
+        'EMA':      'SI-08SB:ID-APU19',
+        'MANACA':   'SI-09SA:ID-APU22',
+        'SABIA':    'SI-10SB:ID-Delta52',
+        'IPE':      'SI-11SP:ID-Delta52',
+        'COLIBRI':  'SI-12SB:ID-Delta52',
+        'HARPIA':   'SI-14SB:ID-Delta21',
+        'SAGUI':    'SI-15SP:ID-Delta21',
+        'INGA':     'SI-16SB:ID-Delta21',
+        'SAPUCAIA': 'SI-17SA:ID-APU19',
+        'PAINEIRA': 'SI-18SB:ID-Delta21',
     }
-    _subsection2beamline = {v: k for k, v in _beamline2subsection.items()}
+    _idname2beamline = {v: k for k, v in _beamline2idname.items()}
 
     @staticmethod
-    def conv_subsection_2_beamline(subsection):
+    def conv_idname_2_beamline(idname):
         """Return the beam line name corresponding to sector."""
-        if subsection in IDSearch._subsection2beamline.keys():
-            return IDSearch._subsection2beamline[subsection]
+        if idname in IDSearch._idname2beamline.keys():
+            return IDSearch._idname2beamline[idname]
         return None
 
     @staticmethod
-    def conv_beamline_2_subsection(beamline):
-        """Return the beam line name corresponding to subsection."""
-        if beamline in IDSearch._beamline2subsection.keys():
-            return IDSearch._beamline2subsection[beamline]
+    def conv_beamline_2_idname(beamline):
+        """Return the beam line name corresponding to idname."""
+        if beamline in IDSearch._beamline2idname.keys():
+            return IDSearch._beamline2idname[beamline]
         return None
 
     @staticmethod
-    def get_subsection_2_beamline_dict():
-        """Return subsection to beamline dictionary."""
-        return IDSearch._subsection2beamline.copy()
+    def get_idname_2_beamline_dict():
+        """Return idname to beamline dictionary."""
+        return IDSearch._idname2beamline.copy()
 
     @staticmethod
-    def get_beamline_2_subsection_dict():
-        """Return beamline to subsection dictionary."""
-        return IDSearch._beamline2subsection.copy()
+    def get_beamline_2_idname_dict():
+        """Return beamline to idname dictionary."""
+        return IDSearch._beamline2idname.copy()
