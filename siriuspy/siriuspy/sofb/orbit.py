092df03b84e680b16a72f9178fcc121e25b7283a
Fernando <fernandohds564@gmail.com>
2020-07-22 18:11:41 -0300

    SOFB.BUG: Fix orbit.shutdown method.

--- siriuspy/siriuspy/sofb/orbit.py
+++ siriuspy/siriuspy/sofb/orbit.py
@@ -134,11 +134,10 @@ class EpicsOrbit(BaseOrbit):
             proc.start()
 
     def shutdown(self):
+        """."""
         if self.acc == 'SI':
             for pipe in self._mypipes:
-                if pipe.poll():
-                    pipe.recv()
-                pipe.send(False)
+                pipe.close()
             for proc in self._processes:
                 proc.join()
 
