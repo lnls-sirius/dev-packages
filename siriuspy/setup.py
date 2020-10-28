a00c8e3bb7bde59b22ee7b71e7da23a50d5b51b6
carneirofc <claudiofcarneiro@hotmail.com>
2020-10-28 10:43:22 -0300

    siriuspy: Include python_requires and long_description

--- siriuspy/setup.py
+++ siriuspy/setup.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python-sirius
 
-"""Siriuspy setup script."""
+'''Siriuspy setup script.'''
 
 
 from setuptools import setup, find_packages
+
+with open('README.md', 'r') as _f:
+    _long_description = _f.read().strip()
 
 with open('VERSION', 'r') as _f:
     __version__ = _f.read().strip()
@@ -16,17 +19,20 @@ setup(
     version=__version__,
     author='lnls-sirius',
     description='Development packages for Sirius',
+    long_description=_long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/lnls-sirius/dev-packages',
     download_url='https://github.com/lnls-sirius/dev-packages',
     license='GNU GPLv3',
     classifiers=[
         'Intended Audience :: Science/Research',
         'Programming Language :: Python',
-        'Topic :: Scientific/Engineering'
+        'Topic :: Scientific/Engineering',
     ],
     packages=find_packages(),
     install_requires=_requirements,
     package_data={'siriuspy': ['VERSION']},
     test_suite='tests',
-    zip_safe=False
+    python_requires='>=3.4',
+    zip_safe=False,
 )
