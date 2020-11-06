852df98d1447fae302cfb1149d28ceb7967742ec
carneirofc <claudiofcarneiro@hotmail.com>
2020-11-06 16:35:13 -0300

    pypi: Add MANIFEST.in file.

--- siriuspy/setup.py
+++ siriuspy/setup.py
@@ -1,38 +1,44 @@
 #!/usr/bin/env python-sirius
 
-'''Siriuspy setup script.'''
+"""Siriuspy setup script."""
+
+from setuptools import setup, find_namespace_packages, find_packages
+import pkg_resources
 
 
-from setuptools import setup, find_packages
+def get_abs_path(relative):
+    return pkg_resources.resource_filename(__name__, relative)
 
-with open('README.md', 'r') as _f:
+
+with open(get_abs_path("README.md"), "r") as _f:
     _long_description = _f.read().strip()
 
-with open('VERSION', 'r') as _f:
+with open(get_abs_path("VERSION"), "r") as _f:
     __version__ = _f.read().strip()
 
-with open('requirements.txt', 'r') as _f:
-    _requirements = _f.read().strip().split('\n')
+with open(get_abs_path("requirements.txt"), "r") as _f:
+    _requirements = _f.read().strip().split("\n")
 
 setup(
-    name='siriuspy',
+    name="siriuspy",
     version=__version__,
-    author='lnls-sirius',
-    description='Development packages for Sirius',
+    author="lnls-sirius",
+    description="Development packages for Sirius",
     long_description=_long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/lnls-sirius/dev-packages',
-    download_url='https://github.com/lnls-sirius/dev-packages',
-    license='GNU GPLv3',
+    long_description_content_type="text/markdown",
+    url="https://github.com/lnls-sirius/dev-packages",
+    download_url="https://github.com/lnls-sirius/dev-packages",
+    license="GNU GPLv3",
     classifiers=[
-        'Intended Audience :: Science/Research',
-        'Programming Language :: Python',
-        'Topic :: Scientific/Engineering',
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering",
     ],
     packages=find_packages(),
     install_requires=_requirements,
-    package_data={'siriuspy': ['VERSION']},
-    test_suite='tests',
-    python_requires='>=3.4',
+    package_data={"siriuspy": ["VERSION"]},
+    include_package_data=True,
+    test_suite="tests",
+    python_requires=">=3.4",
     zip_safe=False,
 )
