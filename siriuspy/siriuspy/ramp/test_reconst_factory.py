9e2abc6c45fa9f6ad6c3712a91236cfa04ca1223
anacso17 <anacso1777@gmail.com>
2021-02-09 11:26:37 -0300

    ramp.MNT: update reconstruction factories tests

--- siriuspy/siriuspy/ramp/test_reconst_factory.py
+++ siriuspy/siriuspy/ramp/test_reconst_factory.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 from siriuspy.clientconfigdb import ConfigDBDocument
 from siriuspy.ramp.ramp import BoosterRamp
 from siriuspy.ramp.waveform import Waveform
+from siriuspy.ramp.magnet import get_magnet
 from siriuspy.ramp.reconst_factory import BONormListFactory, BORFRampFactory, \
     BOTIRampFactory, BODipRampFactory
 
@@ -28,13 +29,14 @@ R_ORIG = BoosterRamp('testing')
 R_ORIG.load()
 
 GLOBAL_CONF = ConfigDBDocument(
-    # config_type='global_config', name='ConfigB_adjust_rffreq')
     config_type='global_config', name='ref_config')
 GLOBAL_CONF.load()
 
 
 def run():
     if ARGS.factory == 'BONormListFactory':
+        dip_wav = R_ORIG.ps_waveform_get('BO-Fam:PS-B-1')
+
         ps2wfm = dict()
         # for psn in PSNAMES:
         #     if psn in ('BO-Fam:PS-B-1', 'BO-Fam:PS-B-2'):
@@ -53,7 +55,9 @@ def run():
         fac = BONormListFactory(
             ramp_config=R_ORIG, waveforms=ps2wfm,
             opt_global=False, opt_times=False,
-            opt_metric='strength')
+            opt_metric='current', use_config_times=True,
+            use_straigth_estim=True,
+            consider_beam_interval=False)
         # fac.read_waveforms()
 
         r_built = BoosterRamp('config_test')
@@ -91,7 +95,7 @@ def run():
             setattr(r_built, attr, getattr(R_ORIG, attr))
 
         r_built.ps_normalized_configs_set(fac.normalized_configs)
-        times = r_built.ps_normalized_configs_times
+        ntimes = r_built.ps_normalized_configs_times
         print(r_built)
 
         des_prec = fac.desired_reconstr_precision
@@ -99,14 +103,15 @@ def run():
         print('Precision rechead?', prec_reached[0])
         print('Maximum abs.error:', prec_reached[1])
 
-        dip_wav = R_ORIG.ps_waveform_get('BO-Fam:PS-B-1')
-        ax = plt.gca()
-        ax.grid()
+        max_error = 0.0
+
+        axs = plt.gca()
+        axs.grid()
         for psn in PSNAMES:
             if psn in ('BO-Fam:PS-B-1', 'BO-Fam:PS-B-2'):
                 continue
             strgs = list()
-            for tim in times:
+            for tim in ntimes:
                 strgs.append(r_built[tim][psn])
             strgs = _np.array(strgs)
 
@@ -120,11 +125,18 @@ def run():
             built_times = r_built.ps_waveform_get_times(psn)
             built_currs = r_built.ps_waveform_get_currents(psn)
             built_strgs = r_built.ps_waveform_get_strengths(psn)
-            dif = orig_currs - built_currs
-            if not _np.allclose(dif, 0, atol=des_prec):
-                ax.plot(orig_times, orig_strgs, 'r.-', label=psn)
-                ax.plot(built_times, built_strgs, 'k.-')
-                ax.plot(times, strgs, 'b.')
+            dif_curr = orig_currs - built_currs
+            dif_strg = orig_strgs - built_strgs
+            max_error = max(max_error, max(dif_curr))
+            if not _np.allclose(dif_curr, 0, atol=des_prec):
+                # axs.plot(orig_times, orig_strgs, 'r.-', label=psn)
+                # axs.plot(built_times, built_strgs, 'k.-')
+                # axs.plot(ntimes, strgs, 'b.')
+                # axs.plot(built_times, dif_curr, label=psn)
+                axs.plot(built_times, dif_strg, label=psn)
+
+        # for tim in ntimes:
+        #     axs.plot([tim, tim], [-max_error, max_error], 'k-.')
         plt.legend()
         plt.title('Curves with error > than desired ({})'.format(des_prec))
         plt.show()
@@ -140,34 +152,44 @@ def run():
         _print_param_dict(params)
 
     elif ARGS.factory == 'BODipRampFactory':
-        wav = R_ORIG.ps_waveform_get_currents('BO-Fam:PS-B-1')
-        fac = BODipRampFactory(ramp_config=R_ORIG, waveform=wav)
-        params_init = fac._get_initial_params()
+        dip_name = 'BO-Fam:PS-B-1'
+        dip_mag = get_magnet('BO-Fam:MA-B')
+
+        # wav_orig = R_ORIG.ps_waveform_get_currents(dip_name)
+
+        for row in GLOBAL_CONF.value['pvs']:
+            if row[0] == dip_name + ':Wfm-SP':
+                wav_orig = row[1]
+                break
+
+        wav_energy = dip_mag.conv_current_2_strength(wav_orig)
+
+        fac = BODipRampFactory(ramp_config=R_ORIG, waveform=wav_orig)
         # fac.read_waveforms()
         params_final = fac.dip_params
-        print('initial')
-        _print_param_dict(params_init)
-        print('final')
         _print_param_dict(params_final)
 
-        r_built_init = _dcopy(R_ORIG)
-        for name, value in params_init.items():
-            setattr(r_built_init, 'ps_ramp_' + name, value)
-        wav_new_init = r_built_init.ps_waveform_get_currents('BO-Fam:PS-B-1')
-        wav_err_init = wav - wav_new_init
-
-        r_built_final = _dcopy(R_ORIG)
+        w_built = _dcopy(R_ORIG.ps_waveform_get(dip_name))
         for name, value in params_final.items():
-            setattr(r_built_final, 'ps_ramp_' + name, value)
-        wav_new_final = r_built_final.ps_waveform_get_currents('BO-Fam:PS-B-1')
-        wav_err_final = wav - wav_new_final
+            setattr(w_built, name, value)
+        wav_curr = w_built.currents
+        err_curr = wav_orig - wav_curr
+        wav_strgt = w_built.strengths
+        err_strgt = wav_energy - wav_strgt
 
-        axis = plt.gca()
-        axis.plot(_np.arange(len(wav)), wav_err_init, 'k', label='initial')
-        axis.plot(_np.arange(len(wav)), wav_err_final, 'r--', label='final')
-        axis.grid()
-        plt.title('Fit errors')
-        plt.legend()
+        success = _np.allclose(err_curr, 0, atol=1e-5)
+        max_error = _np.max(_np.abs(err_curr))
+        print('Precision rechead?', success)
+        print('Maximum abs.error:', max_error)
+
+        fig, axs = plt.subplots(1, 2)
+        fig.set_size_inches(12, 6)
+        axs[0].plot(err_curr)
+        axs[0].grid()
+        axs[0].set_title('Current error')
+        axs[1].plot(err_strgt)
+        axs[1].grid()
+        axs[1].set_title('Strength error')
         plt.show()
 
 
