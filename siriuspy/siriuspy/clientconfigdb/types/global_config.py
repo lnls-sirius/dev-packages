b67c33f27c3b6868fcdedf168b5988c044a7c6ba
Fernando <fernandohds564@gmail.com>
2021-03-10 10:40:02 -0300

    CONFIG.BUG: remove PU PwrState and Pulse PVs from global config.

--- siriuspy/siriuspy/clientconfigdb/types/global_config.py
+++ siriuspy/siriuspy/clientconfigdb/types/global_config.py
@@ -991,26 +991,6 @@ _pvs_as_pu = [
     ['TS-04:PU-InjSeptF:Voltage-SP', 0.0, 0.0],  # [V]
     ['TS-01:PU-EjeSeptG:Voltage-SP', 0.0, 0.0],  # [V]
     ['TS-01:PU-EjeSeptF:Voltage-SP', 0.0, 0.1],  # [V]
-    ['TB-04:PU-InjSept:Pulse-Sel', 0, 0.0],
-    ['BO-01D:PU-InjKckr:Pulse-Sel', 0, 0.0],
-    ['BO-48D:PU-EjeKckr:Pulse-Sel', 0, 0.0],
-    ['SI-01SA:PU-InjDpKckr:Pulse-Sel', 0, 0.0],
-    ['SI-01SA:PU-InjNLKckr:Pulse-Sel', 0, 0.0],
-    ['TS-04:PU-InjSeptG-1:Pulse-Sel', 0, 0.0],
-    ['TS-04:PU-InjSeptG-2:Pulse-Sel', 0, 0.0],
-    ['TS-04:PU-InjSeptF:Pulse-Sel', 0, 0.0],
-    ['TS-01:PU-EjeSeptG:Pulse-Sel', 0, 0.0],
-    ['TS-01:PU-EjeSeptF:Pulse-Sel', 0, 0.1],
-    ['TB-04:PU-InjSept:PwrState-Sel', _OFF, 0.0],
-    ['BO-01D:PU-InjKckr:PwrState-Sel', _OFF, 0.0],
-    ['BO-48D:PU-EjeKckr:PwrState-Sel', _OFF, 0.0],
-    ['SI-01SA:PU-InjDpKckr:PwrState-Sel', _OFF, 0.0],
-    ['SI-01SA:PU-InjNLKckr:PwrState-Sel', _OFF, 0.0],
-    ['TS-04:PU-InjSeptG-1:PwrState-Sel', _OFF, 0.0],
-    ['TS-04:PU-InjSeptG-2:PwrState-Sel', _OFF, 0.0],
-    ['TS-04:PU-InjSeptF:PwrState-Sel', _OFF, 0.0],
-    ['TS-01:PU-EjeSeptG:PwrState-Sel', _OFF, 0.0],
-    ['TS-01:PU-EjeSeptF:PwrState-Sel', _OFF, 0.0],
     ]
 
 
