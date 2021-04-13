6d7e2efd4d40c20e1d1f9e13959fd632f27f56d0
anacso17 <anacso1777@gmail.com>
2021-04-13 20:46:41 -0300

    clientarch.ENH: add new version of ClientArchiver
    - use aiohttp and asyncio in requests for better performance
    - add option to pass a list/tuple of timestamps to perform parallelized requests
    - implement connected method
    - add logout method to properly close the connection when the user logs in

--- siriuspy/siriuspy/clientarch/client.py
+++ siriuspy/siriuspy/clientarch/client.py
@@ -4,9 +4,13 @@
 See https://slacmshankar.github.io/epicsarchiver_docs/userguide.html
 """
 
-from urllib import parse as _parse
-import requests
+import asyncio as _asyncio
+import urllib as _urllib
+import ssl as _ssl
 import urllib3 as _urllib3
+from aiohttp import ClientSession as _ClientSession
+
+import numpy as _np
 
 from .. import envars as _envars
 
@@ -25,7 +29,7 @@ class ClientArchiver:
     ENDPOINT = '/mgmt/bpl'
 
     def __init__(self, server_url=None):
-        """."""
+        """Initialize."""
         self.session = None
         self.timeout = _TIMEOUT
         self._url = server_url or self.SERVER_URL
@@ -34,39 +38,58 @@ class ClientArchiver:
 
     @property
     def connected(self):
-        """."""
-        # TODO: choose minimal request command in order to check connection.
-        raise NotImplementedError
+        """Connected."""
+        try:
+            status = _urllib.request.urlopen(
+                self._url, timeout=self.timeout,
+                context=_ssl.SSLContext()).status
+            return status == 200
+        except _urllib.error.URLError:
+            return False
 
     def login(self, username, password):
-        """."""
+        """Open login session."""
         headers = {"User-Agent": "Mozilla/5.0"}
         payload = {"username": username, "password": password}
         url = self._create_url(method='login')
-        self.session = requests.Session()
-        response = self.session.post(
-            url, headers=headers, data=payload, verify=False)
-        return b"authenticated" in response.content
+        loop = _asyncio.get_event_loop()
+        self.session, authenticated = loop.run_until_complete(
+            self._create_session(
+                url, headers=headers, payload=payload, ssl=False))
+        if authenticated:
+            print('Reminder: close connection after using this '
+                  'session by calling logout method!')
+        else:
+            self.logout()
+        return authenticated
+
+    def logout(self):
+        """Close login session."""
+        if self.session:
+            loop = _asyncio.get_event_loop()
+            resp = loop.run_until_complete(self._close_session())
+            self.session = None
+            return resp
+        return None
 
     def getPVsInfo(self, pvnames):
-        """."""
+        """Get PVs Info."""
         if isinstance(pvnames, (list, tuple)):
             pvnames = ','.join(pvnames)
         url = self._create_url(method='getPVStatus', pv=pvnames)
-        req = self._make_request(url)
-        if not req.ok:
-            return None
-        return req.json()
+        resp = self._make_request(url, return_json=True)
+        return None if not resp else resp
 
     def getAllPVs(self, pvnames):
-        """."""
+        """Get All PVs."""
         if isinstance(pvnames, (list, tuple)):
             pvnames = ','.join(pvnames)
         url = self._create_url(method='getAllPVs', pv=pvnames, limit='-1')
-        return self._make_request(url).json()
+        resp = self._make_request(url, return_json=True)
+        return None if not resp else resp
 
     def deletePVs(self, pvnames):
-        """."""
+        """Delete PVs."""
         if not isinstance(pvnames, (list, tuple)):
             pvnames = (pvnames, )
         for pvname in pvnames:
@@ -75,12 +98,13 @@ class ClientArchiver:
             self._make_request(url, need_login=True)
 
     def getPausedPVsReport(self):
-        """."""
+        """Get Paused PVs Report."""
         url = self._create_url(method='getPausedPVsReport')
-        return self._make_request(url).json()
+        resp = self._make_request(url, return_json=True)
+        return None if not resp else resp
 
     def pausePVs(self, pvnames):
-        """."""
+        """Pause PVs."""
         if not isinstance(pvnames, (list, tuple)):
             pvnames = (pvnames, )
         for pvname in pvnames:
@@ -88,12 +112,12 @@ class ClientArchiver:
             self._make_request(url, need_login=True)
 
     def renamePV(self, oldname, newname):
-        """."""
+        """Rename PVs."""
         url = self._create_url(method='renamePV', pv=oldname, newname=newname)
-        self._make_request(url, need_login=True)
+        return self._make_request(url, need_login=True)
 
     def resumePVs(self, pvnames):
-        """."""
+        """Resume PVs."""
         if not isinstance(pvnames, (list, tuple)):
             pvnames = (pvnames, )
         for pvname in pvnames:
@@ -120,8 +144,16 @@ class ClientArchiver:
         stddev -- number of standard deviations.
                   argument used in processing 'ignoreflyers' and 'flyers'.
         """
-        tstart = _parse.quote(timestamp_start)
-        tstop = _parse.quote(timestamp_stop)
+        if isinstance(timestamp_start, str):
+            timestamp_start = [timestamp_start, ]
+        if isinstance(timestamp_stop, str):
+            timestamp_stop = [timestamp_stop, ]
+        if not isinstance(timestamp_start, (list, tuple)) or \
+                not isinstance(timestamp_stop, (list, tuple)):
+            raise TypeError(
+                "'timestampstart' and 'timestamp_stop' arguments must be "
+                "timestamp strings or iterable.")
+
         if process_type:
             process_str = process_type
             if interval is not None:
@@ -129,50 +161,61 @@ class ClientArchiver:
                 if 'flyers' in process_type and stddev is not None:
                     process_str += '_' + str(int(stddev))
             pvname = process_str + '(' + pvname + ')'
-        url = self._create_url(
-            method='getData.json', pv=pvname, **{'from': tstart, 'to': tstop})
+
         if get_request_url:
+            tstart = _urllib.parse.quote(timestamp_start[0])
+            tstop = _urllib.parse.quote(timestamp_stop[-1])
+            url = self._create_url(
+                method='getData.json', pv=pvname,
+                **{'from': tstart, 'to': tstop})
             return url
-        req = self._make_request(url)
-        if not req.ok:
+
+        urls = []
+        for tstart, tstop in zip(timestamp_start, timestamp_stop):
+            urls.append(self._create_url(
+                method='getData.json', pv=pvname,
+                **{'from': _urllib.parse.quote(tstart),
+                   'to': _urllib.parse.quote(tstop)}))
+
+        resps = self._make_request(urls, return_json=True)
+        if any([not resp for resp in resps]):
             return None
-        ans = req.json()
-        data = ans[0]['data']
-        value = [v['val'] for v in data]
-        timestamp = [v['secs'] + v['nanos']/1.0e9 for v in data]
-        status = [v['status'] for v in data]
-        severity = [v['severity'] for v in data]
+
+        _ts, _vs, _st, _sv = [], [], [], []
+        for resp in resps:
+            data = resp[0]['data']
+            _ts = _np.r_[_ts, [v['secs'] + v['nanos']/1.0e9 for v in data]]
+            _vs = _np.r_[_vs, [v['val'] for v in data]]
+            _st = _np.r_[_st, [v['status'] for v in data]]
+            _sv = _np.r_[_sv, [v['severity'] for v in data]]
+        if not _ts.size:
+            return None
+        _, _tsidx = _np.unique(_ts, return_index=True)
+        timestamp, value, status, severity = \
+            _ts[_tsidx], _vs[_tsidx], _st[_tsidx], _sv[_tsidx]
+
         return timestamp, value, status, severity
 
     def getPVDetails(self, pvname, get_request_url=False):
-        """."""
+        """Get PV Details."""
         url = self._create_url(
             method='getPVDetails', pv=pvname)
         if get_request_url:
             return url
-        req = self._make_request(url)
-        if not req.ok:
-            return None
-        data = req.json()
-        return data
+        resp = self._make_request(url, return_json=True)
+        return None if not resp else resp
 
-    def _make_request(self, url, need_login=False):
-        if self.session is not None:
-            try:
-                req = self.session.get(url, timeout=self.timeout)
-            except requests.exceptions.ConnectTimeout as err_msg:
-                raise ConnectionError(err_msg)
-        elif need_login:
-            raise AuthenticationError('You need to login first.')
-        else:
-            try:
-                req = requests.get(url, verify=False, timeout=self.timeout)
-            except requests.exceptions.ConnectTimeout as err_msg:
-                raise ConnectionError(err_msg)
-        return req
+    # ---------- auxiliary methods ----------
+
+    def _make_request(self, url, need_login=False, return_json=False):
+        """Make request."""
+        loop = _asyncio.get_event_loop()
+        response = loop.run_until_complete(self._handle_request(
+            url, return_json=return_json, need_login=need_login))
+        return response
 
     def _create_url(self, method, **kwargs):
-        """."""
+        """Create URL."""
         url = self._url
         if method.startswith('getData.json'):
             url += '/retrieval/data'
@@ -183,3 +226,52 @@ class ClientArchiver:
             url += '?'
             url += '&'.join(['{}={}'.format(k, v) for k, v in kwargs.items()])
         return url
+
+    # ---------- async methods ----------
+
+    async def _handle_request(
+            self, url, return_json=False, need_login=False):
+        """Handle request."""
+        if self.session is not None:
+            response = await self._get_request_response(
+                url, self.session, return_json)
+        elif need_login:
+            raise AuthenticationError('You need to login first.')
+        else:
+            async with _ClientSession() as sess:
+                response = await self._get_request_response(
+                    url, sess, return_json)
+        return response
+
+    async def _get_request_response(self, url, session, return_json):
+        """Get request response."""
+        try:
+            if isinstance(url, list):
+                response = await _asyncio.gather(
+                    *[session.get(u, ssl=False, timeout=self.timeout)
+                      for u in url])
+                if return_json:
+                    response = await _asyncio.gather(
+                        *[r.json() for r in response])
+            else:
+                response = await session.get(
+                    url, ssl=False, timeout=self.timeout)
+                if return_json:
+                    response = await response.json()
+        except _asyncio.TimeoutError as err_msg:
+            raise ConnectionError(err_msg)
+        return response
+
+    async def _create_session(self, url, headers, payload, ssl):
+        """Create session and handle login."""
+        session = _ClientSession()
+        async with session.post(
+                url, headers=headers, data=payload, ssl=ssl,
+                timeout=self.timeout) as response:
+            content = await response.content.read()
+            authenticated = b"authenticated" in content
+        return session, authenticated
+
+    async def _close_session(self):
+        """Close session."""
+        return await self.session.close()
