81d9c859a5a74094b62fa20c99bc229e98da1e01
carneirofc <claudiofcarneiro@hotmail.com>
2021-06-02 13:05:28 -0300

    bsmp/ isort

--- siriuspy/siriuspy/bsmp/__init__.py
+++ siriuspy/siriuspy/bsmp/__init__.py
@@ -7,12 +7,11 @@ https://github.com/lnls-sirius/libbsmp/blob/master/doc/protocol_v2-30_pt_BR.pdf
 https://github.com/lnls-sirius/libbsmp/blob/master/doc/protocol_v2-30_en_US.pdf
 """
 
+from .commands import *
+from .entities import *
+from .exceptions import *
 from .serial import *
 from .types import *
-from .entities import *
-from .commands import *
-from .exceptions import *
-
 
 __version__ = '2.30.0'
 del serial, types, entities, commands, exceptions
