7e61fff2a00fde96ce79ae808f94a481f5c9d864
Fernando <fernandohds564@gmail.com>
2021-06-25 15:08:08 -0300

    CLTARC.ENH: Create classes to get Orbit, Correctors strength and TrimsQuad from archiver.

--- siriuspy/siriuspy/clientarch/devices.py
+++ siriuspy/siriuspy/clientarch/devices.py
@@ -4,86 +4,49 @@ from copy import deepcopy as _dcopy
 
 import numpy as _np
 
-from ..search import BPMSearch as _BPMSearch
-from ..util import ClassProperty as _classproperty
+from mathphys.functions import get_namedtuple as _get_namedtuple
+
+from ..search import BPMSearch as _BPMSearch, PSSearch as _PSSearch
 
 from .pvarch import PVDataSet as _PVDataSet
 from .time import Time as _Time
 
 
-class Orbit(_PVDataSet):
+class BaseDevice(_PVDataSet):
     """."""
 
-    _CONV = 0.001  # [nm -> um]
-    _BPMS_TB = None
-    _BPMS_BO = None
-    _BPMS_TS = None
-    _BPMS_SI = None
-    _PBPMS_SI = None
+    _CONV = 0.001
 
-    class DEVICES:
-        """."""
-
-        TB_X = 'TB_X'
-        TB_Y = 'TB_Y'
-        BO_X = 'BO_X'
-        BO_Y = 'BO_Y'
-        TS_X = 'TS_X'
-        TS_Y = 'TS_Y'
-        SI_X = 'SI_X'
-        SI_Y = 'SI_Y'
-        PSI_X = 'PSI_X'
-        PSI_Y = 'PSI_Y'
+    DEVICES = _get_namedtuple('Devices', ('a', ))
+    PROPTIES = _get_namedtuple('Propties', ('a', ))
 
     Time = _Time
 
-    @_classproperty
-    def BPMS_TB(cls):
+    def __init__(self, devname, propty='', connector=None):
         """."""
-        if cls._BPMS_TB is None:
-            cls._BPMS_TB = _BPMSearch.get_names({'sec': 'TB', 'dev': 'BPM'})
-        return _dcopy(cls._BPMS_TB)
-
-    @_classproperty
-    def BPMS_BO(cls):
-        """."""
-        if cls._BPMS_BO is None:
-            cls._BPMS_BO = _BPMSearch.get_names({'sec': 'BO', 'dev': 'BPM'})
-        return _dcopy(cls._BPMS_BO)
-
-    @_classproperty
-    def BPMS_TS(cls):
-        """."""
-        if cls._BPMS_TS is None:
-            cls._BPMS_TS = _BPMSearch.get_names({'sec': 'TS', 'dev': 'BPM'})
-        return _dcopy(cls._BPMS_TS)
-
-    @_classproperty
-    def BPMS_SI(cls):
-        """."""
-        if cls._BPMS_SI is None:
-            cls._BPMS_SI = _BPMSearch.get_names({'sec': 'SI', 'dev': 'BPM'})
-        return _dcopy(cls._BPMS_SI)
-
-    @_classproperty
-    def PBPMS_SI(cls):
-        """."""
-        if cls._PBPMS_SI is None:
-            cls._PBPMS_SI = _BPMSearch.get_names({'sec': 'SI', 'dev': 'PBPM'})
-        return _dcopy(cls._PBPMS_SI)
-
-    def __init__(self, devname, connector=None):
-        """."""
-        self._devname = devname
-        self._bpmnames, pvnames = self._get_pvnames()
+        if devname in self.DEVICES:
+            self._devname = self.DEVICES._fields[devname]
+        elif devname in self.DEVICES._fields:
+            self._devname = devname
+        else:
+            ValueError('Wrong value for devname')
+        if propty in self.PROPTIES:
+            self._propty = propty
+        elif propty in self.PROPTIES._fields:
+            idx = self.PROPTIES._fields.index(propty)
+            self._propty = self.PROPTIES[idx]
+        else:
+            ValueError('Wrong value for propty')
+        self._devnames, pvnames = self._get_pvnames()
         self._times = None
-        self._orbits = None
+        self._values = None
         super().__init__(pvnames, connector=connector)
+        self._parallel_query_bin_interval = 3600
 
     @property
-    def bpmnames(self):
-        """Return ordered list of device BPMs."""
-        return list(self._bpmnames)
+    def devnames(self):
+        """Return ordered list of device names."""
+        return _dcopy(self._devnames)
 
     @property
     def timestamps(self):
@@ -91,63 +54,46 @@ class Orbit(_PVDataSet):
         return self._times
 
     @property
-    def orbits(self):
+    def values(self):
         """Return retrieved orbit interpolated values."""
-        return self._orbits
+        return self._values
 
     def update(self, mean_sec=None, parallel=True):
         """Update state by retrieving data."""
         super().update(mean_sec=mean_sec, parallel=parallel)
 
         # interpolate data
-        self._times, self._orbits = self._interpolate_data(mean_sec)
+        self._times, self._values = self._interpolate_data(mean_sec)
 
     # --- private methods ---
 
     def _get_pvnames(self):
-        if self._devname.startswith('TB'):
-            bpmnames = Orbit.BPMS_TB
-        elif self._devname.startswith('BO'):
-            bpmnames = Orbit.BPMS_BO
-        elif self._devname.startswith('TS'):
-            bpmnames = Orbit.BPMS_TS
-        elif self._devname.startswith('SI'):
-            bpmnames = Orbit.BPMS_SI
-        elif self._devname.startswith('PSI'):
-            bpmnames = Orbit.PBPMS_SI
-        else:
-            raise ValueError('Device not defined!')
-        if self._devname.endswith('X'):
-            propty = ':PosX-Mon'
-        elif self._devname.endswith('Y'):
-            propty = ':PosY-Mon'
-        else:
-            raise ValueError('Device not defined!')
-        pvnames = tuple(bpm + propty for bpm in bpmnames)
-        return bpmnames, pvnames
+        devnames = []
+        pvnames = []
+        return devnames, pvnames
 
     def _interpolate_data(self, mean_sec):
-        # calc avg_dtime if not passed
-        nr_bpms = len(self._pvdata)
+        # calc mean_sec if not passed
+        nr_pvs = len(self._pvdata)
         if mean_sec is None:
             mean_sec = sum(map(
                 lambda p: p.timestamp[1]-p.timestamp[0],
                 self._pvdata.values()))
-            mean_sec /= nr_bpms
+            mean_sec /= nr_pvs
 
         # times vector
         t0_, t1_ = self.timestamp_start, self.timestamp_stop
         times = _np.arange(t0_, t1_, mean_sec)
 
         # builds orbit matrix using interpolation
-        orbits = _np.zeros((len(times), nr_bpms), dtype=float)
+        values = _np.zeros((len(times), nr_pvs), dtype=float)
         for i, pvname in enumerate(self._pvnames):
             pvdata = self._pvdata[pvname]
-            orbits[:, i] = _np.interp(times, pvdata.timestamp, pvdata.value)
-        orbits *= Orbit._CONV
+            values[:, i] = _np.interp(times, pvdata.timestamp, pvdata.value)
+        values *= Orbit._CONV
 
         # return data
-        return times, orbits
+        return times, values
 
     def __getitem__(self, index):
         """Return raw timestamp and value tuple data for a given BPM."""
@@ -156,10 +102,99 @@ class Orbit(_PVDataSet):
         elif isinstance(index, str):
             if index in self._pvnames:
                 pvname = index
-            elif index in self._bpmnames:
-                index = self._bpmnames.index(index)
+            elif index in self._devnames:
+                index = self._devnames.index(index)
                 pvname = self._pvnames[index]
             else:
                 raise IndexError
         pvdata = self._pvdata[pvname]
         return pvdata.timestamp, pvdata.value * Orbit._CONV
+
+
+class Orbit(BaseDevice):
+    """."""
+
+    _CONV = 0.001  # [nm -> um]
+
+    DEVICES = _get_namedtuple(
+        'Devices', ('TB_BPM', 'TS_BPM', 'BO_BPM', 'SI_BPM', 'SI_PBPM'))
+    PROPTIES = _get_namedtuple(
+        'Propties',
+        ('PosX', 'PosY', 'PosQ', 'Sum'),
+        ('PosX-Mon', 'PosY-Mon', 'PosQ-Mon', 'Sum-Mon'))
+
+    def __init__(self, devname='SI_BPM', propty='PosX-Mon', connector=None):
+        """."""
+        super().__init__(devname, propty=propty, connector=connector)
+
+    # --- private methods ---
+    def _get_pvnames(self):
+        sec, dev = self._devname.split('_')
+        devnames = _dcopy(_BPMSearch.get_names({'sec': sec, 'dev': dev}))
+        pvnames = tuple(dev+':'+self._propty for dev in devnames)
+        return devnames, pvnames
+
+
+class Correctors(BaseDevice):
+    """."""
+
+    _CONV = 1  # [urad -> urad]
+
+    DEVICES = _get_namedtuple(
+        'Devices',
+        (
+            'TB_CH', 'TB_CV', 'TS_CH', 'TS_CV',
+            'BO_CH', 'BO_CV', 'SI_CH', 'SI_CV', 'SI_FCH', 'SI_FCV'
+        )
+    )
+    PROPTIES = _get_namedtuple(
+        'Propties',
+        (
+            'Curr_Mon', 'Curr_Ref', 'Curr_SP', 'Curr_RB',
+            'Kick_Mon', 'Kick_Ref', 'Kick_SP', 'Kick_RB',),
+        (
+            'Current-Mon', 'CurrentRef-Mon', 'Current-SP', 'Current-RB',
+            'Kick-Mon', 'KickRef-Mon', 'Kick-SP', 'Kick-RB'))
+
+    def __init__(self, devname='SI_CH', propty='Current-Mon', connector=None):
+        """."""
+        super().__init__(devname, propty=propty, connector=connector)
+
+    # --- private methods ---
+    def _get_pvnames(self):
+        sec, dev = self._devname.split('_')
+        devnames = _PSSearch.get_psnames({'sec': sec, 'dev': dev})
+        pvnames = tuple(dev+':'+self._propty for dev in devnames)
+        return devnames, pvnames
+
+
+class TrimQuads(BaseDevice):
+    """."""
+
+    _CONV = 1  # [urad -> urad]
+
+    DEVICES = _get_namedtuple(
+        'Devices', ('TB_QN', 'TS_QN', 'SI_QN', 'SI_QS'))
+    PROPTIES = _get_namedtuple(
+        'Propties',
+        (
+            'Curr_Mon', 'Curr_Ref', 'Curr_SP', 'Curr_RB',
+            'KL_Mon', 'KL_Ref', 'KL_SP', 'KL_RB',),
+        (
+            'Current-Mon', 'CurrentRef-Mon', 'Current-SP', 'Current-RB',
+            'KL-Mon', 'KLRef-Mon', 'KL-SP', 'KL-RB'))
+
+    def __init__(self, devname='SI_QS', propty='Current-Mon', connector=None):
+        """."""
+        super().__init__(devname, propty=propty, connector=connector)
+
+    # --- private methods ---
+    def _get_pvnames(self):
+        sec, dev = self._devname.split('_')
+        if dev == 'QN':
+            devnames = _PSSearch.get_psnames({'sec': sec, 'dev': 'Q[DF1-9]'})
+            devnames = [dev for f in devnames if 'Fam' not in dev]
+        else:
+            devnames = _PSSearch.get_psnames({'sec': sec, 'dev': dev})
+        pvnames = tuple(dev+':'+self._propty for dev in devnames)
+        return devnames, pvnames
