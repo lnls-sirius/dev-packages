46942cd86d6f5e50a8c25df6fe7d3bfc3c057bc5
anacso17 <anacso1777@gmail.com>
2021-07-19 10:54:15 -0300

    devices.ENH: add first version of EGPulsePS device

--- siriuspy/siriuspy/devices/egun.py
+++ siriuspy/siriuspy/devices/egun.py
@@ -259,3 +259,138 @@ class EGTriggerPS(_Device):
     def is_on(self):
         """."""
         return self['enablereal'] == 1
+
+
+class EGPulsePS(_Device):
+    """Egun Pulse Power Supply Device."""
+
+    DEF_TIMEOUT = 10  # [s]
+
+    class DEVICES:
+        """Devices names."""
+
+        LI = 'LI-01:EG-PulsePS'
+        ALL = (LI, )
+
+    _properties = (
+        'singleselect', 'singleselstatus', 'singleswitch', 'singleswstatus',
+        'multiselect', 'multiselstatus', 'multiswitch', 'multiswstatus',
+        'poweroutsoft', 'powerinsoft')
+
+    def __init__(self, devname=DEVICES.LI):
+        """Init."""
+        # check if device exists
+        if devname not in EGPulsePS.DEVICES.ALL:
+            raise NotImplementedError(devname)
+
+        # call base class constructor
+        super().__init__(devname, properties=EGPulsePS._properties)
+
+    @property
+    def single_bunch_mode(self):
+        """Single bunch mode."""
+        return self['singleselstatus']
+
+    @single_bunch_mode.setter
+    def single_bunch_mode(self, value):
+        self['singleselect'] = value
+
+    @property
+    def single_bunch_switch(self):
+        """Single bunch switch."""
+        return self['singleswstatus']
+
+    @single_bunch_switch.setter
+    def single_bunch_switch(self, value):
+        self['singleswitch'] = value
+
+    @property
+    def multi_bunch_mode(self):
+        """Multi bunch mode."""
+        return self['multiselstatus']
+
+    @multi_bunch_mode.setter
+    def multi_bunch_mode(self, value):
+        self['multiselect'] = value
+
+    @property
+    def multi_bunch_switch(self):
+        """Multi bunch switch."""
+        return self['multiswstatus']
+
+    @multi_bunch_switch.setter
+    def multi_bunch_switch(self, value):
+        self['multiswitch'] = value
+
+    @property
+    def power(self):
+        """Power."""
+        return self['powerinsoft']
+
+    @power.setter
+    def power(self, value):
+        self['poweroutsoft'] = value
+
+    def cmd_turn_off_single_bunch_switch(self, timeout=DEF_TIMEOUT):
+        """Turn off single bunch switch."""
+        self.single_bunch_switch = 0
+        return self._wait('singleswstatus', 0, timeout=timeout)
+
+    def cmd_turn_off_single_bunch_mode(self, timeout=DEF_TIMEOUT):
+        """Turn off single bunch mode."""
+        self.single_bunch_mode = 0
+        return self._wait('singleselstatus', 0, timeout=timeout)
+
+    def cmd_turn_off_single_bunch(self, timeout=DEF_TIMEOUT):
+        """Turn off single bunch."""
+        if not self.cmd_turn_off_single_bunch_switch():
+            return False
+        return self.cmd_turn_off_single_bunch_mode()
+
+    def cmd_turn_on_single_bunch_switch(self, timeout=DEF_TIMEOUT):
+        """Turn on single bunch switch."""
+        self.single_bunch_switch = 1
+        return self._wait('singleswstatus', 1, timeout=timeout)
+
+    def cmd_turn_on_single_bunch_mode(self, timeout=DEF_TIMEOUT):
+        """Turn on single bunch mode."""
+        self.single_bunch_mode = 1
+        return self._wait('singleselstatus', 1, timeout=timeout)
+
+    def cmd_turn_on_single_bunch(self, timeout=DEF_TIMEOUT):
+        """Turn on single bunch."""
+        if not self.cmd_turn_on_single_bunch_mode():
+            return False
+        return self.cmd_turn_on_single_bunch_switch()
+
+    def cmd_turn_off_multi_bunch_switch(self, timeout=DEF_TIMEOUT):
+        """Turn off multi bunch switch."""
+        self.multi_bunch_switch = 0
+        return self._wait('multiswstatus', 0, timeout=timeout)
+
+    def cmd_turn_off_multi_bunch_mode(self, timeout=DEF_TIMEOUT):
+        """Turn off multi bunch mode."""
+        self.multi_bunch_mode = 0
+        return self._wait('multiselstatus', 0, timeout=timeout)
+
+    def cmd_turn_off_multi_bunch(self, timeout=DEF_TIMEOUT):
+        """Turn off multi bunch."""
+        if not self.cmd_turn_off_multi_bunch_switch():
+            return False
+        return self.cmd_turn_off_multi_bunch_mode()
+
+    def cmd_turn_on_multi_bunch_switch(self, timeout=DEF_TIMEOUT):
+        """Turn on multi bunch switch."""
+        self.multi_bunch_switch = 1
+        return self._wait('multiswstatus', 1, timeout=timeout)
+
+    def cmd_turn_on_multi_bunch_mode(self, timeout=DEF_TIMEOUT):
+        """Turn on multi bunch mode."""
+        self.multi_bunch_mode = 1
+        return self._wait('multiselstatus', 1, timeout=timeout)
+
+    def cmd_turn_on_multi_bunch(self, timeout=DEF_TIMEOUT):
+        """Turn on multi bunch."""
+        if not self.cmd_turn_on_multi_bunch_mode():
+            return False
+        return self.cmd_turn_on_multi_bunch_switch()
