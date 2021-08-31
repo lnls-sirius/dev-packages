b45d1ee81777f53c99dd8b8e0f89656561cec9bf
anacso17 <anacso1777@gmail.com>
2021-08-31 17:12:25 -0300

    devices.WIP: add first version of methods to set EGun HVPS voltage and FilaPS Current
    (partially based on old scripts used in commissioning)

--- siriuspy/siriuspy/devices/egun.py
+++ siriuspy/siriuspy/devices/egun.py
@@ -1,10 +1,13 @@
 """E-Gun devices."""
 
 import time as _time
+from datetime import timedelta as _timedelta
+import numpy as _np
 
 from ..pwrsupply.psctrl.pscstatus import PSCStatus as _PSCStatus
 
-from .device import Device as _Device, Devices as _Devices
+from .device import Device as _Device, Devices as _Devices, \
+    DeviceNC as _DeviceNC
 from .timing import Trigger
 
 
@@ -132,6 +135,14 @@ class EGFilament(_Device):
         """."""
         return self['swstatus'] == self.PWRSTATE.On
 
+    def wait_current(self, value, tol=0.20, timeout=DEF_TIMEOUT):
+        """Wait current to reach value with tolerance 'tol'."""
+        _t0 = _time.time()
+        while _time.time() - _t0 < timeout:
+            if abs(self.current - value) < tol:
+                return True
+        return False
+
 
 class EGHVPS(_Device):
     """Egun High-Voltage Power Supply Device."""
@@ -203,6 +214,14 @@ class EGHVPS(_Device):
         ison = self['enstatus'] == self.PWRSTATE.On
         ison &= self['swstatus'] == self.PWRSTATE.On
         return ison
+
+    def wait_voltage(self, value, tol=1, timeout=DEF_TIMEOUT):
+        """Wait voltage to reach value with tolerance 'tol'."""
+        _t0 = _time.time()
+        while _time.time() - _t0 < timeout:
+            if abs(self.voltage - value) < tol:
+                return True
+        return False
 
 
 class EGTriggerPS(_Device):
@@ -420,9 +439,19 @@ class EGun(_Devices):
         self.trigmulti = Trigger('LI-01:TI-EGun-MultBun')
         self.trigmultipre = Trigger('LI-01:TI-EGun-MultBunPre')
 
+        self.sys_ext = _DeviceNC('LI-01:EG-External', ('status', ))
+        self.sys_vlv = _DeviceNC('LI-01:EG-Valve', ('status', ))
+        self.sys_gat = _DeviceNC('LI-01:EG-Gate', ('status', ))
+        self.sys_vac = _DeviceNC('LI-01:EG-Vacuum', ('status', ))
+        self.mps_ccg = _DeviceNC(
+            'LA-CN:H1MPS-1', ('CCG1Warn_L', 'CCG2Warn_L'))
+        self.mps_gun = _DeviceNC('LA-CN:H1MPS-1', ('GunPermit', ))
+
         devices = (
             self.bias, self.fila, self.hvps, self.trigps, self.pulse,
-            self.trigsingle, self.trigmulti, self.trigmultipre)
+            self.trigsingle, self.trigmulti, self.trigmultipre,
+            self.sys_ext, self.sys_vlv, self.sys_gat, self.sys_vac,
+            self.mps_ccg, self.mps_gun)
 
         self._bias_mb = EGun.BIAS_MULTI_BUNCH
         self._bias_sb = EGun.BIAS_SINGLE_BUNCH
@@ -431,6 +460,7 @@ class EGun(_Devices):
         self._hv_tol = EGun.HV_TOLERANCE
         self._filacurr_opval = EGun.FILACURR_OPVALUE
         self._filacurr_tol = EGun.FILACURR_TOLERANCE
+        self._last_status = ''
 
         super().__init__('', devices)
 
@@ -532,6 +562,11 @@ class EGun(_Devices):
         return sts
 
     @property
+    def last_status(self):
+        """Return last status log."""
+        return self._last_status
+
+    @property
     def high_voltage_opvalue(self):
         """High voltage operation value."""
         return self._hv_opval
@@ -546,6 +581,63 @@ class EGun(_Devices):
         is_on = self.hvps.is_on()
         is_op = abs(self.hvps.voltage - self._hv_opval) < self._hv_tol
         return is_on and is_op
+
+    def set_hv_voltage(self, value=None, nrpts=15, timeout=DEF_TIMEOUT):
+        """Set HVPS voltage."""
+        if not self._check_status_ok():
+            self._update_last_status('ERR:MPS or LI Status not ok. Aborted.')
+            return False
+        if not self.hvps.is_on():
+            self._update_last_status('HVPS voltage is not on.')
+            return False
+        if value is None:
+            value = self._hv_opval
+
+        # if voltage already satisfies value
+        cond = abs(self.hvps['voltinsoft'] - value) < self._hv_tol
+        cond &= abs(self.hvps['voltoutsoft'] - value) < 1e-4
+        if cond:
+            self._update_last_status(
+                'HVPS Voltage is already at {0:.3f}kV.'.format(value))
+            self.hvps.voltage = value
+            return True
+
+        # elif value is lower, do only one setpoint
+        if value < self.hvps.voltage:
+            self.hvps.voltage = value
+            return self.hvps.wait_voltage(value, self._hv_tol)
+
+        # else, do a ramp up
+        xdata = _np.linspace(self.hvps.voltage/value, 1, nrpts)
+        ydata = self._get_ramp(xdata, value, False)
+
+        self._update_last_status(
+            'Starting HVPS ramp to {0:.3f} kV.'.format(value))
+        self.hvps.voltage = ydata[0]
+        for volt in ydata[1:]:
+            self.hvps.voltage = volt
+            self._update_last_status('HV: {:.3f}kV'.format(volt))
+            _t0 = _time.time()
+            while _time.time() - _t0 < timeout:
+                if not self._check_status_ok():
+                    self._update_last_status(
+                        'ERR:MPS or LI Status not ok. Aborted.')
+                    return False
+                if self.hvps.voltage - volt < self._hv_tol and \
+                        self.hvps.current < volt * 1e-4:
+                    break
+            else:
+                if self.hvps.voltage - volt > self._hv_tol:
+                    self._update_last_status(
+                        'ERR:HVPS Voltage is taking too'
+                        ' long to increase. Aborted.')
+                else:
+                    self._update_last_status(
+                        'ERR:HVPS Current is taking too'
+                        ' long to decrease. Aborted.')
+                return False
+        self._update_last_status('HVPS Ready!')
+        return True
 
     @property
     def fila_current_opvalue(self):
@@ -563,3 +655,72 @@ class EGun(_Devices):
         is_op = abs(self.fila.current - self._filacurr_opval) < \
             self._filacurr_tol
         return is_on and is_op
+
+    def set_fila_current(self, value=None, nrpts=10, duration=5*60):
+        """Set filament current."""
+        if not self.fila.is_on():
+            self._update_last_status('FilaPS is not on.')
+            return False
+        if value is None:
+            value = self._filacurr_opval
+
+        # if current already satisfies value
+        cond = abs(self.fila['currentinsoft'] - value) < self._filacurr_tol
+        cond &= abs(self.fila['currentoutsoft'] - value) < 1e-4
+        if cond:
+            self._update_last_status(
+                'FilaPS current is already at {0:.3f}A.'.format(value))
+            self.fila.current = value
+            return True
+
+        # elif value is lower, do only one setpoint
+        if value < self.fila.current:
+            self.fila.current = value
+            return self.fila.wait_current(value, self._filacurr_tol)
+
+        # else, do a ramp up
+        isfast = value < 0.7
+        if isfast:
+            duration = 5
+            nrpts = 10
+        xdata = _np.linspace(0, 1, nrpts)
+        ydata = self._get_ramp(xdata, value, isfast)
+
+        t_inter = duration / (nrpts-1)
+
+        strg = 'fast' if isfast else 'slow'
+        self._update_last_status(
+            'Starting {0:s} filament ramp to {1:.3f}.'.format(strg, value))
+        self.fila.current = ydata[0]
+        for i, cur in enumerate(ydata[1:]):
+            dur = str(_timedelta(seconds=duration - i*t_inter)).split('.')[0]
+            self._update_last_status(
+                'Remaining Time: {0:s}  Curr: {1:.3f} A'.format(dur, cur))
+            _time.sleep(t_inter)
+            self.fila.current = cur
+        self._update_last_status('FilaPS Ready!')
+
+    def _get_ramp(self, xdata, value, isfast):
+        if isfast:
+            ydata = xdata.copy()
+        else:
+            ydata = 1 - (1-xdata)**3
+            # ydata = 1 - (1-xdata)**2
+            # ydata = _np.sqrt(1 - (1-xdata)**2)
+            # ydata = _np.sin(2*np.pi * xdata/4)
+        return ydata*value
+
+    def _check_status_ok(self):
+        """Check if interlock signals are ok."""
+        isok = [self.mps_ccg[ppty] == 0 for ppty in self.mps_ccg.properties]
+        allok = all(isok)
+        allok &= self.mps_gun['GunPermit'] == 1
+        allok &= self.sys_ext['status'] == 1
+        allok &= self.sys_vlv['status'] == 1
+        allok &= self.sys_gat['status'] == 1
+        allok &= self.sys_vac['status'] == 1
+        return allok
+
+    def _update_last_status(self, status):
+        self._last_status = status
+        print(status)
