39aac2ab60088c705fa29e2845082eff27ad95c2
ximenes <xresende@gmail.com>
2021-10-18 12:12:20 -0300

    DEV.ENH: Add Cycle properties to PowerSupply device.

--- siriuspy/siriuspy/devices/pwrsupply.py
+++ siriuspy/siriuspy/devices/pwrsupply.py
@@ -25,6 +25,12 @@ class _PSDev(_Device):
         'Current-SP', 'Current-RB', 'Current-Mon',
         'OpMode-Sel', 'OpMode-Sts',
         'WfmUpdateAuto-Sel', 'WfmUpdateAuto-Sts',
+        'CycleType-Sel', 'CycleType-Sts',
+        'CycleNrCycles-SP', 'CycleNrCycles-RB',
+        'CycleFreq-SP', 'CycleFreq-RB',
+        'CycleAmpl-SP', 'CycleAmpl-RB',
+        'CycleOffset-SP', 'CycleOffset-RB',
+        'CycleAuxParam-SP', 'CycleAuxParam-RB',
     )
     _properties_pulsed = (
         'Voltage-SP', 'Voltage-RB', 'Voltage-Mon',
@@ -178,6 +184,7 @@ class PowerSupply(_PSDev):
 
     OPMODE_SEL = _PSCStatus.OPMODE
     OPMODE_STS = _PSCStatus.STATES
+    CYCLETYPE = _PSCStatus.CYCLETYPE
 
     class DEVICES:
         """Devices names."""
@@ -201,6 +208,188 @@ class PowerSupply(_PSDev):
         """."""
         return self['OpMode-Sts']
 
+    @opmode.setter
+    def opmode(self, value):
+        self._enum_setter(
+            'OpMode-Sel', value, self.OPMODE_SEL)
+
+    @property
+    def opmode_str(self):
+        """."""
+        return self.OPMODE_STS._fields[self['OpMode-Sts']]
+
+    @property
+    def cycle_type(self):
+        """."""
+        return self['CycleType-Sts']
+
+    @cycle_type.setter
+    def cycle_type(self, value):
+        self._enum_setter(
+            'CycleType-Sel', value, self.CYCLETYPE)
+
+    @property
+    def cycle_type_str(self):
+        """."""
+        return self.CYCLETYPE._fields[self['CycleType-Sts']]
+
+    @property
+    def cycle_num_cycles(self):
+        """."""
+        return self['CycleNrCycles-RB']
+
+    @cycle_num_cycles.setter
+    def cycle_num_cycles(self, value):
+        """."""
+        self['CycleNrCycles-SP'] = value
+
+    @property
+    def cycle_freq(self):
+        """."""
+        return self['CycleFreq-RB']
+
+    @cycle_freq.setter
+    def cycle_freq(self, value):
+        """."""
+        self['CycleFreq-SP'] = value
+
+    @property
+    def cycle_period(self):
+        """."""
+        return 1 / self['CycleFreq-RB']
+
+    @cycle_period.setter
+    def cycle_period(self, value):
+        """."""
+        self['CycleFreq-SP'] = 1 / value
+
+    @property
+    def cycle_ampl(self):
+        """."""
+        return self['CycleAmpl-RB']
+
+    @cycle_ampl.setter
+    def cycle_ampl(self, value):
+        """."""
+        self['CycleAmpl-SP'] = value
+
+    @property
+    def cycle_offset(self):
+        """."""
+        return self['CycleOffset-RB']
+
+    @cycle_offset.setter
+    def cycle_offset(self, value):
+        """."""
+        self['CycleOffset-SP'] = value
+
+    @property
+    def cycle_aux_param(self):
+        """Meaning of each index is presented below.
+
+        for Sine and DampedSine and DampedSquaredSine:
+         - AuxParams[0] --> inital phase [°]
+         - AuxParams[1] --> final phase [°]
+         - AuxParams[2] --> damping time [s]
+         - AuxParams[3] --> not used
+
+        for Trapezoidal:
+         - AuxParams[0] --> rampup time [s]
+         - AuxParams[1] --> plato time [s]
+         - AuxParams[2] --> rampdown time [s]
+         - AuxParams[3] --> not used
+
+        for Square:
+         - AuxParams[0] --> inital phase [s]
+         - AuxParams[1] --> not used
+         - AuxParams[2] --> not used
+         - AuxParams[3] --> not used
+
+        """
+        return self['CycleAuxParam-RB']
+
+    @cycle_aux_param.setter
+    def cycle_aux_param(self, value):
+        """."""
+        self['CycleAuxParam-SP'] = value
+
+    @property
+    def cycle_duration(self):
+        """Total duration of the cycling process."""
+        return self.cycle_num_cycles / self.cycle_freq
+
+    @property
+    def cycle_rampup_time(self):
+        """Rampup time for Trapezoidal signals."""
+        return self.cycle_aux_param[0]
+
+    @cycle_rampup_time.setter
+    def cycle_rampup_time(self, value):
+        """Set Rampup time for Trapezoidal signals."""
+        var = self.cycle_aux_param
+        var[0] = value
+        self.cycle_aux_param = var
+
+    @property
+    def cycle_theta_begin(self):
+        """Return initial phase for Sine or Damped(Squared)Sine signals."""
+        return self.cycle_aux_param[0]
+
+    @cycle_theta_begin.setter
+    def cycle_theta_begin(self, value):
+        """Set Initial phase for Sine or Damped(Squared)Sine signals."""
+        var = self.cycle_aux_param
+        var[0] = value
+        self.cycle_aux_param = var
+
+    @property
+    def cycle_rampdown_time(self):
+        """Rampdown time for Trapezoidal signals."""
+        return self.cycle_aux_param[2]
+
+    @cycle_rampdown_time.setter
+    def cycle_rampdown_time(self, value):
+        """Set Rampdown time for Trapezoidal signals."""
+        var = self.cycle_aux_param
+        var[2] = value
+        self.cycle_aux_param = var
+
+    @property
+    def cycle_theta_end(self):
+        """Return final phase for Sine or Damped(Squared)Sine signals."""
+        return self.cycle_aux_param[1]
+
+    @cycle_theta_end.setter
+    def cycle_theta_end(self, value):
+        """Set Final phase for Sine or Damped(Squared)Sine signals."""
+        var = self.cycle_aux_param
+        var[1] = value
+        self.cycle_aux_param = var
+
+    @property
+    def cycle_plateau_time(self):
+        """Plateau time for Trapezoidal signals."""
+        return self.cycle_aux_param[1]
+
+    @cycle_plateau_time.setter
+    def cycle_plateau_time(self, value):
+        """Set Plateau time for Trapezoidal signals."""
+        var = self.cycle_aux_param
+        var[1] = value
+        self.cycle_aux_param = var
+
+    @property
+    def cycle_decay_time(self):
+        """Decay time constant for Damped(Squared)Sine signals."""
+        return self.cycle_aux_param[2]
+
+    @cycle_decay_time.setter
+    def cycle_decay_time(self, value):
+        """Set Decay time constant for Damped(Squared)Sine signals."""
+        var = self.cycle_aux_param
+        var[2] = value
+        self.cycle_aux_param = var
+
     def cmd_slowref(self, timeout=_PSDev._default_timeout):
         """."""
         self['OpMode-Sel'] = self.OPMODE_SEL.SlowRef
@@ -212,6 +401,12 @@ class PowerSupply(_PSDev):
         self['OpMode-Sel'] = self.OPMODE_SEL.SlowRefSync
         return self._wait(
             'OpMode-Sts', self.OPMODE_STS.SlowRefSync, timeout=timeout)
+
+    def cmd_cycle(self, timeout=_PSDev._default_timeout):
+        """."""
+        self['OpMode-Sel'] = self.OPMODE_SEL.Cycle
+        return self._wait(
+            'OpMode-Sts', self.OPMODE_STS.Cycle, timeout=timeout)
 
 
 class PowerSupplyPU(_PSDev):
