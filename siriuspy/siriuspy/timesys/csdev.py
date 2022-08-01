85c311dae632e4f8d894e4a145524055a2ec0948
Fernando <fernandohds564@gmail.com>
2022-08-01 12:15:05 -0300

    TIM..BUG: Fix LL events list.

--- siriuspy/siriuspy/timesys/csdev.py
+++ siriuspy/siriuspy/timesys/csdev.py
@@ -66,10 +66,10 @@ class Const(_csdev.Const):
     __EvtHL2LLMap = None
     __EvtLL2HLMap = None
 
-    evt_ll_codes = list(range(64)) + [124]
+    evt_ll_codes = list(range(64)) + [117, 124]
     evt_ll_names = ['Evt{0:02d}'.format(i) for i in evt_ll_codes]
     EvtLL = _csdev.Const.register(
-                    'EventsLL', evt_ll_names, values=evt_ll_codes)
+        'EventsLL', evt_ll_names, values=evt_ll_codes)
     del evt_ll_codes, evt_ll_names  # cleanup class namespace
 
     ClkHL2LLMap = {
