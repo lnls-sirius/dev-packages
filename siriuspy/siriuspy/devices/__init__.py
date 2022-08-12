2e74a949d9b08a64436bd498e47d2f794b846840
Ana Clara Oliveira <ana.clara@lnls.br>
2022-08-12 19:24:59 -0300

    DEV.ENH: add first version of BPMLogicalTrigger device

--- siriuspy/siriuspy/devices/__init__.py
+++ siriuspy/siriuspy/devices/__init__.py
@@ -2,7 +2,7 @@
 
 from .bbb import BunchbyBunch
 from .blscreen import BeamlineScreen
-from .bpm import BPM, FamBPMs
+from .bpm import BPM, FamBPMs, BPMLogicalTrigger
 from .currinfo import CurrInfoTranspEff, CurrInfoLinear, \
     CurrInfoBO, CurrInfoSI, CurrInfoAS
 from .dcct import DCCT
