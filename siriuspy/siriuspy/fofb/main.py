e00132e01c9c31f176b9a39b0a8587e43bcd34a1
Ana Clara Oliveira <ana.clara@lnls.br>
2022-10-28 17:41:20 -0300

    fofb.FIX: update LoopState-Sel PV too

--- siriuspy/siriuspy/fofb/main.py
+++ siriuspy/siriuspy/fofb/main.py
@@ -1351,6 +1351,7 @@ class App(_Callback):
                      self._thread_loopstate.is_alive() and
                      self._loop_state_lastsp != self._const.LoopState.Open):
                 self._update_log('FATAL: Opening FOFB loop...')
+                self.run_callbacks('LoopState-Sel', self._const.LoopState.Open)
                 self.run_callbacks('LoopState-Sts', self._const.LoopState.Open)
                 self.set_loop_state(self._const.LoopState.Open, reset=True)
 
