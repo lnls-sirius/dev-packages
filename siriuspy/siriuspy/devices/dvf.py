8fe7c31f9b054979117bc94ddd70ab52b190d1a3
ximenes <xresende@gmail.com>
2022-11-08 09:30:14 -0300

    Update DVF methods and properties

--- siriuspy/siriuspy/devices/dvf.py
+++ siriuspy/siriuspy/devices/dvf.py
@@ -6,7 +6,7 @@ from .device import DeviceNC as _DeviceNC
 
 
 class DVF(_DeviceNC):
-    """."""
+    """Beam Visualization Device ("Dispositivo de Visualização de Feixe")."""
 
     class DEVICES:
         """Devices names."""
@@ -15,11 +15,14 @@ class DVF(_DeviceNC):
         ALL = (CAX_DVF1, CAX_DVF2)
 
     MIN_ACQUISITION_PERIOD = 0.5  # [s]
+    DEF_EXPOSURE_TIME = 0.005  # [s]
+    DEF_ACQUISITION_PERIOD = MIN_ACQUISITION_PERIOD
 
     _default_timeout = 10  # [s]
-    _DEV2SHAPE = {
-        DEVICES.CAX_DVF1 : (1024, 1280),
-        DEVICES.CAX_DVF2 : (1024, 1280),
+    _DEV2PROPTIES = {
+        # DSF device       ((sizey, sizex), conv_pixel_2_physize
+        DEVICES.CAX_DVF1 : ((1024, 1280), None),
+        DEVICES.CAX_DVF2 : ((1024, 1280), 1.0),
     }
 
     _properties = (
@@ -34,7 +37,7 @@ class DVF(_DeviceNC):
         )
 
     def __init__(self, devname):
-        """."""
+        """Init."""
         # check if device exists
         if devname not in DVF.DEVICES.ALL:
             raise NotImplementedError(devname)
@@ -68,32 +71,52 @@ class DVF(_DeviceNC):
         """Return IOC acquisition status."""
         return self['cam1:Acquire']
 
+    @property
+    def image_sizex(self):
+        """Image horizontal size (pixels)."""
+        shape, *_ = DVF._DEV2PROPTIES[self.devname]
+        return shape[1]
+
+    @property
+    def image_sizey(self):
+        """Image vertical size (pixels)."""
+        shape, *_ = DVF._DEV2PROPTIES[self.devname]
+        return shape[0]
+
+    @property
+    def image(self):
+        """Return DVF image formatted as a (sizey, sizex) matriz."""
+        data = self['image1:ArrayData']
+        shape, *_ = DVF._DEV2PROPTIES[self.devname]
+        image = _np.reshape(data, shape)
+        return image
+
+    @property
+    def conv_pixel_2_physize(self):
+        """Image horizontal size (pixels)."""
+        _, conv_pixel_2_physize, *_ = DVF._DEV2PROPTIES[self.devname]
+        return conv_pixel_2_physize
+
     def reset_device(self):
-        """."""
+        """Reset DVF to a standard configuration."""
         self['cam1:EnableCallbacks'] = 1  # Enable
         self['image1:EnableCallbacks'] = 1  # Enable
         self['ffmstream1:EnableCallbacks'] = 1  # Enable
         self['Trans1:EnableCallbacks'] = 1  # Enable
-
-    def acquire_image(self):
-        """."""
-        data = self['image1:ArrayData']
-        shape = DVF._DEV2SHAPE[self.devname]
-        image = _np.reshape(data, shape)
-        return image
+        self.exposure_time = DVF.DEF_EXPOSURE_TIME
+        self.acquisition_period = DVF.DEF_ACQUISITION_PERIOD
 
     def cmd_acquire_on(self, timeout=None):
-        """."""
-        self._cmd_acquire(1, timeout=timeout)
+        """Tune IOC image acquisition on."""
+        self._set_and_wait('cam1:Acquire', 1, timeout=timeout)
 
     def cmd_acquire_off(self, timeout=None):
-        """."""
-        self._cmd_acquire(0, timeout=timeout)
+        """Tune IOC image acquisition off."""
+        self._set_and_wait('cam1:Acquire', 0, timeout=timeout)
 
-    def _cmd_acquire(self, value, timeout=None):
+    def _set_and_wait(self, propty, value, timeout=None):
         """."""
         timeout = timeout or self._default_timeout
-        propty = 'cam1:Acquire'
         self[propty] = value
         self._wait(propty + '_RBV', value, timeout=timeout, comp='eq')
 
