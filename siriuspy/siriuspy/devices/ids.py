324a33aea144b8bcb4f06afe57b044132b99e6b8
ximenes <xresende@gmail.com>
2022-11-23 21:23:46 -0300

    Add EPU docstrings and changes parameters list

--- siriuspy/siriuspy/devices/ids.py
+++ siriuspy/siriuspy/devices/ids.py
@@ -107,7 +107,6 @@ class APU(_Device):
         while self.is_moving:
             _time.sleep(APU._MOVECHECK_SLEEP)
 
-
 class EPU(_Device):
     """."""
 
@@ -119,20 +118,17 @@ class EPU(_Device):
 
     _idparam_fields = (
         'PERIOD',  # [mm]
-        'PHASE_MIN',  # [mm]
-        'PHASE_MAX',  # [mm]
-        'PHASE_PARK',  # [mm]
-        'GAP_MIN',   # [mm]
-        'GAP_MAX',  # [mm]
-        'GAP_PARK',  # [mm]
+        'PHASE_REFERENCE',  # [mm]
+        'GAP_REFERENCE',  # [mm]
         )
 
     _dev2params = {
         DEVICES.EPU50_10SB:
             _get_namedtuple('IDParameters',
-                _idparam_fields, (50.0, -50.0/4, 50/4, 0, 22.0, 300.0, 300.0)),
+                _idparam_fields, (50.0, 0, 300.0)),
         }
 
+    _short_shut_eye = 0.1  # [s]
     _default_timeout = 5  # [s]
 
     _properties = (
@@ -166,118 +162,136 @@ class EPU(_Device):
 
     @property
     def parameters(self):
-        """."""
+        """Return EPU parameters."""
         return EPU._dev2params[self.devname]
 
     @property
-    def phase_drive_power_on(self):
-        """."""
-        return self['PwrPhase-Mon'] != 0
-
-    @property
-    def gap_drive_power_on(self):
-        """."""
-        return self['PwrGap-Mon'] != 0
-
-    @property
     def phase_speed(self):
-        """Phase speed [mm/s]."""
+        """Return phase speed readback [mm/s]."""
         return self['PhaseSpeed-RB']
 
     @property
     def phase_speed_mon(self):
-        """Phase speed [mm/s]."""
+        """Return phase speed monitor [mm/s]."""
         return self['PhaseSpeed-Mon']
 
     @property
     def phase_speed_max(self):
-        """Max phase speed [mm/s]."""
+        """Return max phase speed readback [mm/s]."""
         return self['MaxPhaseSpeed-RB']
 
     @property
     def gap_speed(self):
-        """[mm/s]."""
+        """Return gap speed readback [mm/s]."""
         return self['GapSpeed-RB']
 
     @property
     def gap_speed_mon(self):
-        """[mm/s]."""
+        """Return gap speed monitor [mm/s]."""
         return self['GapSpeed-Mon']
 
     @property
     def gap_speed_max(self):
-        """Max gap speed [mm/s]."""
+        """Return max gap speed readback [mm/s]."""
         return self['MaxGapSpeed-RB']
 
     @property
     def phase(self):
-        """[mm]."""
+        """Return EPU phase readback [mm]."""
         return self['Phase-RB']
 
     @property
+    def phase_min(self):
+        """Return EPU phase lower control limit [mm]."""
+        ctrlvars = self.pv_ctrlvars('Phase-SP')
+        return ctrlvars['lower_ctrl_limit']
+
+    @property
+    def phase_max(self):
+        """Return EPU phase upper control limit [mm]."""
+        ctrlvars = self.pv_ctrlvars('Phase-SP')
+        return ctrlvars['upper_ctrl_limit']
+
+    @property
     def phase_mon(self):
-        """[mm]."""
+        """Return EPU phase monitor [mm]."""
         return self['Phase-Mon']
 
     @property
     def gap(self):
-        """[mm]."""
+        """Return EPU gap readback [mm]."""
         return self['Gap-RB']
 
     @property
+    def gap_min(self):
+        """Return EPU gap lower control limit [mm]."""
+        ctrlvars = self.pv_ctrlvars('Gap-SP')
+        return ctrlvars['lower_ctrl_limit']
+
+    @property
+    def gap_max(self):
+        """Return EPU gap upper control limit [mm]."""
+        ctrlvars = self.pv_ctrlvars('Gap-SP')
+        return ctrlvars['upper_ctrl_limit']
+
+    @property
     def gap_mon(self):
-        """[mm]."""
+        """Return EPU gap monitor [mm]."""
         return self['Gap-Mon']
 
     @property
+    def is_phase_drive_powered(self):
+        """Return phase driver power state on (True|False)."""
+        return self['PwrPhase-Mon'] != 0
+
+    @property
+    def is_gap_drive_powered(self):
+        """Return gap driver power state on (True|False)."""
+        return self['PwrGap-Mon'] != 0
+
+    @property
     def is_drives_powered(self):
-        """."""
-        status = True
-        status &= self['PwrPhase-Mon'] != 0
-        status &= self['PwrGap-Mon'] != 0
-        return status
+        """Return phase & gap drives powered state on (True|False)."""
+        return self.is_phase_drive_powered and self.is_gap_drive_powered
 
     @property
     def is_move_phase_enabled(self):
-        """."""
+        """Return phase movement enabled state (True|False)."""
         return self['AllowedToChangePhase-Mon'] != 0
 
     @property
     def is_move_gap_enabled(self):
-        """."""
+        """Return gap movement enabled state (True|False)."""
         return self['AllowedToChangeGap-Mon'] != 0
 
     @property
     def is_move_enabled(self):
-        """."""
-        status = True
-        status &= self.is_move_phase_enabled
-        status &= self.is_move_gap_enabled
-        return status
+        """Return phase and gap movements enabled state (True|False)."""
+        return self.is_move_phase_enabled and self.is_move_gap_enabled
 
     @property
     def is_moving(self):
-        """."""
+        """Return is moving state (True|False)."""
         return self['Moving-Mon'] != 0
 
     @property
     def is_busy(self):
-        """."""
+        """Return is busy state (True|False)."""
         return self['IsBusy-Mon'] != 0
 
     @property
     def beamline_ctrl_enabled(self):
-        """."""
+        """Return beamline control enabled state (True|False)."""
         return self['BeamLineCtrlEnbl-Sts'] != 0
 
     # --- cmd_wait
 
     def cmd_wait_while_busy(self, timeout=None):
-        """."""
+        """Command wait within timeout while EPU control is busy."""
         timeout = timeout or self._default_timeout
         time_init = _time.time()
         while self.is_busy:
-            _time.sleep(min(0.1, timeout))
+            _time.sleep(min(EPU._short_shut_eye, timeout))
             if _time.time() - time_init > timeout:
                 return False
         return True
@@ -285,7 +299,7 @@ class EPU(_Device):
     # --- cmd_beamline and cmd_drive
 
     def cmd_drive_turn_on(self, timeout=None):
-        """."""
+        """Command turn phase and gap drives on."""
         if self.phase_drive_power_on and self.gap_drive_power_on:
             return True
         self['EnblPwrAll-Cmd'] = 1
@@ -296,80 +310,76 @@ class EPU(_Device):
         return self._wait(props_values, timeout=timeout, comp='ne')
 
     def cmd_beamline_ctrl_enable(self, timeout=None):
-        """."""
-        # props_values = {'BeamLineCtrlEnbl-Sel': 1}
-        # return self._set_and_wait(props_values, timeout=timeout, comp='eq')
+        """Command enable bealine EPU control."""
         return self._set_sp('BeamLineCtrlEnbl-Sel', 1, timeout)
 
     def cmd_beamline_ctrl_disable(self, timeout=None):
-        """."""
-        # props_values = {'BeamLineCtrlEnbl-Sel': 0}
-        # return self._set_and_wait(props_values, timeout=timeout, comp='eq')
+        """Command disable bealine EPU control."""
         return self._set_sp('BeamLineCtrlEnbl-Sel', 0, timeout)
 
     # --- cmd_set ---
 
     def cmd_set_phase(self, phase, timeout=None):
-        """."""
+        """Command to set EPU target phase for movement [mm]."""
         return self._set_sp('Phase-SP', phase, timeout)
 
     def cmd_set_gap(self, gap, timeout=None):
-        """."""
+        """Command to set EPU target gap for movement [mm]."""
         return self._set_sp('Gap-SP', gap, timeout)
 
     def cmd_set_phase_speed(self, phase_speed, timeout=None):
-        """."""
+        """Command to set EPU cruise phase speed for movement [mm/s]."""
         return self._set_sp('PhaseSpeed-SP', phase_speed, timeout)
 
     def cmd_set_gap_speed(self, gap_speed, timeout=None):
-        """."""
+        """Command to set EPU cruise gap speed for movement [mm/s]."""
         return self._set_sp('GapSpeed-SP', gap_speed, timeout)
 
     def cmd_set_phase_speed_max(self, phase_speed_max, timeout=None):
-        """."""
+        """Command to set EPU max cruise phase speed for movement [mm/s]."""
         return self._set_sp('MaxPhaseSpeed-SP', phase_speed_max, timeout)
 
     def cmd_set_gap_speed_max(self, gap_speed_max, timeout=None):
-        """."""
+        """Command to set EPU max cruise gap speed for movement [mm/s]."""
         return self._set_sp('MaxGapSpeed-SP', gap_speed_max, timeout)
 
     # --- cmd_move disable/enable ---
 
-    # NOTE: not working. PVs not consistent!
+    # TODO: not working. IOC with Sel/Sts PVs that are not consistent!
 
     def cmd_move_phase_enable(self, timeout=None):
-        """."""
+        """Command to release and enable EPU phase movement."""
         # return self._set_sp('EnblAndReleasePhase-Sel', 1, timeout)
         self['EnblAndReleasePhase-Sel'] = 1
         return True
 
     def cmd_move_phase_disable(self, timeout=None):
-        """."""
+        """Command to disable and break EPU phase movement."""
         # return self._set_sp('EnblAndReleasePhase-Sel', 0, timeout)
         self['EnblAndReleasePhase-Sel'] = 0
         return True
 
     def cmd_move_gap_enable(self, timeout=None):
-        """."""
+        """Command to release and enable EPU gap movement."""
         # return self._set_sp('EnblAndReleaseGap-Sel', 1, timeout)
         self['EnblAndReleaseGap-Sel'] = 1
         return True
 
     def cmd_move_gap_disable(self, timeout=None):
-        """."""
+        """Command to disable and break EPU gap movement."""
         # return self._set_sp('EnblAndReleaseGap-Sel', 0, timeout)
         self['EnblAndReleaseGap-Sel'] = 1
         return True
 
     def cmd_move_enable(self, timeout=None):
-        """."""
+        """Command to release and enable EPU phase and gap movements."""
         success = True
         success &= self.cmd_move_phase_enable(timeout=timeout)
         success &= self.cmd_move_gap_enable(timeout=timeout)
         return success
 
     def cmd_move_disable(self, timeout=None):
-        """."""
+        """Command to disable and break EPU phase and gap movements."""
         success = True
         success &= self.cmd_move_phase_disable(timeout=timeout)
         success &= self.cmd_move_gap_disable(timeout=timeout)
@@ -378,7 +388,7 @@ class EPU(_Device):
     # -- cmd_move
 
     def cmd_move_stop(self, timeout=None):
-        """."""
+        """Command to interrupt and then enable phase and gap movements."""
         timeout = timeout or self._default_timeout
 
         # wait for not busy state
@@ -389,9 +399,9 @@ class EPU(_Device):
         self['Stop-Cmd'] = 1
 
         # check for successful stop
-        success = True
         if self.cmd_wait_while_busy(timeout=timeout):
             return False
+        success = True
         success &= super()._wait('Moving-Mon', 0, tiemout=timeout)
         success &= super()._wait('IsBusy-Mon', 0, tiemout=timeout)
         if not success:
@@ -401,51 +411,54 @@ class EPU(_Device):
         return self.cmd_move_enable(timeout=timeout)
 
     def cmd_move_phase_start(self, timeout=None):
-        """."""
+        """Command to start phase movement."""
         return self._move_start('ChangePhase-Cmd', timeout=timeout)
 
     def cmd_move_gap_start(self, timeout=None):
-        """."""
+        """Command to start gap movement."""
         return self._move_start('ChangeGap-Cmd', timeout=timeout)
 
     def cmd_move(self, phase, gap, timeout=None):
-        """."""
-        dtime_tol = 1.4  # additional percentual ETA
+        """Command to set and start phase and gap movements."""
+        # calc ETA
         dtime_phase = abs(phase - self.phase) / self.phase_speed
         dtime_gap = abs(gap - self.gap) / self.gap_speed
         dtime_max = max(dtime_phase, dtime_gap)
 
-        # command move start
+        # additional percentual in ETA
+        dtime_tol = 40  # [%]
+        tol_factor = (1 + dtime_tol/100)
+
+        # set target phase and gap
         self.cmd_set_phase(phase=phase, timeout=timeout)
         self.cmd_set_gap(gap=gap, timeout=timeout)
 
-        success = self.cmd_move_phase_start(timeout=timeout)
-        if not success:
+        # command move start
+        if not self.cmd_move_phase_start(timeout=timeout):
             return False
-        success = self.cmd_move_gap_start(timeout=timeout)
-        if not success:
+        if not self.cmd_move_gap_start(timeout=timeout):
             return False
 
         # wait for movement within reasonable time
         time_init = _time.time()
         while self.is_moving:
-            if _time.time() - time_init > dtime_tol * dtime_max:
+            if _time.time() - time_init > tol_factor * dtime_max:
                 return False
-            _time.sleep(0.1)
+            _time.sleep(EPU._short_shut_eye)
 
         # successfull movement at this point
         return True
 
     def cmd_move_park(self, timeout=None):
-        """."""
+        """Command to set and start EPU movement reference config."""
         params = self.parameters
         return self.cmd_move(
-            params.PHASE_PARK, params.GAP_PARK, timeout=timeout)
+            params.PHASE_REFERENCE, params.GAP_REFERENCE, timeout=timeout)
 
     # --- cmd_reset
 
     def cmd_reset(self, timeout=None):
-        """."""
+        """Command to reset EPU to a standard movement state."""
         success = True
         success &= self.cmd_beamline_ctrl_disable(timeout=timeout)
         success &= self.cmd_drive_turn_on(timeout=timeout)
@@ -469,10 +482,10 @@ class EPU(_Device):
 
     def _set_sp(self, propties_sp, values, timeout=None):
         timeout = timeout or self._default_timeout
-        success = True
         if isinstance(propties_sp, str):
             propties_sp = (propties_sp, )
             values = (values, )
+        success = True
         for propty_sp, value in zip(propties_sp, values):
             if value == self[propty_sp]:
                 continue
