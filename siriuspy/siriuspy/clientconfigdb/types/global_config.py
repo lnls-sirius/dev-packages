7ee07dcdfbd615ee1ec5faf9d29aa622d83c9fba
Fernando <fernandohds564@gmail.com>
2023-06-30 10:34:22 -0300

    CONFDB.ENH: Add PsMtn BPM trigger to global_config.

--- siriuspy/siriuspy/clientconfigdb/types/global_config.py
+++ siriuspy/siriuspy/clientconfigdb/types/global_config.py
@@ -620,6 +620,15 @@ _pvs_as_ti = [
     ['SI-Fam:TI-BPM:DeltaDelayRaw-SP', 30*[0, ], 0],
     ['SI-Fam:TI-BPM:LowLvlLock-Sel', 0, 0.0],
 
+    ['SI-Fam:TI-BPM-PsMtn:DelayRaw-SP', 0, 0],
+    ['SI-Fam:TI-BPM-PsMtn:WidthRaw-SP', 0, 0.0],
+    ['SI-Fam:TI-BPM-PsMtn:NrPulses-SP', 0, 0.0],
+    ['SI-Fam:TI-BPM-PsMtn:Polarity-Sel', 0, 0.0],
+    ['SI-Fam:TI-BPM-PsMtn:Src-Sel', 0, 0.0],
+    ['SI-Fam:TI-BPM-PsMtn:State-Sel', 0, 0.0],
+    ['SI-Fam:TI-BPM-PsMtn:DeltaDelayRaw-SP', 30*[0, ], 0],
+    ['SI-Fam:TI-BPM-PsMtn:LowLvlLock-Sel', 0, 0.0],
+
     ['SI-Fam:TI-FOFB:DelayRaw-SP', 0, 0],
     ['SI-Fam:TI-FOFB:WidthRaw-SP', 0, 0.0],
     ['SI-Fam:TI-FOFB:NrPulses-SP', 0, 0.0],
