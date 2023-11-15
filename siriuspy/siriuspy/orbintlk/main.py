e9a14f451ad5fff89dc5ff1ad9f48ea14230bb0b
Ana Clara Oliveira <ana.clara@lnls.br>
2023-11-14 21:32:57 -0300

    ORBINLKT.FIX: fix Fouts RxLockedLtc-Mon check value

--- siriuspy/siriuspy/orbintlk/main.py
+++ siriuspy/siriuspy/orbintlk/main.py
@@ -919,7 +919,7 @@ class App(_Callback):
             for devname, rxenbl in self._fout2rxenbl.items():
                 dev = self._fout_devs[devname]
                 okg &= dev['RxEnbl-RB'] == rxenbl
-                okg &= dev['RxLockedLtc-Mon']
+                okg &= dev['RxLockedLtc-Mon'] == rxenbl
             value = _updt_bit(value, 4, not okg)
         else:
             value += 0b11 << 3
