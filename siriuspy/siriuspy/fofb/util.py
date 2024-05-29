a4dab6b24f06b938a7082177e066d013b2732db0
Júlia Carvalho <97030556+juliacscarvalho@users.noreply.github.com>
2024-05-29 09:40:38 -0300

    Update siriuspy/siriuspy/fofb/util.py
    Co-authored-by: fernandohds564 <fernandohds564@gmail.com>

--- siriuspy/siriuspy/fofb/util.py
+++ siriuspy/siriuspy/fofb/util.py
@@ -47,8 +47,9 @@ def set_default_psconfig_mat(option=None):
                          coefficients - each biquad must have 5 coefficients.')
 
     std_psconfig_mat = _np.zeros(
-            (_const.nr_chcv, _const.psconfig_nr_coeffs_columns + 3),
-            dtype=float)
+        (_const.nr_chcv, _const.psconfig_nr_coeffs_columns + 3),
+        dtype=float
+    )
     std_psconfig_mat[:, 0] = _corr_currloop_kp
     std_psconfig_mat[:, 1] = _corr_currloop_ti
     std_psconfig_mat[:, 2] = _corr_accfilter_gain
