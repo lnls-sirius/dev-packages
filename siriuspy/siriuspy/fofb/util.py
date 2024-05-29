fb6e0e115f4f23b6fe80b7d7df984ab4d31c9879
Júlia Carvalho <juliacdscarvalho@gmail.com>
2024-05-29 10:07:46 -0300

    Update siriuspy/siriuspy/fofb/util.py: get the arrays of Kp and Ti for all the correctors

--- siriuspy/siriuspy/fofb/util.py
+++ siriuspy/siriuspy/fofb/util.py
@@ -1,7 +1,7 @@
 import numpy as _np
 from .csdev import HLFOFBConst as _Const
 from ..devices import FamFOFBControllers as _FamFOFBCtrls, \
-        FamFastCorrs as _FamFastCorrs, PowerSupplyFC as _PS
+        FamFastCorrs as _FamFastCorrs
 
 
 def set_default_psconfig_mat(option=None):
@@ -19,7 +19,6 @@ def set_default_psconfig_mat(option=None):
 
     _const = _Const()
     _llfofb_dev = _FamFOFBCtrls()
-    _ps = _PS()
 
     corrnames = _const.ch_names + _const.cv_names
     _corrs_dev = _FamFastCorrs(corrnames)
@@ -32,21 +31,15 @@ def set_default_psconfig_mat(option=None):
     if option == 'Unit' or option is None:
         _acc_filter_coeffs = num_biquads * unit
 
-    if option == 'Switching':
+    elif option == 'Switching':
         _acc_filter_coeffs = sw2 + sw4 + (num_biquads - 2) * unit
 
     else:
         raise ValueError('Option must be Unit or Switching.')
 
-    _corr_currloop_kp = _ps.currloop_kp
-    _corr_currloop_ti = _ps.currloop_ti
+    _corr_currloop_kp = _corrs_dev.currloop_kp
+    _corr_currloop_ti = _corrs_dev.currloop_ti
     _corr_accfilter_gain = _corrs_dev.fofbacc_filter_gain
-
-    if not _const.psconfig_nr_coeffs_columns == num_biquads*5:
-        raise ValueError(
-            'Number of biquads does not match number of filter '
-            'coefficients - each biquad must have 5 coefficients.'
-        )
 
     std_psconfig_mat = _np.zeros(
         (_const.nr_chcv, _const.psconfig_nr_coeffs_columns + 3),
