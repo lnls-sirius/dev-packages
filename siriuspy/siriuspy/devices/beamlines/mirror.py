e17fa2c053f990771ccb3dfefee9395fe6d0c290
Jefferson Barros Vieira <jefferson.negociom03@gmail.com>
2025-07-03 15:41:58 -0300

    mirror module refactoring

--- siriuspy/siriuspy/devices/beamlines/mirror.py
+++ siriuspy/siriuspy/devices/beamlines/mirror.py
@@ -2,136 +2,39 @@
 
 import inspect as _inspect
 import time as _time
+from types import SimpleNamespace as _SimpleNamespace
 
 from ..device import Device as _Device
 
 
-class _PVs:
-    # MIRROR MOTORS
-    # --- ROTY ---
-    ROTY_SP = None
-    ROTY_RB = None
-    ROTY_MON = None
-    ROTY_STOP = None
-
-    # --- Tx ---
-    TX_SP = None
-    TX_RB = None
-    TX_MON = None
-    TX_STOP = None
-
-    # --- Y1 ---
-    Y1_SP = None
-    Y1_RB = None
-    Y1_MON = None
-    Y1_STOP = None
-
-    # --- Y2 ---
-    Y2_SP = None
-    Y2_RB = None
-    Y2_MON = None
-    Y2_STOP = None
-
-    # --- Y3 ---
-    Y3_SP = None
-    Y3_RB = None
-    Y3_MON = None
-    Y3_STOP = None
-
-    # MIRROR SENSORS
-    # --- PHOTOCOLLECTOR ---
-    PHOTOCOLLECTOR = None
-
-    # --- TEMPERATURES ---
-    TEMP_SP = None
-    TEMP_RB = None
-    TEMP0_MON = None
-    TEMP1_MON = None
-    TEMP2_MON = None
-    TEMP3_MON = None
-    TEMP4_MON = None
+class _PVNames(_SimpleNamespace):
+    def __getattr__(self, name):
+        """."""
+        return None
 
 
-class Mirror(_Device):
-    """Mirror device.
-
-    * M1 online autodesk drawing:
-    https://drive.autodesk.com/de29ccb7d/shares/SH9285eQTcf875d3c5396deb53e725438482
-    """
-
-    class DEVICES:
-        """Devices names."""
-
-        MIRROR1 = "CAX:A"
-
-        ALL = (MIRROR1,)
+class MirrorBase(_Device):
+    """Base Mirror device."""
 
     _DEFAULT_MOTOR_TIMEOUT = 2.0  # [s]
 
     # --- PVS ---
-    PVS = _PVs()
-
-    # MIRROR CONTROL
-    PVS.ROTY_SP = "PP01:A.VAL"
-    PVS.ROTY_RB = "PP01:A.VAL"  # That doesn't have a RB PV
-    PVS.ROTY_MON = "PP01:A.RBV"  # RBV is not the pv of readback
-    PVS.ROTY_STOP = "PP01:A.STOP"
-
-    PVS.TX_SP = "PP01:B.VAL"
-    PVS.TX_RB = "PP01:B.VAL"  # That doesn't have a RB PV
-    PVS.TX_MON = "PP01:B.RBV"  # RBV is not the pv of readback
-    PVS.TX_STOP = "PP01:B.STOP"
-
-    PVS.Y1_SP = "PP01:C.VAL"
-    PVS.Y1_RB = "PP01:C.VAL"  # That doesn't have a RB PV
-    PVS.Y1_MON = "PP01:C.RBV"  # RBV is not the pv of readback
-    PVS.Y1_STOP = "PP01:C.STOP"
-
-    PVS.Y2_SP = "PP01:D.VAL"
-    PVS.Y2_RB = "PP01:D.VAL"  # That doesn't have a RB PV
-    PVS.Y2_MON = "PP01:D.RBV"  # RBV is not the pv of readback
-    PVS.Y2_STOP = "PP01:D.STOP"
-
-    PVS.Y3_SP = "PP01:E.VAL"
-    PVS.Y3_RB = "PP01:E.VAL"  # That doesn't have a RB PV
-    PVS.Y3_MON = "PP01:E.RBV"  # RBV is not the pv of readback
-    PVS.Y3_STOP = "PP01:E.STOP"
-
-    # SENSORS
-    PVS.PHOTOCOLLECTOR = "RIO01:9215A:ai0"
-    PVS.TEMP0_MON = "RIO01:9226B:temp0"
-    PVS.TEMP1_MON = "RIO01:9226B:temp1"
-    PVS.TEMP2_MON = "RIO01:9226B:temp2"
-    PVS.TEMP3_MON = "RIO01:9226B:temp3"
-    PVS.TEMP4_MON = "RIO01:9226B:temp4"
-    PVS.TEMP_SP = "RIO01:M1_CtrltempSp"
-    PVS.TEMP_RB = "RIO01:M1_CtrltempSp"  # That doesn't have a RB PV
-
-    PROPERTIES_DEFAULT = tuple(
-        set(
-            value
-            for key, value in _inspect.getmembers(PVS)
-            if not key.startswith("_") and value is not None
-        )
-    )
+    PVS = _PVNames()
 
     def __init__(self, devname=None, props2init="all", **kwargs):
         """Init."""
-        # check if device exists
-        if devname not in self.DEVICES.ALL:
-            raise NotImplementedError(devname)
         super().__init__(devname, props2init=props2init, **kwargs)
 
     @property
-    def roty_pos(self):
-        """Return the linear actuator pos related to RotY rotation [mm].
+    def ry_pos(self):
+        """Return the linear actuator pos related to RY rotation [mm].
 
-        RotY is performed by a linear actuator in one of
+        RY is performed by a linear actuator in one of
         longitudinal ends of the mirror. The mirror is pivoted
         at its longitudinal center and the linear actuator induces
         a rotation around the Y axis.
         """
-        return self[self.PVS.ROTY_MON]
+        return self[self.PVS.RY_MON]
 
     @property
     def tx_pos(self):
@@ -210,16 +113,16 @@ class Mirror(_Device):
         """Return M1 temperature reference setpoint [°C]."""
         return self[self.PVS.TEMP_RB]
 
-    @roty_pos.setter
-    def roty_pos(self, value):
-        """Set the linear actuator pos related to RotY rotation [mm].
+    @ry_pos.setter
+    def ry_pos(self, value):
+        """Set the linear actuator pos related to RY rotation [mm].
 
-        RotY is performed by a linear actuator in one of
+        RY is performed by a linear actuator in one of
         longitudinal ends of the mirror. The mirror is pivoted
         at its longitudinal center and the linear actuator induces
         a rotation around the Y axis.
         """
-        self[self.PVS.ROTY_SP] = value
+        self[self.PVS.RY_SP] = value
 
     @tx_pos.setter
     def tx_pos(self, value):
@@ -273,9 +176,9 @@ class Mirror(_Device):
         self[propty] = 1
         return self._wait(propty, 0, timeout=timeout)
 
-    def cmd_roty_stop(self, timeout=None):
-        """Stop linear actuator for RotY rotation."""
-        return self._cmd_motor_stop(self.PVS.ROTY_STOP, timeout)
+    def cmd_ry_stop(self, timeout=None):
+        """Stop linear actuator for RY rotation."""
+        return self._cmd_motor_stop(self.PVS.RY_STOP, timeout)
 
     def cmd_tx_stop(self, timeout=None):
         """Stop linear actuator for Tx translation."""
@@ -292,3 +195,66 @@ class Mirror(_Device):
     def cmd_y3_stop(self, timeout=None):
         """Stop linear actuator Y3."""
         return self._cmd_motor_stop(self.PVS.Y3_STOP, timeout)
+
+
+class CAXMirror(MirrorBase):
+    """CARCARÁ Mirror Device.
+
+    * M1 online autodesk drawing:
+    https://drive.autodesk.com/de29ccb7d/shares/SH9285eQTcf875d3c5396deb53e725438482.
+    """
+
+    class DEVICES:
+        """."""
+
+        CAX_FOE = "CAX:A"
+
+        ALL = (CAX_FOE,)
+
+    def __init__(self, devname, props2init="all", **kwargs):
+        """Init."""
+        if devname is None:
+            devname = self.DEVICES.CAX_FOE
+        if devname not in self.DEVICES.ALL:
+            raise ValueError("Wrong value for devname")
+
+        super().__init__(devname=devname, props2init=props2init, **kwargs)
+        # MOTORS
+        self.PVS.RY_SP = "PP01:A.VAL"
+        self.PVS.RY_RB = "PP01:A.VAL"  # That doesn't have a RB PV
+        self.PVS.RY_MON = "PP01:A.RBV"  # RBV is not the pv of readback
+        self.PVS.RY_STOP = "PP01:A.STOP"
+
+        self.PVS.TX_SP = "PP01:B.VAL"
+        self.PVS.TX_RB = "PP01:B.VAL"  # That doesn't have a RB PV
+        self.PVS.TX_MON = "PP01:B.RBV"  # RBV is not the pv of readback
+        self.PVS.TX_STOP = "PP01:B.STOP"
+
+        self.PVS.Y1_SP = "PP01:C.VAL"
+        self.PVS.Y1_RB = "PP01:C.VAL"  # That doesn't have a RB PV
+        self.PVS.Y1_MON = "PP01:C.RBV"  # RBV is not the pv of readback
+        self.PVS.Y1_STOP = "PP01:C.STOP"
+
+        self.PVS.Y2_SP = "PP01:D.VAL"
+        self.PVS.Y2_RB = "PP01:D.VAL"  # That doesn't have a RB PV
+        self.PVS.Y2_MON = "PP01:D.RBV"  # RBV is not the pv of readback
+        self.PVS.Y2_STOP = "PP01:D.STOP"
+
+        self.PVS.Y3_SP = "PP01:E.VAL"
+        self.PVS.Y3_RB = "PP01:E.VAL"  # That doesn't have a RB PV
+        self.PVS.Y3_MON = "PP01:E.RBV"  # RBV is not the pv of readback
+        self.PVS.Y3_STOP = "PP01:E.STOP"
+
+        # SENSORS
+        self.PVS.PHOTOCOLLECTOR = "RIO01:9215A:ai0"
+        self.PVS.TEMP0_MON = "RIO01:9226B:temp0"
+        self.PVS.TEMP1_MON = "RIO01:9226B:temp1"
+        self.PVS.TEMP2_MON = "RIO01:9226B:temp2"
+        self.PVS.TEMP3_MON = "RIO01:9226B:temp3"
+        self.PVS.TEMP4_MON = "RIO01:9226B:temp4"
+        self.PVS.TEMP_SP = "RIO01:M1_CtrltempSp"
+        self.PVS.TEMP_RB = "RIO01:M1_CtrltempSp"  # That doesn't have a RB PV
+
+        self.PROPERTIES_DEFAULT = tuple(
+            set(value for key, value in vars(self.PVS).items())
+        )
