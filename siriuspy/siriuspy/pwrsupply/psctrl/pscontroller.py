2254010b86e4938459c0b7df479579f32220a1a4
Ximenes Rocha Resende <xresende@gmail.com>
2025-09-23 10:29:28 -0300

    Fix erroneous commit from another PR

--- siriuspy/siriuspy/pwrsupply/psctrl/pscontroller.py
+++ siriuspy/siriuspy/pwrsupply/psctrl/pscontroller.py
@@ -20,7 +20,6 @@ class PSController:
         'WfmOffsetKL-SP', 'WfmOffsetKL-RB',
         'WfmOffsetSL-SP', 'WfmOffsetSL-RB',
         'WfmOffsetEnergy-SP', 'WfmOffsetEnergy-RB',
-        'Log-Mon',  # This PV is related to the IOC, not PSController
         }
 
     def __init__(self, readers, writers,
