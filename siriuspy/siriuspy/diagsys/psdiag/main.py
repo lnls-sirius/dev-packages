a6808c13258de118c85a2779774688d746623e63
Ana Clara Oliveira <ana.clara@lnls.br>
2025-12-03 19:35:47 -0300

    psdiag.ENH: only check diff for IDFF correctors if the IDFFMode is disabled

--- siriuspy/siriuspy/diagsys/psdiag/main.py
+++ siriuspy/siriuspy/diagsys/psdiag/main.py
@@ -50,7 +50,7 @@ class PSDiagApp(_App):
                         alarm_list.extend(
                             [aux+':'+alm for alm in intlks if 'Alarm' in alm])
 
-                nbpvs = 5
+                nbpvs = 6 if psname.sub[2:] in ['SA', 'SB', 'SP'] else 5
                 pvs = [None]*(nbpvs+len(intlk_list)+len(alarm_list))
                 pvs[_PSStatusPV.PWRSTE_STS] = devname + ':PwrState-Sts'
                 pvs[_PSStatusPV.CURRT_DIFF] = devname + ':DiagCurrentDiff-Mon'
@@ -58,6 +58,8 @@ class PSDiagApp(_App):
                 pvs[_PSStatusPV.OPMODE_STS] = devname + ':OpMode-Sts'
                 if psname.dev not in ['FCH', 'FCV']:
                     pvs[_PSStatusPV.WAVFRM_MON] = devname + ':Wfm-Mon'
+                    if psname.sub[2:] in ['SA', 'SB', 'SP']:
+                        pvs[_PSStatusPV.FFMODE_STS] = devname + ':IDFFMode-Sts'
                 else:
                     pvs[_PSStatusPV.TRIGEN_STS] = devname + ':TrigEn-Sts'
 
