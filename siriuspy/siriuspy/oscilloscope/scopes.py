46e9654f750414cdc5519d4d9e5dbe189c23a572
ximenes <xresende@gmail.com>
2026-07-27 17:55:51 -0300

    MNT: (SCOPES) use control subnet IPs for scopes

--- siriuspy/siriuspy/oscilloscope/scopes.py
+++ siriuspy/siriuspy/oscilloscope/scopes.py
@@ -4,14 +4,14 @@
 class Scopes:
     """Keysight oscilloscopes names and IPs."""
 
-    AS_DI_FCTDIG = '10.128.150.78'  # 'AS-DI-FCTDig.lnls-sirius.com.br'
-    AS_DI_FPMDIG = '10.128.150.77'  # 'AS-DI-FPMDig.lnls-sirius.com.br'
-    LI_DI_ICTOSC = '10.128.1.220'  # 'li-di-ictosc.lnls-sirius.com.br'
-    LI_PU_OSC_MODLTR = '10.0.38.48'  # 'KEYSIGH-QQI8MNR.abtlus.org.br'
-    TB_PU_OSC_INJBO = '10.0.38.74'  # 'TB-PU-Osc-InjBO.abtlus.org.br'
-    TS_PU_OSC_EJEBO = '10.0.38.77'  # 'TS-PU-Osc-EjeBO.abtlus.org.br'
-    TS_PU_OSC_INJSI = '10.0.38.20'  # 'TS-PU-Osc-InjSI.abtlus.org.br'
-    SI_PU_OSC_INJSI = '10.0.38.69'  # 'SI-PU-Osc-InjSI.abtlus.org.br'
+    AS_DI_FCTDIG = '10.128.150.22'  # 'AS-DI-FCTDig.lnls-sirius.com.br'
+    AS_DI_FPMDIG = '10.128.150.21'  # 'AS-DI-FPMDig.lnls-sirius.com.br'
+    LI_DI_ICTOSC = '10.128.1.150'  # 'li-di-ictosc.lnls-sirius.com.br'
+    LI_PU_OSC_MODLTR = '10.128.150.20'  # 'KEYSIGH-QQI8MNR.abtlus.org.br'
+    TB_PU_OSC_INJBO = '10.128.101.70'  # 'TB-PU-Osc-InjBO.abtlus.org.br'
+    TS_PU_OSC_EJEBO = '10.128.120.70'  # 'TS-PU-Osc-EjeBO.abtlus.org.br'
+    TS_PU_OSC_INJSI = '10.128.101.72'  # 'TS-PU-Osc-InjSI.abtlus.org.br'
+    SI_PU_OSC_INJSI = '10.128.101.71'  # 'SI-PU-Osc-InjSI.abtlus.org.br'
 
 
 class ScopeSignals:
