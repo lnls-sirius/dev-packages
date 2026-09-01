d89f4eceaef53614157314dc732ba9bc08a453a2
ximenes <xresende@gmail.com>
2026-09-01 16:50:58 -0300

    Update

--- siriuspy/siriuspy/oscilloscope/__init__.py
+++ siriuspy/siriuspy/oscilloscope/__init__.py
@@ -1,5 +1,3 @@
 """Oscilloscope subpackage."""
 
-from .scopes import Scopes, ScopeSignals, Keysight
-# from .keysight import Keysight
-from .waveform import WaveformAcquisition
+from .scopes import ScopeSignals, Keysight, Scopes
--- siriuspy/siriuspy/oscilloscope/scopes.py
+++ siriuspy/siriuspy/oscilloscope/scopes.py
@@ -11,16 +11,22 @@ class ScopeSignals:
 
     SI_FILL_PATTERN = ('SI_FILL_PATTERN', 'AS_DI_FPMDIG', 'channel1')
     BO_FILL_PATTERN = ('BO_FILL_PATTERN', 'AS_DI_FPMDIG', 'channel4')
+
     TS_EJESEPTG_PULSE = ('TS_EJESEPTG_PULSE', 'TS_PU_OSC_EJEBO', 'channel1')
     TS_EJESEPTF_PULSE = ('TS_EJESEPTF_PULSE', 'TS_PU_OSC_EJEBO', 'channel2')
     BO_EJEKCKR_PULSE = ('BO_EJEKCKR_PULSE', 'TS_PU_OSC_EJEBO', 'channel3')
     SI_PINGV_PULSE = ('SI_PINGV_PULSE', 'TS_PU_OSC_EJEBO', 'channel4')
+
     LI_ICT1 = ('LI_ICT1', 'LI_DI_ICTOSC', 'channel1')
     LI_ICT2 = ('LI_ICT2', 'LI_DI_ICTOSC', 'channel2')
     TB_ICT1 = ('TB_ICT1', 'LI_DI_ICTOSC', 'channel3')
     TB_ICT2 = ('TB_ICT2', 'LI_DI_ICTOSC', 'channel4')
+
+    TB_FCT = ('TB_FCT', 'AS_DI_FCTDIG', 'channel1')
+    TS_FCT = ('TS_FCT', 'AS_DI_FCTDIG', 'channel2')
     TS_ICT1 = ('TS_ICT1', 'AS_DI_FCTDIG', 'channel3')
     TS_ICT2 = ('TS_ICT2', 'AS_DI_FCTDIG', 'channel4')
+
     MODLTR1_PULSE = ('MODLTR1_PULSE', 'LI_PU_OSC_MODLTR', 'channel1')
     MODLTR2_PULSE = ('MODLTR2_PULSE', 'LI_PU_OSC_MODLTR', 'channel3')
 
@@ -95,8 +101,8 @@ class Scope:
             channel4 (str, optional): Name of the signal connected to
                 channel 4. Overrides the automatic mapping, if
                 provided. Defaults to None.
-            stats (tuple, optional): Measurement statistics to be
-                collected. Defaults to None.
+            stats_fields (tuple, optional): Measurement statistics to
+                be collected. Defaults to None.
         """
         self.ipaddr = ipaddr
         self.hostname = hostname
@@ -171,7 +177,7 @@ class Scope:
         stat_splits = _np.array_split(meas, nr_stats)
         for stat_split in stat_splits:
             stats = list(float(val) for val in stat_split[1:])
-            dct = {fld: val for fld, val in zip(flds, stats)}  # ruff: ignore[zip-without-explicit-strict]
+            dct = dict(zip(flds, stats))  # ruff: ignore[zip-without-explicit-strict]
             stat_dict[stat_split[0]] = dct
         return stat_dict
 
@@ -207,7 +213,7 @@ class Keysight(Scope):
 
     def read_meas(self):
         """Get the measurements from the oscilloscope."""
-        meas = self._send_command(b":MEASure:RESults?\n")
+        meas = self._send_cmd(b":MEASure:RESults?\n")
         if meas:
             meas = self._process_meas(meas)
             return meas
@@ -219,7 +225,7 @@ class Keysight(Scope):
         if channel not in self.valid_channels:
             raise ValueError('Invalid channel name "{}"'.format(channel))
         chan = channel.replace('channel', 'CHAN')
-        self._send_command(
+        self._send_cmd(
             b':WAVeform:SOURce ' + chan.encode('ascii') + b'\n',
             get_res=False,
         )
@@ -227,9 +233,9 @@ class Keysight(Scope):
     def read_channel_scales(self, channel):
         """Read the scales for the selected channel."""
         self.select_channel(channel)
-        xinc = self._send_command(b":WAVeform:XINCrement?\n")
-        yinc = self._send_command(b":WAVeform:YINCrement?\n")
-        yor = self._send_command(b":WAVeform:YORigin?\n")
+        xinc = self._send_cmd(b":WAVeform:XINCrement?\n")
+        yinc = self._send_cmd(b":WAVeform:YINCrement?\n")
+        yor = self._send_cmd(b":WAVeform:YORigin?\n")
         xinc = float(xinc) if xinc else None
         yinc = float(yinc) if yinc else None
         yor = float(yor) if yor else None
@@ -238,7 +244,7 @@ class Keysight(Scope):
             raise ValueError('None returned in read_all_channel_scales')
         return scales
 
-    def update_channel_scales(self, channel, update=False):
+    def update_channel_scales(self, channel):
         """Update the scales for the selected channel."""
         scales = self.read_channel_scales(channel)
         self.channels_scales[channel] = scales
@@ -251,9 +257,9 @@ class Keysight(Scope):
 
     def config_acq_wfm(self):
         """Configure the oscilloscope for waveform acquisition."""
-        idn = self._send_command(b'*IDN?\r\n')  # needed ?
-        self._send_command(b":WAVeform:FORMat WORD\n", get_res=False)
-        self._send_command(b":WAVeform:BYTeorder MSBF\n", get_res=False)
+        idn = self._send_cmd(b'*IDN?\r\n')  # needed ?
+        self._send_cmd(b":WAVeform:FORMat WORD\n", get_res=False)
+        self._send_cmd(b":WAVeform:BYTeorder MSBF\n", get_res=False)
         return idn
 
     def init_wfm(self):
@@ -261,6 +267,49 @@ class Keysight(Scope):
         self.config_acq_wfm()
         if not self.channels_scales:
             self.update_all_channels_scales()
+
+    def read_header_wfm(self):
+        """Read the header of the waveform data."""
+        if not self._socket:
+            raise ValueError('socket is None')
+        marker = self._recv_cmd(1)
+        if marker != b'#':
+            raise RuntimeError(
+                f'Esperava "#", recebi {repr(marker)}')
+        num = int(self._recv_cmd(1).decode('ascii'))
+        datanum = int(self._recv_cmd(num).decode('ascii'))
+        return datanum
+
+    def read_raw_wfm(self):
+        """Read the raw waveform data from the oscilloscope."""
+        self._send_cmd(b":WAVeform:STReaming OFF\n", get_res=False)
+        self._send_cmd(b":WAVeform:DATA?\n", get_res=False)
+        datanum = self.read_header_wfm()
+        dataraw = b''
+        while len(dataraw) < datanum:
+            datasize = datanum - len(dataraw)
+            dataraw += self._recv_cmd(datasize)
+        # consome '\n' final
+        self._recv_cmd(1)
+        return dataraw
+
+    def read_channel_wfm(self, channel):
+        """Read waveform data from a specific channel."""
+        self.select_channel(channel)
+        dataraw = self.read_raw_wfm()
+        xinc, yinc, yor = self.channels_scales[channel]
+        datax, datay = self._process_scales_wfm(dataraw, yinc, yor, xinc)
+        return datax, datay
+
+    def read_wfm(self, channels=None):
+        """Read waveform data from channels."""
+        channels = channels or self.valid_channels
+        waveforms = {}
+        for channel in channels:
+            datax, datay = self.read_channel_wfm(channel)
+            signal = getattr(self, channel)
+            waveforms[signal] = (datax, datay)
+        return waveforms
 
     def acquire(self, acq_meas=False, acq_wfms=False):
         """Acquire waveform data from all channels."""
@@ -273,20 +322,36 @@ class Keysight(Scope):
             if acq_wfms:
                 self.init_wfm()
                 wfms = self.read_wfm()
-        except Exception:
-            raise
         finally:
             self.disconnect()
 
         return meas, wfms
 
-    def _send_command(self, cmd, get_res=True):
+    @staticmethod
+    def _process_scales_wfm(dataraw, yinc, yor, xinc):
+        """Process the raw waveform data into x and y arrays."""
+        dataraw = dataraw[0:-1]
+        va1 = _np.array(list(dataraw)[0::2])
+        va0 = _np.array(list(dataraw)[1::2])
+        va1 = va1[:va0.size]
+        datay = ((va1 << 8) + va0 - 2**16*(va1 >> 7)) * yinc + yor
+        datax = _np.arange(datay.size) * xinc
+        return datax, datay
+
+    def _send_cmd(self, cmd, get_res=True):
         """."""
         if self._socket:
             self._socket.sendall(cmd)
             if get_res:
-                return self._socket.recv(1024).decode('ascii')
+                return self._recv_cmd(1024).decode('ascii')
         return None
+
+    def _recv_cmd(self, nrbytes):
+        """."""
+        if self._socket:
+            return self._socket.recv(nrbytes)
+        else:
+            raise ValueError('Communication socket is None!')
 
 
 class Scopes:
@@ -314,22 +379,22 @@ class Scopes:
         stats_fields=Keysight.STATS_FIELDS1,
     )
     LI_PU_OSC_MODLTR = Scope(
-            ipaddr='10.128.101.70',
-            hostname='TB-PU-Osc-InjBO.abtlus.org.br',
+            ipaddr='10.128.150.20',
+            hostname='KEYSIGH-QQI8MNR.abtlus.org.br',
             port=5025,
             scopename='LI_PU_OSC_MODLTR',
             stats_fields=None,
     )
-    TS_PU_OSC_EJEBO = Scope(
-        ipaddr='10.128.120.70',
-        hostname='TS-PU-Osc-EjeBO.abtlus.org.br',
+    TB_PU_OSC_INJBO = Scope(
+        ipaddr='10.128.101.70',
+        hostname='TB-PU-Osc-InjBO.abtlus.org.br',
         port=5025,
-        scopename='TS_PU_OSC_EJEBO',
+        scopename='TB_PU_OSC_INJBO',
         stats_fields=None,
     )
     TS_PU_OSC_EJEBO = Scope(
-        ipaddr='10.128.101.72',
-        hostname='TS-PU-Osc-InjSI.abtlus.org.br',
+        ipaddr='10.128.120.70',
+        hostname='TS-PU-Osc-EjeBO.abtlus.org.br',
         port=5025,
         scopename='TS_PU_OSC_EJEBO',
         stats_fields=None,
